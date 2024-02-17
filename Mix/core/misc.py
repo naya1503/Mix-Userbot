88288a52c18dc1a64f385f2853021df7939bd394
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-02-17 18:40:01 +0700

    update Mix/core/misc.py

---
+++ Mix/core/misc.py
@@ -0,0 +1,55 @@
+#
+# Copyright (C) 2023-2024 by YukkiOwner@Github, < https://github.com/YukkiOwner >.
+#
+# This file is part of < https://github.com/YukkiOwner/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/YukkiOwner/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+import socket
+import time
+
+import heroku3
+from pyrogram import filters
+
+from config import *
+
+from Mix import LOGGER
+
+HAPP = None
+
+def is_heroku():
+    return "heroku" in socket.getfqdn()
+
+
+XCB = [
+    "/",
+    "@",
+    ".",
+    "com",
+    ":",
+    "git",
+    "heroku",
+    "push",
+    str(heroku_api_key),
+    "https",
+    str(heroku_app_name),
+    "HEAD",
+    "dev",
+]
+
+
+def heroku():
+    global HAPP
+    if is_heroku:
+        if heroku_api and heroku_app_name:
+            try:
+                Heroku = heroku3.from_key(heroku_api)
+                HAPP = Heroku.app(heroku_app_name)
+                LOGGER.info(f"Heroku App Configured")
+            except BaseException:
+                LOGGER.warning(
+                    f"Please make sure your Heroku API Key and Your App name are configured correctly in the heroku."
+                )
