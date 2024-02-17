e09379aed8753667384ef21fc0fa536b3e1a8362
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-02-17 18:30:37 +0700

    update config.py

--- config.py
+++ config.py
@@ -12,3 +12,11 @@ db_name = getenv("db_name", None)
 mongo_uri = getenv("mongo_uri", None)
 owner_id = int(getenv("owner_id", None))
 log_pic = getenv("log_pic", "https://telegra.ph//file/43cec0ae0ded594b55247.jpg")
+heroku_api = getenv("heroku_api")
+heroku_app_name = getenv("heroku_app_name")
+upstream_repo = getenv(
+    "upstream_repo",
+    "https://github.com/naya1503/Mix-Userbot",
+)
+upstream_branch = getenv("upstream_branch", "dev")
+git_token = getenv("git_token", None)
