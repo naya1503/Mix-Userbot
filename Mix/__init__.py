8567110215d01afb976127e71a4f998b1784456d
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-02-17 22:07:00 +0700

    update Mix/__init__.py

--- Mix/__init__.py
+++ Mix/__init__.py
@@ -15,33 +15,6 @@ user = Userbot(
 )
 
 
-#bot = Bot()
-
-class Bot2(Client):
-    def __init__(self, **kwargs):
-        super().__init__(name="bot", **kwargs)
-        self.api_id = api_id
-        self.api_hash = api_hash
-        self.bot_token = bot_token or udB.get_token(owner_id)
-        self.in_memory = False
-
-    def on_message(self, filters=None, group=99):
-        def decorator(func):
-            self.add_handler(MessageHandler(func, filters), group)
-            return func
-
-        return decorator
-
-    def on_callback_query(self, filters=None, group=99):
-        def decorator(func):
-            self.add_handler(CallbackQueryHandler(func, filters), group)
-            return func
-
-        return decorator
-
-    async def start(self):
-        await super().start()
-
-bot = Bot2()
+bot = Bot()
 
 from team import *
