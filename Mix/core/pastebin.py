495423f23165bc6f347165ba3cb6bc66b6b99607
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-02-17 19:00:12 +0700

    update Mix/core/pastebin.py

---
+++ Mix/core/pastebin.py
@@ -0,0 +1,48 @@
+import aiohttp
+
+from Mix.core.http import post
+
+BASE = "https://batbin.me/"
+
+dev_paste = {
+    "nekobin": {
+        "URL": "https://nekobin.com/api/documents",
+        "RAV": "result.key",
+        "GAS": "https://github.com/nekobin/nekobin",
+    },
+    "pasty": {
+        "URL": "https://pasty.lus.pm/api/v2/pastes",
+        "HEADERS": {
+            "User-Agent": "PyroGramBot/6.9",
+            "Content-Type": "application/json",
+        },
+        "RAV": "id",
+        "GAS": "https://github.com/lus/pasty",
+        "AVDTS": "modificationToken",
+    },
+    "pasting": {
+        "URL": "https://pasting.codes/api",
+    },
+}
+
+
+async def post(url: str, *args, **kwargs):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, *args, **kwargs) as resp:
+            try:
+                data = await resp.json()
+            except Exception:
+                data = await resp.text()
+        return data
+
+
+async def paste(content: str):
+    resp = await post(f"{BASE}api/v2/paste", data=content)
+    if not resp["success"]:
+        return
+    return BASE + resp["message"]
+
+
+
+
+
