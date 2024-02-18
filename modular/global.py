1f4804ef74ec10e5f46727ec3b5fe965cd0f835c
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-02-18 18:58:19 +0700

    update modular/global.py

--- modular/global.py
+++ modular/global.py
@@ -118,10 +118,6 @@ async def _(c: user, m):
             await c.unban_chat_member(chat, user.id)
             bs += 1
             await asyncio.sleep(0.1)
-        except FloodWait as e:
-            await asyncio.sleep(int(e.value))
-            await c.unban_chat_member(chat, user.id)
-            bs += 1
         except BaseException:
             gg += 1
             await asyncio.sleep(0.1)
