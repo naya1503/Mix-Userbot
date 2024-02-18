a2426f53cfb5d582f07c44633ef644172a72a24d
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-02-19 00:37:13 +0700

    update Mix/__main__.py

--- Mix/__main__.py
+++ Mix/__main__.py
@@ -63,5 +63,5 @@ async def main():
 
 if __name__ == "__main__":
     install()
-    asyncio.set_event_loop(event_loop)
-    event_loop.run_until_complete(main())
+    loop = asyncio.get_event_loop_policy().get_event_loop()
+    loop.run_until_complete(main())
