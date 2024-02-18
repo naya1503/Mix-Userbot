d42397aaa38974ed174f77a84c09841390fbaf96
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-02-18 18:36:13 +0700

    update Mix/__init__.py

--- Mix/__init__.py
+++ Mix/__init__.py
@@ -2,10 +2,7 @@ import asyncio
 from Mix.core import *
 from team.nandev.class_ubot import Userbot, Bot
 from config import *
-from pyrogram import filters
 
-GBAN_USER = filters.user()
-GMUTE_USER = filters.user()
 
 
 user = Userbot(
