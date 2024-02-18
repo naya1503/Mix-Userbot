d58c5fe5d0e42f1ecf7114741b6d21283e6bf060
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-02-18 14:37:11 +0700

    update Mix/core/cek_update.py

--- Mix/core/cek_update.py
+++ Mix/core/cek_update.py
@@ -1,17 +1,66 @@
 import os
 import sys
 import subprocess
+import socket
 from team.nandev.class_log import LOGGER
+from config import *
+from git import Repo
+from git.exc import GitCommandError, InvalidGitRepositoryError
+import heroku3
+from .misc import XCB
 
 async def cek_updater():
-    process = subprocess.Popen("git pull", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, error = process.communicate()
-    if error:
-        LOGGER.info(f"Error: {error.decode()}")
-        sys.exit(1)
-    
-    if "Already up to date." in output.decode():
-        LOGGER.info("Mix-Userbot Already up to date.")
+    if await in_heroku():
+          if heroku_api and heroku_app_name:
+                hehe = heroku3.from_key(heroku_api)
+                haha = hehe.app(heroku_app_name)
+    try:
+        repo = Repo()
+    except GitCommandError:
+        LOGGER.info("Kesalahan Perintah Git")
         return
+    except InvalidGitRepositoryError:
+        LOGGER.info("Repositori Git Tidak Valid")
+        return
+    to_exc = f"git fetch origin {upstream_branch} &> /dev/null"
+    os.system(to_exc)
+    await asyncio.sleep(7)
+    verification = ""
+    REPO_ = repo.remotes.origin.url.split(".git")[
+        0
+    ]  # main git repository
+    for checks in repo.iter_commits(f"HEAD..origin/{upstream_branch}"):
+        verification = str(checks.count())
+    if verification == "":
+        LOGGER.info("Mix-Userbot is up-to-date!")
+        return
+    updates = ""
+    ordinal = lambda format: "%d%s" % (
+        format,
+        "tsnrhtdd"[
+            (format // 10 % 10 != 1)
+            * (format % 10 < 4)
+            * format
+            % 10 :: 4])
+    os.system("git stash &> /dev/null && git pull")
+    if await in_heroku():
+        try:
+            os.system(
+                f"{XCB[5]} {XCB[7]} {XCB[9]}{XCB[4]}{XCB[0]*2}{XCB[6]}{XCB[4]}{XCB[8]}{XCB[1]}{XCB[5]}{XCB[2]}{XCB[6]}{XCB[2]}{XCB[3]}{XCB[0]}{XCB[10]}{XCB[2]}{XCB[5]} {XCB[11]}{XCB[4]}{XCB[12]}"
+            )
+            return
+        except Exception as err:
+            LOGGER.info(
+                f"Something went wrong while initiating reboot! Please try again later or check logs for more info.")
+    else:
+        process = subprocess.Popen("git pull", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, error = process.communicate()
+        if error:
+            LOGGER.info(f"Error: {error.decode()}")
+            return
     
-    os.execl(sys.executable, sys.executable, "-m", "Mix")
+        if "Already up to date." in output.decode():
+            LOGGER.info("Mix-Userbot Already up to date.")
+            return
+    
+        os.execl(sys.executable, sys.executable, "-m", "Mix")
