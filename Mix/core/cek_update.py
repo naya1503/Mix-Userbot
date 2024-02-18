4c064304fbf73862fb04193b634d372b21637de1
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-02-18 08:57:17 +0700

    update Mix/core/cek_update.py

--- Mix/core/cek_update.py
+++ Mix/core/cek_update.py
@@ -1,6 +1,16 @@
 import os
 import sys
+import subprocess
 
 async def cek_updater():
-    os.system("git pull")
+    process = subprocess.Popen("git pull", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, error = process.communicate()
+    if error:
+        LOGGER(f"Error: {error.decode()}")
+        sys.exit(1)
+    
+    if "Already up to date." in output.decode():
+        LOGGER("Mix-Userbot Already up to date.")
+        return
+    
     os.execl(sys.executable, sys.executable, "-m", "Mix")
