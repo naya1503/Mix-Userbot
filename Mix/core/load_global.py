8f618c8e6eaa0cb8cde561385ccf013919120786
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-02-18 18:40:44 +0700

    update Mix/core/load_global.py

--- Mix/core/load_global.py
+++ Mix/core/load_global.py
@@ -2,14 +2,15 @@
 from team.nandev.database import udB
 from team.nandev.class_log import LOGGER
 from pyrogram import filters
+from Mix import user
 
 GBAN_USER = filters.user()
 GMUTE_USER = filters.user()
 
-async def _global_users(c):
+async def _global_users():
     LOGGER.info(f"Loading For Gbanned And Gmute User.")
-    gmute = udB.get_list_from_var(c.me.id, "GMUTE", "USER")
-    gbanu = udB.get_list_from_var(c.me.id, "GBANNED", "USER")
+    gmute = udB.get_list_from_var(user.me.id, "GMUTE", "USER")
+    gbanu = udB.get_list_from_var(user.me.id, "GBANNED", "USER")
     try:
         for x in gmute:
             GMUTE_USER.add(x)
