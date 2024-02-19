1f27e14f690d04bd992bf3037f31f5d3dc57f1d5
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-02-19 14:20:37 +0700

    update modular/vcs.py

--- modular/vcs.py
+++ modular/vcs.py
@@ -31,7 +31,7 @@ from pytgcalls.types.input_stream import InputAudioStream, InputStream
 from Mix import *
 
 
-async def get_group_call(c, m, err_msg: str = "") -> Optional[InputGroupCall]:
+async def get_group_call(c: user, m, err_msg: str = "") -> Optional[InputGroupCall]:
     chat_peer = await c.resolve_peer(m.chat.id)
     if isinstance(chat_peer, (InputPeerChannel, InputPeerChat)):
         if isinstance(chat_peer, InputPeerChannel):
@@ -47,15 +47,15 @@ async def get_group_call(c, m, err_msg: str = "") -> Optional[InputGroupCall]:
 
 
 @ky.ubot("startvc", sudo=True)
-async def _(c, m):
+async def _(c: user, m):
 
     flags = " ".join(m.command[1:])
-    ky = await m.reply(f"{proses} <b>Processing....</b>")
+    ky = await m.reply(f"{c.proses} <b>Processing....</b>")
     if flags == enums.ChatType.CHANNEL:
         chat_id = m.chat.title
     else:
         chat_id = m.chat.id
-    args = f"{sukses} <b>Obrolan Suara Aktif</b>\n<b> Chat : </b><code>{m.chat.title}</code>"
+    args = f"{c.sukses} <b>Obrolan Suara Aktif</b>\n<b> Chat : </b><code>{m.chat.title}</code>"
     try:
         await c.invoke(
             CreateGroupCall(
@@ -65,19 +65,19 @@ async def _(c, m):
         )
         await ky.edit(args)
     except Exception as e:
-        await ky.edit(f"{gagal} <b>INFO:</b> `{e}`")
+        await ky.edit(f"{c.gagal} <b>INFO:</b> `{e}`")
 
 
 @ky.ubot("stopvc", sudo=True)
-async def _(c, m):
+async def _(c: user, m):
 
-    ky = await m.reply(f"{proses} <b>Processing....</b>")
+    ky = await m.reply(f"{c.proses} <b>Processing....</b>")
     m.chat.id
-    if not (group_call := (await get_group_call(c, m, err_msg=", Kesalahan..."))):
+    if not (group_call := (await get_group_call(c: user, m, err_msg=", Kesalahan..."))):
         return
     await c.invoke(DiscardGroupCall(call=group_call))
     await ky.edit(
-        f"{gagal} <b>Obrolan Suara Diakhiri</b>\n<b> Chat : </b><code>{m.chat.title}</code>"
+        f"{c.gagal} <b>Obrolan Suara Diakhiri</b>\n<b> Chat : </b><code>{m.chat.title}</code>"
     )
 
 
@@ -85,7 +85,7 @@ async def _(c, m):
 async def _(c: user, m):
 
     # global turun_dewek
-    ky = await m.reply(f"{proses} <b>Processing....</b>")
+    ky = await m.reply(f"{c.proses} <b>Processing....</b>")
     chat_id = m.command[1] if len(m.command) > 1 else m.chat.id
     with suppress(ValueError):
         chat_id = int(chat_id)
@@ -104,20 +104,20 @@ async def _(c: user, m):
             )
             await sleep(2)
             await ky.edit(
-                f"{sukses} <b>Berhasil Join Voice Chat</b>\n <b>Chat :</b><code>{m.chat.title}</code>"
+                f"{c.sukses} <b>Berhasil Join Voice Chat</b>\n <b>Chat :</b><code>{m.chat.title}</code>"
             )
             await sleep(1)
         except AlreadyJoinedError:
-            await ky.edit(f"{gagal} Akun anda sudah diatas.")
+            await ky.edit(f"{c.gagal} Akun anda sudah diatas.")
         except Exception as e:
-            return await ky.edit(f"{gagal} ERROR: {e}")
+            return await ky.edit(f"{c.gagal} ERROR: {e}")
 
 
 @ky.ubot("leavevc", sudo=True)
 async def _(c: user, m):
 
     global turun_dewek
-    ky = await m.reply(f"{proses} <b>Processing....</b>")
+    ky = await m.reply(f"{c.proses} <b>Processing....</b>")
     chat_id = m.command[1] if len(m.command) > 1 else m.chat.id
     with suppress(ValueError):
         chat_id = int(chat_id)
@@ -127,11 +127,11 @@ async def _(c: user, m):
             await user.call_py.leave_group_call(chat_id)
             # turun_dewek = True
             await ky.edit(
-                f"{sukses} <b>Berhasil Meninggalkan Voice Chat</b>\n <b>Chat :</b><code>{m.chat.title}</code>"
+                f"{c.sukses} <b>Berhasil Meninggalkan Voice Chat</b>\n <b>Chat :</b><code>{m.chat.title}</code>"
             )
         except Exception as e:
-            await ky.edit(f"{gagal} <b>ERROR:</b> {e}")
+            await ky.edit(f"{c.gagal} <b>ERROR:</b> {e}")
     else:
         return ky.edit(
-            f"{gagal} Akun anda sedang tidak berada dalam obrolan {chat_id}."
+            f"{c.gagal} Akun anda sedang tidak berada dalam obrolan {chat_id}."
         )
