2ff25f294e42ca6ec37ba8155d286883f06c49f7
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-02-19 12:26:39 +0700

    update modular/sudo.py

--- modular/sudo.py
+++ modular/sudo.py
@@ -25,13 +25,13 @@ __help__ = """
 
 @ky.ubot("addsudo", sudo=True)
 async def _(c: user, m):
-    emo = Emojii(c.me.id)
-    emo.initialize()
-    msg = await m.reply(f"{emo.proses} <b>Processing...</b>")
+    
+    
+    msg = await m.reply(f"{proses} <b>Processing...</b>")
     user_id = await c.extract_user(m)
     if not user_id:
         return await msg.edit(
-            f"{emo.gagal} <b>Silakan balas pesan pengguna/username/user id</b>"
+            f"{gagal} <b>Silakan balas pesan pengguna/username/user id</b>"
         )
     try:
         user = await c.get_users(user_id)
@@ -42,13 +42,13 @@ async def _(c: user, m):
 
     if user.id in sudo_users:
         return await msg.edit(
-            f"{emo.sukses} <b>[{user.first_name} {user.last_name or ''}](tg://user?id={user.id}) Sudah menjadi pengguna sudo.</b>"
+            f"{sukses} <b>[{user.first_name} {user.last_name or ''}](tg://user?id={user.id}) Sudah menjadi pengguna sudo.</b>"
         )
 
     try:
         udB.add_to_var(c.me.id, "SUDO_USER", user.id, "ID_NYA")
         return await msg.edit(
-            f"{emo.sukses} <b>[{user.first_name} {user.last_name or ''}](tg://user?id={user.id}) Ditambahkan ke pengguna sudo.</b>"
+            f"{sukses} <b>[{user.first_name} {user.last_name or ''}](tg://user?id={user.id}) Ditambahkan ke pengguna sudo.</b>"
         )
     except Exception as error:
         return await msg.edit(error)
@@ -56,13 +56,13 @@ async def _(c: user, m):
 
 @ky.ubot("delsudo", sudo=True)
 async def _(c: user, m):
-    emo = Emojii(c.me.id)
-    emo.initialize()
-    msg = await m.reply(f"{emo.proses} <b>Processing...</b>")
+    
+    
+    msg = await m.reply(f"{proses} <b>Processing...</b>")
     user_id = await c.extract_user(m)
     if not user_id:
         return await m.reply(
-            f"{emo.gagal} <b>Silakan balas pesan penggjna/username/user id.</b>"
+            f"{gagal} <b>Silakan balas pesan penggjna/username/user id.</b>"
         )
 
     try:
@@ -74,13 +74,13 @@ async def _(c: user, m):
 
     if user.id not in sudo_users:
         return await msg.edit(
-            f"{emo.gagal} <b>{user.first_name} {user.last_name or ''}](tg://user?id={user.id}) Bukan bagian pengguna sudo.</b>"
+            f"{gagal} <b>{user.first_name} {user.last_name or ''}](tg://user?id={user.id}) Bukan bagian pengguna sudo.</b>"
         )
 
     try:
         udB.remove_from_var(c.me.id, "SUDO_USER", user.id, "ID_NYA")
         return await msg.edit(
-            f"{emo.sukses} <b>[{user.first_name} {user.last_name or ''}](tg://user?id={user.id}) Dihapus dari pengguna sudo.</b>"
+            f"{sukses} <b>[{user.first_name} {user.last_name or ''}](tg://user?id={user.id}) Dihapus dari pengguna sudo.</b>"
         )
     except Exception as error:
         return await msg.edit(error)
@@ -88,29 +88,29 @@ async def _(c: user, m):
 
 @ky.ubot("sudolist", sudo=True)
 async def _(c: user, m):
-    emo = Emojii(c.me.id)
-    emo.initialize()
-    msg = await m.reply(f"{emo.proses} <b>Processing...</b>")
+    
+    
+    msg = await m.reply(f"{proses} <b>Processing...</b>")
     sudo_users = udB.get_list_from_var(c.me.id, "SUDO_USER", "ID_NYA")
 
     if not sudo_users:
-        return await msg.edit(f"{emo.gagal} <b>Tidak ada pengguna sudo ditemukan.</b>")
+        return await msg.edit(f"{gagal} <b>Tidak ada pengguna sudo ditemukan.</b>")
 
     sudo_list = []
     for user_id in sudo_users:
         try:
             user = await c.get_users(int(user_id))
             sudo_list.append(
-                f" {emo.profil} • [{user.first_name} {user.last_name or ''}](tg://user?id={user.id}) | <code>{user.id}</code>"
+                f" {profil} • [{user.first_name} {user.last_name or ''}](tg://user?id={user.id}) | <code>{user.id}</code>"
             )
         except:
             continue
 
     if sudo_list:
         response = (
-            f"{emo.profil} <b>Daftar Pengguna:</b>\n"
+            f"{profil} <b>Daftar Pengguna:</b>\n"
             + "\n".join(sudo_list)
-            + f"\n{emo.sukses} <code>{len(sudo_list)}</code>"
+            + f"\n{sukses} <code>{len(sudo_list)}</code>"
         )
         return await msg.edit(response)
     else:
