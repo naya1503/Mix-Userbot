f0d3a41e3df3794dffaeb5f1305c865d530f55bd
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-02-19 13:38:31 +0700

    update Mix/__init__.py

--- Mix/__init__.py
+++ Mix/__init__.py
@@ -19,20 +19,14 @@ bot = Bot()
 
 from team import *
 
-# def fexid():
-# emo = Emojii()
-# emo.initialize()
-#   return emo
+emoji = Emojii()
 
-
-# fex = fexid()
-
-ping = "<emoji id=5269563867305879894>🏓</emoji>"
-pong = "<emoji id=6183961455436498818>🥵</emoji>"
-proses = "<emoji id=6113844439292054570>🔄</emoji>"
-gagal = "<emoji id=6113872536968104754>❌</emoji>"
-sukses = "<emoji id=6113647841459047673>✅</emoji>"
-profil = "<emoji id=5373012449597335010>👤</emoji>"
-alive = "<emoji id=6127272826341690178>⭐</emoji>"
-warn = "<emoji id=6172475875368373616>❗️</emoji>"
-block = "<emoji id=5240241223632954241>🚫</emoji>"
+ping = emoji["ping"]()
+pong = emoji["pong"]()
+proses = emoji["proses"]()
+sukses = emoji["sukses"]()
+gagal = emoji["gagal"]()
+profil = emoji["profil"]()
+alive = emoji["alive"]()
+warn = emoji["warn"]()
+block = emoji["block"]()
