f81be5b76f1537cc2a32deac4ed56c1241f7d2fc
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-02-19 12:31:32 +0700

    update modular/global.py

--- modular/global.py
+++ modular/global.py
@@ -41,20 +41,20 @@ from Mix import *
 @ky.ubot("gban", sudo=True)
 @ky.devs("cgban")
 async def _(c: user, m):
-    emo = Emojii(c.me.id)
-    emo.initialize()
+    
+    
     nyet, alasan = await c.extract_user_and_reason(m)
-    xx = await m.reply(f"{emo.proses} Processing...")
+    xx = await m.reply(f"{proses} Processing...")
     try:
         org = await c.get_users(nyet)
     except PeerIdInvalid:
-        await xx.edit(f"{emo.gagal} Pengguna tidak ditemukan.")
+        await xx.edit(f"{gagal} Pengguna tidak ditemukan.")
         return
     if not org:
-        await xx.edit(f"{emo.gagal} Pengguna tidak ditemukan.")
+        await xx.edit(f"{gagal} Pengguna tidak ditemukan.")
         return
     if org.id in DEVS:
-        await xx.edit(f"{emo.gagal} Dia adalah Developer Mix-Userbot.")
+        await xx.edit(f"{gagal} Dia adalah Developer Mix-Userbot.")
         return
     bs = 0
     gg = 0
@@ -69,7 +69,7 @@ async def _(c: user, m):
             else "Anon")
     for chat in chats:
         if org.id in gban_users:
-            await xx.edit(f"{emo.gagal} Pengguna sudah digban.")
+            await xx.edit(f"{gagal} Pengguna sudah digban.")
             return
         try:
             await c.ban_chat_member(chat, org.id)
@@ -84,22 +84,22 @@ async def _(c: user, m):
             gg += 1
             await asyncio.sleep(0.1)
     udB.add_to_var(c.me.id, "GBANNED", org.id, "USER")
-    mmg = f"{emo.alive} <b>Warning Global Banned\n\n{emo.sukses} Berhasil: `{bs}` Chat\n{emo.gagal} Gagal: `{gg}` Chat\n{emo.profil} User: `{mention}`</b>\n"
+    mmg = f"{warn} <b>Warning Global Banned\n\n{sukses} Berhasil: `{bs}` Chat\n{gagal} Gagal: `{gg}` Chat\n{profil} User: `{mention}`</b>\n"
     if alasan:
-        mmg += f"{emo.alive} **Alasan: `{alasan}`**"
+        mmg += f"{block} **Alasan: `{alasan}`**"
     await m.reply(mmg)
     await xx.delete()
 
 @ky.ubot("ungban", sudo=True)
 @ky.devs("cungban")
 async def _(c: user, m):
-    emo = Emojii(c.me.id)
-    emo.initialize()
+    
+    
     nyet = await c.extract_user(m)
-    xx = await m.reply(f"{emo.proses} Processing...")
+    xx = await m.reply(f"{proses} Processing...")
     org = await c.get_users(nyet)
     if not org:
-        await xx.edit(f"{emo.gagal} Pengguna tidak ditemukan.")
+        await xx.edit(f"{gagal} Pengguna tidak ditemukan.")
         return
     bs = 0
     gg = 0
@@ -114,7 +114,7 @@ async def _(c: user, m):
             else "Anon")
     for chat in chats:
         if org.id not in gban_users:
-            await xx.edit(f"{emo.gagal} Pengguna belum digban.")
+            await xx.edit(f"{gagal} Pengguna belum digban.")
             return
         try:
             await c.unban_chat_member(chat, org.id)
@@ -124,23 +124,23 @@ async def _(c: user, m):
             gg += 1
             await asyncio.sleep(0.1)
     udB.remove_from_var(c.me.id, "GBANNED", org.id, "USER")
-    mmg = f"{emo.alive} <b>Warning Global Unbanned\n\n{emo.sukses} Berhasil: `{bs}` Chat\n{emo.gagal} Gagal: `{gg}` Chat\n{emo.profil} User: `{mention}`</b>\n"
+    mmg = f"{warn} <b>Warning Global Unbanned\n\n{sukses} Berhasil: `{bs}` Chat\n{gagal} Gagal: `{gg}` Chat\n{profil} User: `{mention}`</b>\n"
     await m.reply(mmg)
     await xx.delete()
 
 @ky.ubot("gmute", sudo=True)
 @ky.devs("cgmute")
 async def _(c: user, m):
-    emo = Emojii(c.me.id)
-    emo.initialize()
+    
+    
     nyet, alasan = await c.extract_user_and_reason(m)
-    xx = await m.reply(f"{emo.proses} Processing...")
+    xx = await m.reply(f"{proses} Processing...")
     org = await c.get_users(nyet)
     if not org:
-        await xx.edit(f"{emo.gagal} Pengguna tidak ditemukan.")
+        await xx.edit(f"{gagal} Pengguna tidak ditemukan.")
         return
     if org.id in DEVS:
-        await xx.edit(f"{emo.gagal} Dia adalah Developer Mix-Userbot.")
+        await xx.edit(f"{gagal} Dia adalah Developer Mix-Userbot.")
         return
     bs = 0
     gg = 0
@@ -155,7 +155,7 @@ async def _(c: user, m):
             else "Anon")
     for chat in chats:
         if org.id in gmute_users:
-            await xx.edit(f"{emo.gagal} Pengguna sudah digmute.")
+            await xx.edit(f"{gagal} Pengguna sudah digmute.")
             return
         try:
             await c.restrict_chat_member(chat, org.id, ChatPermissions())
@@ -165,9 +165,9 @@ async def _(c: user, m):
             gg += 1
             await asyncio.sleep(0.1)
     udB.add_to_var(c.me.id, "GMUTE", org.id, "USER")
-    mmg = f"{emo.alive} <b>Warning Global Gmute\n\n{emo.sukses} Berhasil: `{bs}` Chat\n{emo.gagal} Gagal: `{gg}` Chat\n{emo.profil} User: `{mention}`</b>\n"
+    mmg = f"{warn} <b>Warning Global Gmute\n\n{sukses} Berhasil: `{bs}` Chat\n{gagal} Gagal: `{gg}` Chat\n{profil} User: `{mention}`</b>\n"
     if alasan:
-        mmg += f"{emo.alive} **Alasan: `{alasan}`**"
+        mmg += f"{block} **Alasan: `{alasan}`**"
     await m.reply(mmg)
     await xx.delete()
     
@@ -175,16 +175,16 @@ async def _(c: user, m):
 @ky.ubot("ungmute", sudo=True)
 @ky.devs("cungmute")
 async def _(c: user, m):
-    emo = Emojii(c.me.id)
-    emo.initialize()
+    
+    
     nyet = await c.extract_user(m)
-    xx = await m.reply(f"{emo.proses} Processing...")
+    xx = await m.reply(f"{proses} Processing...")
     org = await c.get_users(nyet)
     if not org:
-        await xx.edit(f"{emo.gagal} Pengguna tidak ditemukan.")
+        await xx.edit(f"{gagal} Pengguna tidak ditemukan.")
         return
     if org.id in DEVS:
-        await xx.edit(f"{emo.gagal} Dia adalah Developer Mix-Userbot.")
+        await xx.edit(f"{gagal} Dia adalah Developer Mix-Userbot.")
         return
     bs = 0
     gg = 0
@@ -199,7 +199,7 @@ async def _(c: user, m):
             else "Anon")
     for chat in chats:
         if org.id not in gmute_users:
-            await xx.edit(f"{emo.gagal} Pengguna belum pernah digmute.")
+            await xx.edit(f"{gagal} Pengguna belum pernah digmute.")
             return
         try:
             await c.unban_member(chat, org.id, ChatPermissions())
@@ -209,59 +209,59 @@ async def _(c: user, m):
             gg += 1
             await asyncio.sleep(0.1)
     udB.remove_from_var(c.me.id, "GMUTE", org.id, "USER")
-    mmg = f"{emo.alive} <b>Warning Global Ungmute\n\n{emo.sukses} Berhasil: `{bs}` Chat\n{emo.gagal} Gagal: `{gg}` Chat\n{emo.profil} User: `{mention}`</b>\n"
+    mmg = f"{warn} <b>Warning Global Ungmute\n\n{sukses} Berhasil: `{bs}` Chat\n{gagal} Gagal: `{gg}` Chat\n{profil} User: `{mention}`</b>\n"
     await m.reply(mmg)
     await xx.delete()
     
     
 @ky.ubot("gbanlist|listgban", sudo=True)
 async def _(c: user, m):
-    emo = Emojii(c.me.id)
-    emo.initialize()
+    
+    
     gban_list = []
-    msg = await m.reply(f"{emo.proses} <b>Processing...</b>")
+    msg = await m.reply(f"{proses} <b>Processing...</b>")
     gbanu = udB.get_list_from_var(c.me.id, "GBANNED", "USER")
     if not gbanu:
-        return await msg.edit(f"{emo.gagal} <b>Tidak ada pengguna ditemukan.</b>")
+        return await msg.edit(f"{gagal} <b>Tidak ada pengguna ditemukan.</b>")
     for x in gbanu:
         try:
             org = await c.get_users(int(x))
             gban_list.append(
-                f"{emo.profil} • [{user.first_name} {user.last_name or ''}](tg://user?id={org.id}) | <code>{org.id}</code>")
+                f"{profil} • [{user.first_name} {user.last_name or ''}](tg://user?id={org.id}) | <code>{org.id}</code>")
         except:
             continue
     if gban_list:
        stak = (
-            f"{emo.profil} <b>Daftar Pengguna:</b>\n"
+            f"{profil} <b>Daftar Pengguna:</b>\n"
             + "\n".join(gban_list)
-            + f"\n{emo.sukses} <code>{len(gban_list)}</code>")
+            + f"\n{sukses} <code>{len(gban_list)}</code>")
        return await msg.edit(stak)
     else:
-        return await msg.edit(f"{emo.gagal} <b>Eror</b>")
+        return await msg.edit(f"{gagal} <b>Eror</b>")
         
         
 
 @ky.ubot("gmutelist|listgmute", sudo=True)
 async def _(c: user, m):
-    emo = Emojii(c.me.id)
-    emo.initialize()
+    
+    
     gmute_list = []
-    msg = await m.reply(f"{emo.proses} <b>Processing...</b>")
+    msg = await m.reply(f"{proses} <b>Processing...</b>")
     gmute = udB.get_list_from_var(c.me.id, "GMUTE", "USER")
     if not gmute:
-        return await msg.edit(f"{emo.gagal} <b>Tidak ada pengguna ditemukan.</b>")
+        return await msg.edit(f"{gagal} <b>Tidak ada pengguna ditemukan.</b>")
     for x in gmute:
         try:
             org = await c.get_users(int(x))
             gmute_list.append(
-                f"{emo.profil} • [{user.first_name} {user.last_name or ''}](tg://user?id={org.id}) | <code>{org.id}</code>")
+                f"{profil} • [{user.first_name} {user.last_name or ''}](tg://user?id={org.id}) | <code>{org.id}</code>")
         except:
             continue
     if gmute_list:
        stak = (
-            f"{emo.profil} <b>Daftar Pengguna:</b>\n"
+            f"{profil} <b>Daftar Pengguna:</b>\n"
             + "\n".join(gmute_list)
-            + f"\n{emo.sukses} <code>{len(gmute_list)}</code>")
+            + f"\n{sukses} <code>{len(gmute_list)}</code>")
        return await msg.edit(stak)
     else:
-        return await msg.edit(f"{emo.gagal} <b>Eror</b>")
+        return await msg.edit(f"{gagal} <b>Eror</b>")
