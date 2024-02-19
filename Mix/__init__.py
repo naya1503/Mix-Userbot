1d09eceda478ffb39ab86a9c87c2d57bb0b3e1f2
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-02-19 13:32:05 +0700

    update Mix/__init__.py

--- Mix/__init__.py
+++ Mix/__init__.py
@@ -7,7 +7,7 @@ from Mix.core import *
 
 user = Userbot(
     name="user",
-    api_id=api_id,
+    api=api,
     api_hash=api_hash,
     session_string=session,
 )
@@ -20,20 +20,20 @@ bot = Bot()
 from team import *
 
 
-def fexid():
-    emo = Emojii()
-    emo.initialize()
-    return emo
+#def fexid():
+    #emo = Emojii()
+   # emo.initialize()
+ #   return emo
 
 
-fex = fexid()
+#fex = fexid()
 
-ping = fex.ping
-pong = fex.pong
-proses = fex.proses
-alive = fex.alive
-gagal = fex.gagal
-sukses = fex.sukses
-profil = fex.profil
-warn = fex.warn
-block = fex.block
+ping = "<emoji id=5269563867305879894>🏓</emoji>"
+pong = "<emoji id=6183961455436498818>🥵</emoji>"
+proses = "<emoji id=6113844439292054570>🔄</emoji>"
+gagal = "<emoji id=6113872536968104754>❌</emoji>"
+sukses = "<emoji id=6113647841459047673>✅</emoji>"
+profil = "<emoji id=5373012449597335010>👤</emoji>"
+alive = "<emoji id=6127272826341690178>⭐</emoji>"
+warn = "<emoji id=6172475875368373616>❗️</emoji>"
+block = "<emoji id=5240241223632954241>🚫</emoji>"
