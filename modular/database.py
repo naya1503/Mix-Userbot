4236e6ccaa283c216daa5fee094eb9278f41b9c6
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-02-20 18:22:16 +0700

    update modular/database.py

--- modular/database.py
+++ modular/database.py
@@ -93,7 +93,7 @@ async def _(c, m):
                     m.chat.id,
                     x.query_id,
                     x.results[0].id,
-                    reply_to_message_id=ReplyCheck(m),
+                    reply_to_message_id=m.id,
                 )
                 await jing.delete()
             except Exception as e:
