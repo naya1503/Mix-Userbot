7437610574d1e5f215a86b9710a83cd3a20928d3
naya1503 <naya1503@users.noreply.github.com>
2024-02-21 13:01:11 +0000

    Auto Fix

--- Mix/core/btn.py
+++ Mix/core/btn.py
@@ -104,7 +104,6 @@ def parse_mark(teks, xx):
     return " ".join(buttons) if buttons else teks
 
 
-
 def extract_time(time_val):
     if any(time_val.endswith(unit) for unit in ("s", "m", "h", "d")):
         unit = time_val[-1]
