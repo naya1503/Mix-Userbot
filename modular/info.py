8b18098f0c1f59edd2452db28625857bb691a808
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-02-23 11:59:57 +0700

    update modular/info.py

---
+++ modular/info.py
@@ -0,0 +1,7 @@
+################################################################
+"""
+ Mix-Userbot Open Source . Maintained ? Yes Oh No Oh Yes Ngentot
+ 
+ @ CREDIT : NAN-DEV || Gojo_Satoru
+"""
+################################################################
