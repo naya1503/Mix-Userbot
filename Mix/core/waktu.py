eb4b97e71bad6f5d643cfa668321552d011ca9b3
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-02-23 20:58:47 +0700

    update Mix/core/waktu.py

--- Mix/core/waktu.py
+++ Mix/core/waktu.py
@@ -1,9 +1,9 @@
 # @TomiX
 
 from time import time as waktunya
-
+from datetime import datetime, timedelta
 start_time = waktunya()
-
+from team.nandev.database import cleanmode
 
 async def get_time(seconds):
     count = 0
@@ -28,3 +28,13 @@ async def get_time(seconds):
     up_time += ":".join(time_list)
 
     return up_time
+
+async def put_cleanmode(org, message_id):
+    if org not in cleanmode:
+        cleanmode[org] = []
+    time_now = datetime.now()
+    put = {
+        "msg_id": message_id,
+        "timer_after": time_now + timedelta(minutes=1),
+    }
+    cleanmode[org].append(put)
