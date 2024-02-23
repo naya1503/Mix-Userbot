b1b307493e5694d5f4762cffce953f20f6e871b9
naya1503 <naya1503@users.noreply.github.com>
2024-02-23 16:04:05 +0000

    Auto Fix

--- modular/afk.py
+++ modular/afk.py
@@ -26,15 +26,15 @@ __help__ = """
 """
 
 
-#async def apa_ya(f, c, m):
-    #af_k_c = udB.is_afk(user.me.id)
-    #if af_k_c:
-        #return bool(True)
-    #else:
-        #return bool(False)
+# async def apa_ya(f, c, m):
+# af_k_c = udB.is_afk(user.me.id)
+# if af_k_c:
+# return bool(True)
+# else:
+# return bool(False)
 
 
-#isAfk = filters.create(apa_ya)
+# isAfk = filters.create(apa_ya)
 
 
 @ky.ubot("afk")
