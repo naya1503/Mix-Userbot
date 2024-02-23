0935ec1506c8f570b72f65edf6947ad22efb538d
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-02-23 19:44:00 +0700

    update modular/alive.py

--- modular/alive.py
+++ modular/alive.py
@@ -29,8 +29,7 @@ async def _(c: user, m):
     try:
         x = await c.get_inline_bot_results(bot.me.username, "alive")
         await m.reply_inline_bot_result(
-            x.query_id, x.results[0].id, reply_to_message=m.id
-        )
+            x.query_id, x.results[0].id)
     except Exception as error:
         await m.reply(error)
 
