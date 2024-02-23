70365f25a77c6e105e022c5b153d758775a18123
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-02-23 08:52:28 +0700

    update Mix/core/waktu.py

---
+++ Mix/core/waktu.py
@@ -0,0 +1,30 @@
+# @TomiX
+
+from time import time as waktunya
+
+start_time = waktunya()
+
+
+async def get_time(seconds):
+    count = 0
+    up_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "d"]
+
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for i in range(len(time_list)):
+        time_list[i] = str(time_list[i]) + time_suffix_list[i]
+    if len(time_list) == 4:
+        up_time += time_list.pop() + ":"
+
+    time_list.reverse()
+    up_time += ":".join(time_list)
+
+    return up_time
