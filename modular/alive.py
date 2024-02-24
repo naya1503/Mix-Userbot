64748489e76e9ffa8a4ba5db1209c4ab3459aac6
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-02-24 12:05:59 +0700

    update modular/alive.py

--- modular/alive.py
+++ modular/alive.py
@@ -12,7 +12,7 @@ from time import time
 from pyrogram.raw.functions import Ping
 from pyrogram.types import *
 
-from Mix import DEVS, ky, udB, user
+from Mix import DEVS, ky, udB, user, bot
 from Mix.core.waktu import get_time, start_time
 
 __modles__ = "Alive"
