89284aa1cbef8a7c8172d38fd17ca18ae57a191e
naya1503 <naya1503@users.noreply.github.com>
2024-02-24 13:04:07 +0000

    Auto Fix

--- modular/graph.py
+++ modular/graph.py
@@ -19,6 +19,7 @@ from telegraph import Telegraph, exceptions, upload_file
 
 from Mix import *
 
+
 @ky.ubot("tg", sudo=True)
 async def _(c: user, m):
     em = Emojik()
@@ -32,7 +33,9 @@ async def _(c: user, m):
         try:
             media_url = upload_file(m_d)
         except exceptions.TelegraphException as r:
-            return await xx.edit(f"{em.gagal} <code>{r}</code>\n\nLapor ke @KynanSupport!")
+            return await xx.edit(
+                f"{em.gagal} <code>{r}</code>\n\nLapor ke @KynanSupport!"
+            )
         dnbg = f"{em.sukses} <b>Diupload ke : </b> <a href='https://telegra.ph/{media_url[0]}'>Klik Disini</a>"
         await xx.edit(dnbg)
     elif m.reply_to_message.text:
@@ -42,6 +45,8 @@ async def _(c: user, m):
         try:
             response = telegraph.create_page(page_title, html_content=page_text)
         except exceptions.TelegraphException as r:
-            return await xx.edit(f"{em.gagal} <code>{r}</code>\n\nLapor ke @KynanSupport!")
+            return await xx.edit(
+                f"{em.gagal} <code>{r}</code>\n\nLapor ke @KynanSupport!"
+            )
         ybg = f"{em.sukses} <b>Diupload ke :</b> <a href='https://telegra.ph/{response['path']}'>Klik Disini</a>"
         await xx.edit(ybg)
