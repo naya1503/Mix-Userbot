4263d9d546ad5a3c5e48da202baafe8d31ecb457
naya1503 <naya1503@users.noreply.github.com>
2024-02-24 04:48:15 +0000

    Auto Fix

--- modular/filte.py
+++ modular/filte.py
@@ -269,16 +269,18 @@ async def send_filter_reply(c: user, m, trigger: str):
 
 
 async def isFilter_(filter, c, m):
-    xu = db.get_all_filters(m.chat.id)
+    db.get_all_filters(m.chat.id)
     tx = m.text
     for xe in xi:
         if tx in xi:
             return True
     return False
 
+
 # @user.on_message(filters.text & filters.group & ~filters.bot, group=69)
 
 isFilter = filters.create(isFilter_)
+
 
 @user.on_message(isFilter & filters.group, group=69)
 async def _(c: user, m):
