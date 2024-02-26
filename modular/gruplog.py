5055e06e84aad6c7bfc8d4e2239725d16402938f
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-02-26 12:17:23 +0700

    update modular/gruplog.py

--- modular/gruplog.py
+++ modular/gruplog.py
@@ -66,7 +66,7 @@ async def _(c, m):
         [
             [
                 InlineKeyboardButton("Replied Message", url=lenk),
-                InlineKeyboardButton(f"{org}", user_id=m.from_user.id),
+                #InlineKeyboardButton(f"{org}", user_id=m.from_user.id),
             ]
         ]
     )
