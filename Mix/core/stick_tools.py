14f75c75565beccce2cd23b8a4ba01a0d5613b26
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-02-28 22:10:16 +0700

    update Mix/core/stick_tools.py

---
+++ Mix/core/stick_tools.py
@@ -0,0 +1,26 @@
+################################################################
+"""
+ Mix-Userbot Open Source . Maintained ? Yes Oh No Oh Yes Ngentot
+ 
+ @ CREDIT : NAN-DEV || Misskaty || William || Gojo_Satoru
+"""
+################################################################
+
+from pyrogram import emoji
+
+def get_emoji_regex():
+    e_list = [
+        getattr(emoji, e).encode("unicode-escape").decode("ASCII")
+        for e in dir(emoji)
+        if not e.startswith("_")
+    ]
+    # to avoid re.error excluding char that start with '*'
+    e_sort = sorted([x for x in e_list if not x.startswith("*")], reverse=True)
+    # Sort emojis by length to make sure multi-character emojis are
+    # matched first
+    pattern_ = f"({'|'.join(e_sort)})"
+    return re.compile(pattern_)
+    
+    
+EMOJI_PATTERN = get_emoji_regex()
+SUPPORTED_TYPES = ["jpeg", "png", "webp"]
