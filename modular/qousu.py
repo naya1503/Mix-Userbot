6301efd2f0cb3b3cb88f619de337c0db641564b7
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-02-28 16:31:24 +0700

    update modular/qousu.py

--- modular/qousu.py
+++ modular/qousu.py
@@ -42,46 +42,37 @@ async def _(c: user, m):
         )
         messages = [m_one]
     elif len(m.command) > 2:
-        tag = m.command[1].strip()
-        ct = isArgInt(m.command[1])
-        if tag.startswith("@"):
-            user_id = tag[1:]
-            try:
-                org = await c.get_users(user_id)
-                if org.id in DEVS:
-                    await mk.edit(
-                        f"{em.gagal} **Si anjing mengatasnamakan Developer!**"
-                    )
-                    return
-                rep = await c.get_messages(m.chat.id, m.reply_to_message.id)
-                rep.from_user = org
-                messages = [rep]
-            except Exception as e:
-                return await m.reply(f"{em.gagal} Error : <code>{e}</code>")
+        user_id = await c.extract_user(m)
+        try:
+            org = await c.get_users(user_id)
+            if org.id in DEVS:
+                await mk.edit(
+                  f"{em.gagal} **Si anjing mengatasnamakan Developer!**")
+                return
+            rep = await c.get_messages(m.chat.id, m.reply_to_message.id)
+            rep.from_user = org
+            messages = [rep]
+        except Exception as e:
+            await m.reply(f"{em.gagal} Error : <code>{e}</code>")
+        warna = m.text.split()[2].strip()
+        if warna in loanjing:
+            acak = warna
         else:
-            warna = m.text.split()[2].strip().lower()
-            if warna in loanjing:
-                acak = warna
-            else:
-                acak = random.choice(loanjing)
-            if ct[0]:
-                if ct[1] < 2 or ct[1] > 10:
-                    return await m.reply(f"{em.gagal} Batas pesan adalah 10")
-                try:
-                    messages = [
-                        i
-                        for i in await c.get_messages(
-                            chat_id=m.chat.id,
-                            message_ids=range(
-                                m.reply_to_message.id,
-                                m.reply_to_message.id + (ct[1] + 5),
-                            ),
-                            replies=-1,
-                        )
-                        if not i.empty and not i.media
-                    ]
-                except Exception as e:
-                    return await m.reply(f"{em.gagal} Error : <code>{e}</code>")
+            acak = random.choice(loanjing)
+        angka = int(m.text.split()[3].strip())
+        if angka > 10:
+            await m.reply(f"{em.gagal} Batas pesan adalah 10")
+        try:
+            messages = [
+                i
+                for i in await c.get_messages(
+                    chat_id=m.chat.id,
+                    message_ids=range(
+                    m.reply_to_message.id,
+                    m.reply_to_message.id + (angka + 5)), replies=-1)
+                if not i.empty and not i.media]
+        except Exception as e:
+            await m.reply(f"{em.gagal} Error : <code>{e}</code>")
 
     try:
         hasil = await quotly(messages, acak)
