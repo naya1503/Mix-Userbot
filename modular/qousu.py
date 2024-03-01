9bcd78e60f151a7eeb987a8aa70d67a004ee4662
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-01 08:36:02 +0700

    update modular/qousu.py

--- modular/qousu.py
+++ modular/qousu.py
@@ -40,7 +40,7 @@ async def _(c: user, m):
     em = Emojik()
     em.initialize()
     iymek = f"\n• ".join(loanjing)
-    jadi = f"{em.sukses} *Daftar Warna Quote :\n\n**"
+    jadi = f"{em.sukses} **Daftar Warna Quote :\n\n**"
     if len(iymek) > 4096:
         with open("qcolor.txt", "w") as file:
             file.write(iymek)
