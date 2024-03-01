be49c4ff2d7c0bdc905d51e10fa381d6fcd2be50
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-01 11:30:17 +0700

    update modular/terjemah.py

--- modular/terjemah.py
+++ modular/terjemah.py
@@ -119,7 +119,7 @@ async def _(c: user, m):
         return
     kd = m.text.split(None, 1)[1]
     for lang, code in kode_bahasa.items():
-        if code.lower() == kd.lower():
+        if code == kd:
             c._translate[c.me.id] = {"negara": kd}
             await pros.edit(f"{em.sukses} Kode bahasa diganti ke : `{kd} - {lang}`")
             return
