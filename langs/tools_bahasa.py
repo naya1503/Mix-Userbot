f0fef9c122087c4a2207cca6a165b8b9571ab34d
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-02 18:10:17 +0700

    update langs/tools_bahasa.py

--- langs/tools_bahasa.py
+++ langs/tools_bahasa.py
@@ -17,15 +17,16 @@ from team.nandev.database import udB
 
 from Mix import user
 
-list_bhs: List[str] = [
+enabled_locales: List[str] = [
+    "en-US",  # English (United States)
     "id-ID",  # Indonesian
 ]
 
-def_bhs: str = "id-ID"
+default_language: str = "id-ID"
 
 
-def load_bhs(files: List[str]) -> Dict[str, Dict[str, Dict[str, str]]]:
-    ldict = {lang: {} for lang in list_bhs}
+def cache_localizations(files: List[str]) -> Dict[str, Dict[str, Dict[str, str]]]:
+    ldict = {lang: {} for lang in enabled_locales}
     for file in files:
         _, lname, pname = file.split(os.path.sep)
         pname = pname.split(".")[0]
@@ -37,36 +38,38 @@ def load_bhs(files: List[str]) -> Dict[str, Dict[str, Dict[str, str]]]:
 
 jsons: List[str] = []
 
-for locale in list_bhs:
+for locale in enabled_locales:
     jsons += glob(os.path.join("langs", locale, "*.json"))
 
-langdict = load_bhs(jsons)
+langdict = cache_localizations(jsons)
 
 
-def get_bhs_str(
+def get_locale_string(
     dic: dict, language: str, default_context: str, key: str, context: str = None
 ) -> str:
     if context:
         default_context = context
-        dic = langdict[language].get(context, langdict[def_bhs][context])
-    res: str = dic.get(key) or langdict[def_bhs][default_context].get(key) or key
+        dic = langdict[language].get(context, langdict[default_language][context])
+    res: str = (
+        dic.get(key) or langdict[default_language][default_context].get(key) or key
+    )
     return res
 
 
-async def get_bhsnya():
-    lang = def_bhs or udB.get_bahasa(user.me.id)
+async def get_lang(c) -> str:
 
-    # User has a language_code without hyphen
+    lang = default_language or udB.get_bahasa(c.me.id)
+
     if len(lang.split("-")) == 1:
         # Try to find a language that starts with the provided language_code
-        for locale_ in list_bhs:
+        for locale_ in enabled_locales:
             if locale_.startswith(lang):
                 lang = locale_
     elif lang.split("-")[1].islower():
         lang = lang.split("-")
         lang[1] = lang[1].upper()
         lang = "-".join(lang)
-    return lang if lang in list_bhs else def_bhs
+    return lang if lang in enabled_locales else default_language
 
 
 def bahasa(context: str = None):
@@ -83,11 +86,11 @@ def bahasa(context: str = None):
     def decorator(func):
         @wraps(func)
         async def wrapper(c, m):
-            lang = await get_bhsnya(user.me.id)
+            lang = await get_lang(c)
 
-            dic = langdict.get(lang, langdict[def_bhs])
+            dic = langdict.get(lang, langdict[default_language])
 
-            lfunc = partial(get_bhs_str, dic.get(context, {}), lang, context)
+            lfunc = partial(get_locale_string, dic.get(context, {}), lang, context)
             return await func(c, m, lfunc)
 
         return wrapper
