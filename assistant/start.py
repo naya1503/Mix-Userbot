ee2a6f5a909d9ee75f2760a55c7240ad5a0dd0a6
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-02 18:36:04 +0700

    update assistant/start.py

---
+++ assistant/start.py
@@ -0,0 +1,16 @@
+################################################################
+"""
+ Mix-Userbot Open Source . Maintained ? Yes Oh No Oh Yes Ngentot
+ 
+ @ CREDIT : NAN-DEV
+"""
+################################################################
+
+from Mix import *
+
+
+@ky.bots("start")
+async def _(c, m):
+    await m.reply("**Hai nyet!!**")
+    
+    
