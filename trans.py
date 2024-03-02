3a09246c120d92eb68816fd9e92fecdd46be1430
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-02 23:13:55 +0700

    update trans.py

--- trans.py
+++ trans.py
@@ -1,29 +1,24 @@
 import os
-
 import yaml
-from googletrans import Translator
-
+from gpytranslate import Translator
 
 def translate_file(file_path, translator, target_lang):
-    with open(file_path, "r", encoding="utf-8") as file:
+    with open(file_path, 'r', encoding='utf-8') as file:
         data = yaml.safe_load(file)
-
+    
     translated_data = {}
     for key, value in data.items():
         if isinstance(value, str):
-            key_parts = key.split(":")
-            translated_key = key_parts[0].strip() + ":"
-            translated_value = translator.translate(
-                value.strip(), dest=target_lang
-            ).text
+            key_parts = key.split(':')
+            translated_key = key_parts[0].strip() + ':'
+            translated_value = translator.translate(value.strip(), target_lang=target_lang)
             translated_data[translated_key] = translated_value
         elif isinstance(value, dict):
             translated_data[key] = translate_nested(value, translator, target_lang)
         else:
             translated_data[key] = value
-
+    
     return translated_data
-
 
 def translate_nested(data, translator, target_lang):
     translated_data = {}
@@ -31,32 +26,24 @@ def translate_nested(data, translator, target_lang):
         if isinstance(value, dict):
             translated_value = translate_nested(value, translator, target_lang)
         else:
-            translated_value = translator.translate(
-                value.strip(), dest=target_lang
-            ).text
+            translated_value = translator.translate(value.strip(), target_lang=target_lang)
         translated_data[key] = translated_value
     return translated_data
 
-
 def main():
     translator = Translator()
-    target_lang = "en"
-
-    files_to_translate = [
-        file
-        for file in os.listdir("langs/strings")
-        if file.endswith(".yml") and file != "id.yml"
-    ]
-
+    target_lang = 'en'
+    
+    files_to_translate = [file for file in os.listdir('langs/strings') if file.endswith('.yml') and file != 'id.yml']
+    
     for file_name in files_to_translate:
-        input_file = os.path.join("langs/strings", file_name)
-        output_file = os.path.join("langs/strings", f"{target_lang}_{file_name}")
-
+        input_file = os.path.join('langs/strings', file_name)
+        output_file = os.path.join('langs/strings', f'{target_lang}_{file_name}')
+        
         translated_data = translate_file(input_file, translator, target_lang)
-
-        with open(output_file, "w", encoding="utf-8") as output:
+        
+        with open(output_file, 'w', encoding='utf-8') as output:
             yaml.dump(translated_data, output, allow_unicode=True)
-
 
 if __name__ == "__main__":
     main()
