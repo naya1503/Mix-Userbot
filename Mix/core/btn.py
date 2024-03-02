ac53555f2d6ec91061f11744e8debea6bf87260f
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-02 09:23:19 +0700

    update Mix/core/btn.py

--- Mix/core/btn.py
+++ Mix/core/btn.py
@@ -144,10 +144,14 @@ def nan_parse(text):
         if n_escapes % 2 == 0:
             button_text = match.group(1)
             button_url = match.group(2)
-            if "&&" in button_text:
-                button_pairs = button_text.split(" && ")
+            if "&&" in button_url:
+                button_pairs = button_url.split(" && ")
                 for pair in button_pairs:
-                    buttons.append((pair, button_url, True))
+                    if " - " in pair:
+                        pair_text, pair_url = pair.split(" - ")
+                        buttons.append((pair_text, pair_url, True))
+                    else:
+                        buttons.append((pair, True))
             else:
                 buttons.append((button_text, button_url, False))
             note_data += markdown_note[prev : match.start(0)]
