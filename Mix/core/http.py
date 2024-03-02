5cba830a7f4aa3e7dc5ebe138ff86fb65bdab41f
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-02 22:22:02 +0700

    update Mix/core/http.py

--- Mix/core/http.py
+++ Mix/core/http.py
@@ -38,15 +38,6 @@ async def post(url: str, *args, **kwargs):
     return data
 
 
-def post2(url: str, *args, **kwargs):
-    with session.post(url, *args, **kwargs) as resp:
-        try:
-            data = resp.json()
-        except Exception:
-            data = resp.text()
-    return data
-
-
 async def multiget(url: str, times: int, *args, **kwargs):
     return await gather(*[get(url, *args, **kwargs) for _ in range(times)])
 
