bef783ce1002930e49242508c79d019c8472895e
naya1503 <naya1503@users.noreply.github.com>
2024-03-02 18:36:21 +0000

    Auto Fix

--- trans.py
+++ trans.py
@@ -1,15 +1,17 @@
 import yaml
 from gpytranslate import Translator
 
+
 # Fungsi untuk menerjemahkan teks menggunakan Google Translate API
 def translate_text(text, target_language):
     translator = Translator()
     translated_text = translator.translate(text, target_language=target_language)
     return translated_text
 
+
 # Fungsi untuk menerjemahkan string YAML
 def translate_yaml(yaml_file, target_language):
-    with open(yaml_file, 'r') as file:
+    with open(yaml_file, "r") as file:
         data = yaml.safe_load(file)
 
     # Menerjemahkan nilai teks di dalam tanda kutip menggunakan Google Translate
@@ -26,7 +28,8 @@ def translate_yaml(yaml_file, target_language):
 
     return translated_yaml
 
+
 # Menjalankan fungsi untuk menerjemahkan string YAML dan menyimpannya ke file
-translated_yaml = translate_yaml('langs/strings/id.yml', 'en')
-with open('translated_strings.yml', 'w') as file:
+translated_yaml = translate_yaml("langs/strings/id.yml", "en")
+with open("translated_strings.yml", "w") as file:
     file.write(translated_yaml)
