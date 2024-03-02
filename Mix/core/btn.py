d168fd6e09c30974b0c8c494ce8f5c4e73d0ba03
naya1503 <naya1503@users.noreply.github.com>
2024-03-02 02:25:58 +0000

    Auto Fix

--- Mix/core/btn.py
+++ Mix/core/btn.py
@@ -98,7 +98,6 @@ def parse_mark(tk: str, kk):
     return f"{tk} {' '.join(buttons)}"
 
 
-
 def nan_parse(text):
     markdown_note = text
     prev = 0
@@ -127,6 +126,8 @@ def nan_parse(text):
         note_data += markdown_note[prev:]
 
     return note_data, buttons
+
+
 """
 def nan_parse(text):
     markdown_note = text
@@ -162,6 +163,7 @@ def nan_parse(text):
 
     return note_data, buttons
 """
+
 
 def nan_kibor(buttons):
     keyb = []
