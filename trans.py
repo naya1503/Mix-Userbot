75a1138c0237ffe3c52bf38a4d800f6fb675cae1
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-03 01:39:39 +0700

    update trans.py

--- trans.py
+++ trans.py
@@ -1,8 +1,6 @@
-import asyncio
-
 import yaml
+import asyncio
 from gpytranslate import Translator
-
 
 # Fungsi untuk menerjemahkan teks menggunakan Google Translate API
 async def translate_text(text, target_language):
@@ -10,10 +8,9 @@ async def translate_text(text, target_language):
     translated_text = await translator.translate(text, target_language=target_language)
     return translated_text
 
-
 # Fungsi untuk menerjemahkan string YAML
 async def translate_yaml(yaml_file, target_language):
-    with open(yaml_file, "r") as file:
+    with open(yaml_file, 'r') as file:
         data = yaml.safe_load(file)
 
     # Menerjemahkan nilai teks di dalam tanda kutip menggunakan Google Translate
@@ -30,12 +27,10 @@ async def translate_yaml(yaml_file, target_language):
 
     return translated_yaml
 
-
 # Menjalankan fungsi untuk menerjemahkan string YAML dan menyimpannya ke file
 async def main():
-    translated_yaml = await translate_yaml("langs/strings/id.yml", "en")
-    with open("translated_strings.yml", "w") as file:
+    translated_yaml = await translate_yaml('langs/strings/id.yml', 'en')
+    with open('translated_strings.yml', 'w') as file:
         file.write(translated_yaml)
-
 
 asyncio.run(main())
