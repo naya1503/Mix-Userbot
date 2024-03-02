a2d7c1ddc641f1f09ddc146b5d386c6845d08820
naya1503 <naya1503@users.noreply.github.com>
2024-03-02 17:59:14 +0000

    Auto Fix

--- trans.py
+++ trans.py
@@ -1,18 +1,20 @@
 import yaml
 from gpytranslate import Translator
 
+
 def translate_yaml(input_file, output_file):
     translator = Translator()
-    
-    with open(input_file, 'r', encoding='utf-8') as file:
+
+    with open(input_file, "r", encoding="utf-8") as file:
         data = yaml.safe_load(file)
-        
+
     for key, value in data.items():
         if isinstance(value, str):
-            translated_text = translator.translate(value, source='id', target='en')
+            translated_text = translator.translate(value, source="id", target="en")
             data[key] = translated_text
-            
-    with open(output_file, 'w', encoding='utf-8') as file:
+
+    with open(output_file, "w", encoding="utf-8") as file:
         yaml.dump(data, file, allow_unicode=True)
 
-translate_yaml('langs/strings/id.yml', 'langs/strings/en.yml')
+
+translate_yaml("langs/strings/id.yml", "langs/strings/en.yml")
