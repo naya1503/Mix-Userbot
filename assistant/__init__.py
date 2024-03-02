7bf6bbfd38860fa7c04e76f4016469306274610b
naya1503 <naya1503@users.noreply.github.com>
2024-03-02 11:26:48 +0000

    Auto Fix

--- assistant/__init__.py
+++ assistant/__init__.py
@@ -1,12 +1,15 @@
 def __list_all_modules():
-    from os.path import dirname, basename, isfile
     import glob
+    from os.path import basename, dirname, isfile
+
     mod_paths = glob.glob(dirname(__file__) + "/*.py")
-    all_modules = [basename(f)[:-3] for f in mod_paths if isfile(f)
-                   and f.endswith(".py")
-                   and not f.endswith('__init__.py')]
+    all_modules = [
+        basename(f)[:-3]
+        for f in mod_paths
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+    ]
     return all_modules
-    
-    
+
+
 BOT_PLUGINS = sorted(__list_all_modules())
 __all__ = BOT_PLUGINS + ["BOT_PLUGINS"]
