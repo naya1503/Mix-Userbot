11b4650d492ab9432762f649be4168c763f1211c
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-02 22:17:55 +0700

    update Mix/__main__.py

--- Mix/__main__.py
+++ Mix/__main__.py
@@ -15,6 +15,10 @@ async def test():
     LOGGER.info(f"Connecting to {ndB.name}...")
     if ndB.ping():
         LOGGER.info(f"Connected to {ndB.name} Successfully!")
+        if ndB.get_key("bahasa") is None:
+            ndB.set_key("bahasa", "id")
+        else:
+            return
     try:
         LOGGER.info(f"Starting Telegram Client...")
         await user.start()
