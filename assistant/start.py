51f9935d6c9c2bdcb9677effa145be437f08f039
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-03 22:16:15 +0700

    update assistant/start.py

--- assistant/start.py
+++ assistant/start.py
@@ -9,6 +9,32 @@
 from Mix import *
 
 
+def clbk_strt():
+    return okb(
+        [
+            [
+                (cgr("asst_3"), "clbk.bhsa"),
+            ],
+        ],
+        False,
+        "close_asst",
+    )
+    
+def clbk_strto():
+    return okb(
+        [
+            [
+                (cgr("asst_3"), "clbk.bhsa"),
+            ],
+        ],
+        False,
+        "close_asst",
+    )
+
 @ky.bots("start")
 async def _(c, m):
-    await m.reply("**Hai nyet!!**")
+    owner_nih = user.me.id
+    if m.from_user.id == owner_nih:
+        await m.reply(cgr("asst_1"), reply_markup=clbk_strt())
+    else:
+        await m.reply(cgr("asst_2"), reply_markup=clbk_strto())
