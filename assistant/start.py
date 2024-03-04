91c7eabaeecd0b4a268661fdd0f7f9372c2129c7
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-04 11:00:00 +0700

    update assistant/start.py

--- assistant/start.py
+++ assistant/start.py
@@ -7,17 +7,22 @@
 ################################################################
 
 
-
-from pyrogram import *
-from pyrogram.enums import *
-from pyrogram.types import *
-
 from Mix import *
+import asyncio
+import os
+import time
+from gc import get_objects
+from time import time
+
+
+from pyrogram.enums import *
+from pyrogram.errors import *
+from pyrogram.types import *
+from telegraph import upload_file
 
 COPY_ID = {}
 
 nyolong_jalan = False
-
 
 def clbk_strt():
     return okb(
@@ -78,3 +83,81 @@ async def _(c, m):
             await xx.edit(error)
     else:
         await xx.edit("Link tidak valid.")
+
+
+# @Tomi
+
+
+@ky.ubot("send", sudo=True)
+async def _(c: user, m):
+    if m.reply_to_message:
+        chat_id = m.chat.id if len(m.command) < 2 else m.text.split()[1]
+        try:
+            if c.me.id != bot.me.id:
+                if m.reply_to_message.reply_markup:
+                    x = await c.get_inline_bot_results(
+                        bot.me.username, f"_send_ {id(m)}"
+                    )
+                    return await c.send_inline_bot_result(
+                        chat_id, x.query_id, x.results[0].id
+                    )
+        except Exception as error:
+            return await m.reply(error)
+        else:
+            try:
+                return await m.reply_to_message.copy(chat_id)
+            except Exception as t:
+                return await m.reply(f"{t}")
+    else:
+        if len(m.command) < 3:
+            return
+        chat_id, chat_text = m.text.split(None, 2)[1:]
+        try:
+            if "/" in chat_id:
+                to_chat, msg_id = chat_id.split("/")
+                return await c.send_message(
+                    to_chat, chat_text, reply_to_message_id=int(msg_id)
+                )
+            else:
+                return await c.send_message(chat_id, chat_text)
+        except Exception as t:
+            return await m.reply(f"{t}")
+
+
+@ky.inline("^_send_")
+async def send_inline(c, iq):
+    try:
+        _id = int(iq.query.split()[1])
+        m = [obj for obj in get_objects() if id(obj) == _id][0]
+
+        if m.reply_to_message.photo:
+            m_d = await m.reply_to_message.download()
+            photo_tg = upload_file(m_d)
+            cp = m.reply_to_message.caption
+            text = cp if cp else ""
+            hasil = [
+                InlineQueryResultPhoto(
+                    photo_url=f"https://telegra.ph/{photo_tg[0]}",
+                    title="kon",
+                    reply_markup=m.reply_to_message.reply_markup,
+                    caption=text,
+                ),
+            ]
+            os.remove(m_d)
+        else:
+            hasil = [
+                InlineQueryResultArticle(
+                    title="kon",
+                    reply_markup=m.reply_to_message.reply_markup,
+                    input_message_content=InputTextMessageContent(
+                        m.reply_to_message.text
+                    ),
+                )
+            ]
+        await c.answer_inline_query(
+            iq.id,
+            cache_time=0,
+            results=hasil,
+        )
+    except Exception as e:
+        LOGGER.info(f"Error: {e}")
