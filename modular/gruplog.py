36066b129032f584a93b2f267f8c9cb6b88114c0
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-04 20:38:30 +0700

    update modular/gruplog.py

--- modular/gruplog.py
+++ modular/gruplog.py
@@ -154,8 +154,7 @@ async def _(c: user, m):
 
 
 @user.on_message(
-    filters.private & filters.incoming & ~filters.me & ~filters.bot & ~filters.service
-)
+    filters.private & filters.incoming)
 async def _(c: user, m):
     lg = ndB.get_key("TAG_LOG")
     if lg is None:
