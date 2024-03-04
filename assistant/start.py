3499904ba94619dfa9b78785967945a97f19dc00
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-04 08:45:44 +0700

    update assistant/start.py

--- assistant/start.py
+++ assistant/start.py
@@ -7,7 +7,7 @@
 ################################################################
 
 from Mix import *
-
+from pyrogram.types import *
 
 def clbk_strt():
     return okb(
@@ -25,7 +25,7 @@ def clbk_strto():
     return okb(
         [
             [
-                (cgr("asst_3"), "clbk.bhsa"),
+                (cgr("asst_3"), "clbk.info"),
             ],
         ],
         False,
@@ -35,6 +35,7 @@ def clbk_strto():
 
 @ky.bots("start")
 async def _(c, m):
+    udB.add_served_user(m.from_user.id)
     owner_nih = user.me.id
     if m.from_user.id == owner_nih:
         await m.reply(cgr("asst_1"), reply_markup=clbk_strt())
