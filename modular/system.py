638be01cbbc8232ecac1ff67422e5427b4cbb733
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-04 16:56:59 +0700

    update modular/system.py

--- modular/system.py
+++ modular/system.py
@@ -6,19 +6,6 @@
 """
 ################################################################
 
+from Mix import get_cgr
 __modles__ = "System"
-__help__ = """
- Help Command Developer
-
-• Perintah : <code>{0}update</code>
-• Penjelasan : Check Updater.
-
-• Perintah : <code>{0}reboot</code>
-• Penjelasan : Reboot userbot.
-
-• Perintah : <code>{0}host</code>
-• Penjelasan : System host.
-
-• Perintah : <code>{0}stats</code>
-• Penjelasan : System stats.
-"""
+__help__ = get_cgr("help_sys")
