fdd6650b195bad0fa0c942ad06db43c579774058
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-05 06:44:37 +0700

    update langs/__init__.py

--- langs/__init__.py
+++ langs/__init__.py
@@ -118,10 +118,6 @@ def get_bahasa_() -> Dict[str, Union[str, List[str]]]:
     for file in glob("langs/strings/*yml"):
         load(file)
     try:
-        return {
-            bahasa_["name"],
-            bahasa_["natively"],
-            bahasa_["authors"],
-        }
+        return bahasa_["name"]
     except KeyError as e:
         LOGGER.error(f"KeyError: {e} not found in language file")
