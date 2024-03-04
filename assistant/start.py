fe4dc674b7022b057657e5e80387c29c2baa27c7
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-04 10:58:57 +0700

    update assistant/start.py

--- assistant/start.py
+++ assistant/start.py
@@ -9,7 +9,19 @@
 from pyrogram.types import *
 
 from Mix import *
+import asyncio
+import os
+import time
+from gc import get_objects
+from time import time
 
+from pyrogram import *
+from pyrogram.enums import *
+from pyrogram.types import *
+
+COPY_ID = {}
+
+nyolong_jalan = False
 
 def clbk_strt():
     return okb(
@@ -46,3 +58,27 @@ async def _(c, m):
         await m.reply(ts_1, reply_markup=clbk_strt())
     else:
         await m.reply(ts_2, reply_markup=clbk_strto())
+
+
+@ky.bots("copy")
+async def _(c, m):
+    if m.from_user.id != user.me.id:
+        return
+    xx = await m.reply("Tunggu Sebentar...")
+    link = user.get_arg(m)
+    if not link:
+        return await xx.edit(f"<b><code>{m.text}</code> [link]</b>")
+    if link.startswith(("https", "t.me")):
+        msg_id = int(link.split("/")[-1])
+        if "t.me/c/" in link:
+            chat = int("-100" + str(link.split("/")[-2]))
+        else:
+            chat = str(link.split("/")[-2])
+        try:
+            g = await c.get_messages(chat, msg_id)
+            await g.copy(m.chat.id)
+            await xx.delete()
+        except Exception as error:
+            await xx.edit(error)
+    else:
+        await xx.edit("Link tidak valid.")
