662c996ad5a962afc523a6a8d9c1f089dba93f8e
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-04 11:01:25 +0700

    update assistant/start.py

--- assistant/start.py
+++ assistant/start.py
@@ -7,20 +7,37 @@
 ################################################################
 
 
+from Mix import *
+import asyncio
 import os
+import time
 from gc import get_objects
+from time import time
+
 
 from pyrogram.enums import *
 from pyrogram.errors import *
 from pyrogram.types import *
 from telegraph import upload_file
 
-from Mix import *
-
 COPY_ID = {}
 
 nyolong_jalan = False
 
+import os
+
+from pyrogram import *
+from pyrogram.errors import *
+from pyrogram.file_id import *
+from pyrogram.raw.functions.messages import *
+from pyrogram.raw.functions.stickers import *
+from pyrogram.raw.types import *
+
+from Mix import Emojik, bot, ky, ndB, user
+from Mix.core.http import http
+from Mix.core.stick_tools import EMOJI_PATTERN, convert_video, resize_image
+
+LOG_ME = ndB.get_key("TAG_LOG")
 
 def clbk_strt():
     return okb(
