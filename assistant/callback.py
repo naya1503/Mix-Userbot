df3ad8caede7e9bc851c606cc175ac630b71c542
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-04 10:33:21 +0700

    update assistant/callback.py

--- assistant/callback.py
+++ assistant/callback.py
@@ -6,6 +6,8 @@
 """
 ################################################################
 
+from Mix import *
+from pyrogram.types import *
 
 @ky.callback("clbk.")
 async def _(c, cq):
@@ -13,5 +15,29 @@ async def _(c, cq):
     kb = okb([[("Kembali", "clbk.bek")]])
     ck_bhs = udB.get_key("bahasa")
     bhs = get_bahasa_()
+    buttons = [
+        [
+            InlineKeyboardButton(
+                f"{bhs[lang]['penulis']} [{lang.lower()}]",
+                callback_data=f"set_{lang}"
+            )
+        ]
+        for lang in bhs
+    ]
+    buttons.append([InlineKeyboardButton(cgr("balik"), callback_data="clbk.bek")])
     if cmd == "bhsa":
-        teks = cgr("asst_4")
+        teks = cgr("asst_4").format(bhs['nama'])
+        await cq.edit_message_text(text=teks, reply_markup=buttons)
+        
+        
+@ky.callback("^set_(.*)$")
+async def _(c, cq):
+    lang = query.matches[0].group(1)
+    bhs = get_bahasa_()
+    kb = okb([[(cgr("balik"), "clbk.bek")]])
+    if lang == "en":
+        ndB.del_key("bahasa")
+    else:
+        ndB.set_key("bahasa", lang)
+    await cq.edit_message_text(cgr("asst_5").format(bhs[lang]['penulis'][lang]), reply_markup=kb)
+       
