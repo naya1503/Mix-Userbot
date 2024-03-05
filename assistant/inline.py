cab2dae6c7412e430448debea041d5e470633be2
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-05 15:31:05 +0700

    update assistant/inline.py

--- assistant/inline.py
+++ assistant/inline.py
@@ -11,12 +11,16 @@ from gc import get_objects
 from pyrogram import *
 from pyrogram.enums import *
 from pyrogram.errors import *
-from pyrogram.file_id import *
-from pyrogram.raw.functions.messages import *
-from pyrogram.raw.functions.stickers import *
-from pyrogram.raw.types import *
 from pyrogram.types import *
 from telegraph import upload_file
+
+from datetime import datetime
+from time import time
+
+from pyrogram.raw.functions import Ping
+from Mix.core.waktu import get_time, start_time
+
+from modular.gcast import refresh_dialog
 
 from Mix import *
 from modular.copy_con import *
@@ -163,3 +167,68 @@ async def send_inline(c, iq):
         )
     except Exception as e:
         LOGGER.info(f"Error: {e}")
+
+
+# alive
+@ky.inline("^alive")
+async def _(c, iq):
+    pmper = None
+    stutas = None
+    start = datetime.now()
+    await user.invoke(Ping(ping_id=0))
+    pink = (datetime.now() - start).microseconds / 1000
+    upnya = await get_time((time() - start_time))
+    ape = await refresh_dialog("group")
+    apa = await refresh_dialog("users")
+    if user.me.id in DEVS:
+        stutas = "<b>Author</b>"
+    else:
+        stutas = "<b>Connoisseur</b>"
+    cekpr = udB.get_var(user.me.id, "PMPERMIT")
+    if cekpr:
+        pmper = "enable"
+    else:
+        pmper = "disable"
+    txt = f"""
+<b>Mix-Userbot</b>
+    <b>status:</b> {stutas}
+      <b>dc_id:</b> {user.me.dc_id}
+      <b>ping_dc:</b> {str(pink).replace('.', ',')} ms
+      <b>pmpermit:</b> {pmper}
+      <b>peer_users:</b> {len(apa)}
+      <b>peer_groups:</b> {len(ape)}
+      <b>bot_uptime:</b> {upnya}
+"""
+    bo_ol = [[InlineKeyboardButton(text="Support", url="t.me/kynansupport")]]
+    cekpic = udB.get_var(user.me.id, "ALIVEPIC")
+    if not cekpic:
+        duar = [
+            (
+                InlineQueryResultArticle(
+                    title="Alive Teks",
+                    input_message_content=InputTextMessageContent(txt),
+                    reply_markup=InlineKeyboardMarkup(bo_ol),
+                )
+            )
+        ]
+
+    else:
+        filem = (
+            InlineQueryResultVideo
+            if cekpic.endswith(".mp4")
+            else InlineQueryResultPhoto
+        )
+        url_ling = (
+            {"video_url": cekpic, "thumb_url": cekpic}
+            if cekpic.endswith(".mp4")
+            else {"photo_url": cekpic}
+        )
+        duar = [
+            filem(
+                **url_ling,
+                title="Alive Picture",
+                caption=txt,
+                reply_markup=InlineKeyboardMarkup(bo_ol),
+            )
+        ]
+    await c.answer_inline_query(iq.id, cache_time=0, results=duar)
