19d183fce6d5324038a1bc27708f3cc142371561
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-05 15:26:58 +0700

    update modular/gcast.py

--- modular/gcast.py
+++ modular/gcast.py
@@ -187,3 +187,39 @@ async def _(c: user, m):
     for x in get_bls:
         udB.remove_chat(c.me.id, x)
     await msg.edit(cgr("gcs_14").format(em.sukses))
+
+
+@ky.ubot("send", sudo=True)
+async def _(c: user, m):
+    if m.reply_to_message:
+        chat_id = m.chat.id if len(m.command) < 2 else m.text.split()[1]
+        try:
+            if c.me.id != bot.me.id:
+                if m.reply_to_message.reply_markup:
+                    x = await c.get_inline_bot_results(
+                        bot.me.username, f"_send_ {id(m)}"
+                    )
+                    return await c.send_inline_bot_result(
+                        chat_id, x.query_id, x.results[0].id
+                    )
+        except Exception as error:
+            return await m.reply(error)
+        else:
+            try:
+                return await m.reply_to_message.copy(chat_id)
+            except Exception as t:
+                return await m.reply(f"{t}")
+    else:
+        if len(m.command) < 3:
+            return
+        chat_id, chat_text = m.text.split(None, 2)[1:]
+        try:
+            if "/" in chat_id:
+                to_chat, msg_id = chat_id.split("/")
+                return await c.send_message(
+                    to_chat, chat_text, reply_to_message_id=int(msg_id)
+                )
+            else:
+                return await c.send_message(chat_id, chat_text)
+        except Exception as t:
+            return await m.reply(f"{t}")
