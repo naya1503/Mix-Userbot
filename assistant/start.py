1250aee66580b8fc6120ca9ec0613e4906da83b6
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-05 16:34:49 +0700

    update assistant/start.py

--- assistant/start.py
+++ assistant/start.py
@@ -47,7 +47,7 @@ def clbk_strto():
     )
 
 
-@ky.bots("start", filters.private)
+@ky.bots("start", human.pv)
 async def _(c, m):
     udB.add_served_user(m.from_user.id)
     owner_nih = user.me.id
