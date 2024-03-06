4cf146582254323d3e7fdf4a78ece3b730a1266e
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-06 09:04:03 +0700

    update assistant/call_markdown.py

--- assistant/call_markdown.py
+++ assistant/call_markdown.py
@@ -33,38 +33,10 @@ async def _(c, cq):
     cmd = cq.data.split(".")[1]
     kb = okb([[("Kembali", "bace.markd")]])
     if cmd == "butformat":
-        nan = """<b>Markdown Formatting</b>
-<b>Anda dapat memformat pesan Anda menggunakan <b>tebal</b>, <i>miring</i>, <u>garis bawah</u>, <strike>coret</strike>, dan banyak lagi.</b>
-
-<code>`kata kode`</code>: Tanda kutip terbalik digunakan untuk font monospace. Ditampilkan sebagai: <code>kata kode</code>.
-<code>__miring__</code>: Garis bawah digunakan untuk font miring. Ditampilkan sebagai: <i>kata miring</i>.
-<code>**tebal**</code>: Asterisk digunakan untuk font tebal. Ditampilkan sebagai: <b>kata tebal</b>.
-<code>--garis bawah--</code>: Untuk membuat teks <u>garis bawah</u>.
-<code>~~coret~~</code>: Tilda digunakan untuk strikethrough. Ditampilkan sebagai: <strike>coret</strike>.
-<code>||spoiler||</code>: Garis vertikal ganda digunakan untuk spoiler. Ditampilkan sebagai: <spoiler>Spoiler</spoiler>.
-<code>[hyperlink](contoh)</code>: Ini adalah pemformatan yang digunakan untuk hyperlink. Ditampilkan sebagai: <a href="https://example.com/">hyperlink</a>.
-<code>[Tombol Saya](buttonurl://contoh)</code>: Ini adalah pemformatan yang digunakan untuk membuat tombol.
-Jika Anda ingin mengirimkan tombol dalam satu baris yang sama, gunakan pemformatan <code>:same</code>.
-<b>Contoh:</b>
-<code>[tombol 1](buttonurl://1)</code>
-<code>[tombol 2](buttonurl://2:same)</code>
-<code>[tombol 3](buttonurl://3)</code>
-Ini akan menampilkan tombol 1 dan 2 di baris yang sama, sementara 3 akan berada di bawahnya."""
+        nan = cgr("mark_1")
         await cq.edit_message_text(text=nan, reply_markup=kb, parse_mode=ParseMode.HTML)
     elif cmd == "filing":
-        nen = """<b>Fillings</b>
-
-Anda juga dapat menyesuaikan isi pesan Anda dengan data kontekstual. Misalnya, Anda bisa menyebut nama pengguna dalam pesan selamat datang, atau menyebutnya dalam filter!
-
-<b>Isian yang didukung:</b>
-
-<code>{first}</code>: Nama depan pengguna.
-<code>{last}</code>: Nama belakang pengguna.
-<code>{fullname}</code>: Nama lengkap pengguna.
-<code>{username}</code>: Nama pengguna pengguna. Jika mereka tidak memiliki satu, akan menyebutkan pengguna tersebut.
-<code>{mention}</code>: Menyebutkan pengguna dengan nama depan mereka.
-<code>{id}</code>: ID pengguna.
-<code>{chatname}</code>: Nama obrolan."""
+        nen = cgr("mark_2")
         await cq.edit_message_text(
             text=nen,
             reply_markup=kb,
@@ -74,5 +46,5 @@ Anda juga dapat menyesuaikan isi pesan Anda dengan data kontekstual. Misalnya, A
 
 @ky.callback("bace")
 async def _(c, cq):
-    txt = "<b>Untuk melihat format markdown silahkan klik tombol dibawah.</b>"
+    txt = cgr("mark_3")
     await cq.edit_message_text(text=txt, reply_markup=markdown_help())
