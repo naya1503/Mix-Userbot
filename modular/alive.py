c64f18a2c5fd4d3e87d1a8655c253c78c3b8a4cc
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-07 02:20:12 +0700

    alive

--- modular/alive.py
+++ modular/alive.py
@@ -26,7 +26,7 @@ __help__ = """
 """
 
 
-"""
+
 @ky.ubot("alive", sudo=True)
 async def _(c: user, m):
     try:
@@ -36,71 +36,6 @@ async def _(c: user, m):
         await m.reply(error)
 
 
-@ky.inline("^alive")
-async def _(c, iq):
-    pmper = None
-    stutas = None
-    start = datetime.now()
-    await user.invoke(Ping(ping_id=0))
-    pink = (datetime.now() - start).microseconds / 1000
-    upnya = await get_time((time() - start_time))
-    ape = await refresh_dialog("group")
-    apa = await refresh_dialog("users")
-    if user.me.id in DEVS:
-        stutas = "<b>Author</b>"
-    else:
-        stutas = "<b>Connoisseur</b>"
-    cekpr = udB.get_var(user.me.id, "PMPERMIT")
-    if cekpr:
-        pmper = "enable"
-    else:
-        pmper = "disable"
-    txt = f"""
-"""
-<b>Mix-Userbot</b>
-    <b>status:</b> {stutas}
-      <b>dc_id:</b> {user.me.dc_id}
-      <b>ping_dc:</b> {str(pink).replace('.', ',')} ms
-      <b>pmpermit:</b> {pmper}
-      <b>peer_users:</b> {len(apa)}
-      <b>peer_groups:</b> {len(ape)}
-      <b>bot_uptime:</b> {upnya}
-"""
-"""
-    bo_ol = [[InlineKeyboardButton(text="Support", url="t.me/kynansupport")]]
-    cekpic = udB.get_var(user.me.id, "ALIVEPIC")
-    if not cekpic:
-        duar = [
-            (
-                InlineQueryResultArticle(
-                    title="Alive Teks",
-                    input_message_content=InputTextMessageContent(txt),
-                    reply_markup=InlineKeyboardMarkup(bo_ol),
-                )
-            )
-        ]
-
-    else:
-        filem = (
-            InlineQueryResultVideo
-            if cekpic.endswith(".mp4")
-            else InlineQueryResultPhoto
-        )
-        url_ling = (
-            {"video_url": cekpic, "thumb_url": cekpic}
-            if cekpic.endswith(".mp4")
-            else {"photo_url": cekpic}
-        )
-        duar = [
-            filem(
-                **url_ling,
-                title="Alive Picture",
-                caption=txt,
-                reply_markup=InlineKeyboardMarkup(bo_ol),
-            )
-        ]
-    await c.answer_inline_query(iq.id, cache_time=0, results=duar)
-"""
 
 
 @ky.inline("^alive")
