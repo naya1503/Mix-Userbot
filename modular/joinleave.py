e5e9381dd5a0994d45abb3e3807602cb3d2be3c2
NotLumiere <118038685+NotLumiere@users.noreply.github.com>
2024-03-07 23:08:58 +0700

    Update joinleave.py

--- modular/joinleave.py
+++ modular/joinleave.py
@@ -17,9 +17,9 @@ async def _(c, m):
     em = Emojik()
     em.initialize()
     Nan = m.command[1] if len(m.command) > 1 else m.chat.id
-    ceger = await m.reply_text(f"{em.proses} <code>Processing...<\code>")
+    ceger = await m.reply_text(f"{em.proses} <code>Processing...</code>")
     try:
-        await ceger.edit(f"{em.sukses} <b>Berhasil Bergabung ke Chat ID<\b> {Nan}")
+        await ceger.edit(f"{em.sukses} <b>Berhasil Bergabung ke Chat ID</b> {Nan}")
         await c.join_chat(Nan)
     except Exception as ex:
         await ceger.edit(f"ERROR: \n\n{str(ex)}")
