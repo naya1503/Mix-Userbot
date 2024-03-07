62c36a28ec0055d981359ba83d9249c188498921
NotLumiere <118038685+NotLumiere@users.noreply.github.com>
2024-03-07 23:10:13 +0700

    Update joinleave.py

--- modular/joinleave.py
+++ modular/joinleave.py
@@ -19,7 +19,7 @@ async def _(c, m):
     Nan = m.command[1] if len(m.command) > 1 else m.chat.id
     ceger = await m.reply_text(f"{em.proses} <code>Processing...</code>")
     try:
-        await ceger.edit(f"{em.sukses} <b>Berhasil Bergabung ke Chat ID</b> {Nan}")
+        await ceger.edit(f"{em.sukses} <b>Berhasil Bergabung ke</b> {Nan}")
         await c.join_chat(Nan)
     except Exception as ex:
         await ceger.edit(f"ERROR: \n\n{str(ex)}")
