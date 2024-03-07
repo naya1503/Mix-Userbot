c3929acf81813d4b7fc9e5029eada619f726fbf9
NotLumiere <118038685+NotLumiere@users.noreply.github.com>
2024-03-07 21:30:18 +0700

    adding modules

--- modular/invite.py
+++ modular/invite.py
@@ -4,14 +4,33 @@ from pyrogram.types import *
 
 from Mix import *
 
-__modles__ = "Getlink"
+__modles__ = "Invite"
 __help__ = """
-Help Command Info 
+Help Command Invite 
+
+• Perintah: <code>{0}invite</code>
+• Penjelasan: Untuk mengundang seseorang ke group/channel.
 
 • Perintah: <code>{0}getlink</code>
 • Penjelasan: Untuk mendapatkan link invite chat.
 """
 
+@ky.ubot("invite|undang", sudo=True)
+async def _(c, m):
+    em = Emojik()
+    em.initialize()
+    mg = await reply_text(f"{em.proses}<code>Adding Users!</code>")
+    user_s_to_add = m.text.split(" ", 1)[1]
+    if not user_s_to_add:
+        await mg.edit("{em.gagal}Give me User's Id/Username")
+        return
+    user_list = user_s_to_add.split(" ")
+    try:
+        await c.add_chat_members(message.chat.id, user_list, forward_limit=100)
+    except BaseException as e:
+        await mg.edit(f"{em.gagal}Unable To Add Users! \nTraceBack : {e}")
+        return
+    await mg.edit(f"{em.sukses}Sucessfully Added {len(user_list)} To {mg.chat.title}")
 
 @ky.ubot("getlink|invitelink", sudo=True)
 @ky.devs("getling")
