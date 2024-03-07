e41eaec22434f8cfd6600d9e3291dd48dfa69b71
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-07 20:15:21 +0700

    update modular/uemoji.py

--- modular/uemoji.py
+++ modular/uemoji.py
@@ -62,11 +62,9 @@ async def _(c: user, m):
     em = Emojik()
     em.initialize()
     gua = c.me.is_premium
-    jing = await m.reply(f"{em.proses} <b>Processing...</b>")
+    jing = await m.reply(cgr("proses").format(em.proses))
     if len(m.command) < 3:
-        return await jing.edit(
-            f"{em.gagal} <b>Gunakan Format : <code>emoji variable value</code>.</b>"
-        )
+        return await jing.edit(cgr("em_5").format(em.gagal, m.command))
     command, variable, value = m.command[:3]
     emoji_id = None
     if variable.lower() == "ping":
@@ -78,12 +76,12 @@ async def _(c: user, m):
                         break
                 if emoji_id:
                     udB.set_var(c.me.id, "emo_ping", emoji_id)
-                    await jing.edit(
-                        f"{em.sukses} <b>Emoji ping diset ke :</b> <emoji id={emoji_id}>{value}</emoji>"
-                    )
+                    await jing.edit(cgr("em_6").format(em.sukses, emoji_id, value))
+                    return
         elif gua == False:
             udB.set_var(c.me.id, "emo_ping", value)
-            await jing.edit(f"{em.sukses} <b>Emoji ping diset ke :</b> {value}")
+            await jing.edit(cgr("em_7").format(em.sukses, value))
+            return
     elif variable.lower() == "pong":
         if gua == True:
             if m.entities:
@@ -93,12 +91,12 @@ async def _(c: user, m):
                         break
                 if emoji_id:
                     udB.set_var(c.me.id, "emo_pong", emoji_id)
-                    await jing.edit(
-                        f"{em.sukses} <b>Emoji pong diset ke :</b> <emoji id={emoji_id}>{value}</emoji>"
-                    )
+                    await jing.edit(cgr("em_8").format(em.sukses, emoji_id, value))
+                    return
         elif gua == False:
             udB.set_var(c.me.id, "emo_pong", value)
-            await jing.edit(f"{em.sukses} <b>Emoji pong diset ke :</b> {value}")
+            await jing.edit(cgr("em_9").format(em.sukses, value))
+            return
     elif variable.lower() == "proses":
         if gua == True:
             if m.entities:
@@ -108,12 +106,12 @@ async def _(c: user, m):
                         break
                 if emoji_id:
                     udB.set_var(c.me.id, "emo_proses", emoji_id)
-                    await jing.edit(
-                        f"{em.sukses} <b>Emoji proses diset ke :</b> <emoji id={emoji_id}>{value}</emoji>"
-                    )
+                    await jing.edit(cgr("em_10").format(em.sukses, emoji_id, value))
+                    return
         elif gua == False:
             udB.set_var(c.me.id, "emo_proses", value)
-            await jing.edit(f"{em.sukses} <b>Emoji proses diset ke :</b> {value}")
+            await jing.edit(cgr("em_11").format(em.sukses, value))
+            return
     elif variable.lower() == "gagal":
         if gua == True:
             if m.entities:
@@ -123,12 +121,12 @@ async def _(c: user, m):
                         break
                 if emoji_id:
                     udB.set_var(c.me.id, "emo_gagal", emoji_id)
-                    await jing.edit(
-                        f"{em.sukses} <b>Emoji gagal diset ke :</b> <emoji id={emoji_id}>{value}</emoji>"
-                    )
+                    await jing.edit(cgr("em_12").format(em.sukses, emoji_id, value))
+                    return
         elif gua == False:
             udB.set_var(c.me.id, "emo_gagal", value)
-            await jing.edit(f"{em.sukses} <b>Emoji gagal diset ke :</b> {value}")
+            await jing.edit(cgr("em_13").format(em.sukses, value))
+            return
     elif variable.lower() == "sukses":
         if gua == True:
             if m.entities:
@@ -138,12 +136,11 @@ async def _(c: user, m):
                         break
                 if emoji_id:
                     udB.set_var(c.me.id, "emo_sukses", emoji_id)
-                    await jing.edit(
-                        f"{em.sukses} <b>Emoji sukses diset ke :</b> <emoji id={emoji_id}>{value}</emoji>"
-                    )
+                    await jing.edit(cgr("em_14").format(em.sukses, emoji_id, value))
         elif gua == False:
             udB.set_var(c.me.id, "emo_sukses", value)
-            await jing.edit(f"{em.sukses} <b>Emoji sukses diset ke :</b> {value}")
+            await jing.edit(cgr("em_15").format(em.sukses, value))
+            return
     elif variable.lower() == "profil":
         if gua == True:
             if m.entities:
@@ -153,12 +150,12 @@ async def _(c: user, m):
                         break
                 if emoji_id:
                     udB.set_var(c.me.id, "emo_profil", emoji_id)
-                    await jing.edit(
-                        f"{em.sukses} <b>Emoji profil diset ke :</b> <emoji id={emoji_id}>{value}</emoji>"
-                    )
+                    await jing.edit(cgr("em_16").format(em.sukses, emoji_id, value))
+                    return
         elif gua == False:
             udB.set_var(c.me.id, "emo_profil", value)
-            await jing.edit(f"{em.sukses} <b>Emoji profil diset ke :</b> {value}")
+            await jing.edit(cgr("em_17").format(em.sukses, value))
+            return
     elif variable.lower() == "alive":
         if gua == True:
             if m.entities:
@@ -168,13 +165,12 @@ async def _(c: user, m):
                         break
                 if emoji_id:
                     udB.set_var(c.me.id, "emo_alive", emoji_id)
-                    await jing.edit(
-                        f"{em.sukses} <b>Emoji alive diset ke :</b> <emoji id={emoji_id}>{value}</emoji>"
-                    )
-
+                    await jing.edit(cgr("em_18").format(em.sukses, emoji_id, value))
+                    return
         elif gua == False:
             udB.set_var(c.me.id, "emo_alive", value)
-            await jing.edit(f"{em.sukses} <b>Emoji alive diset ke :</b> {value}")
+            await jing.edit(cgr("em_19").format(em.sukses, value))
+            return
     elif variable.lower() == "warn":
         if gua == True:
             if m.entities:
@@ -184,13 +180,12 @@ async def _(c: user, m):
                         break
                 if emoji_id:
                     udB.set_var(c.me.id, "emo_warn", emoji_id)
-                    await jing.edit(
-                        f"{em.sukses} <b>Emoji alive diset ke :</b> <emoji id={emoji_id}>{value}</emoji>"
-                    )
-
+                    await jing.edit(cgr("em_20").format(em.sukses, emoji_id, value))
+                    return
         elif gua == False:
             udB.set_var(c.me.id, "emo_warn", value)
-            await jing.edit(f"{em.sukses} <b>Emoji warn diset ke :</b> {value}")
+            await jing.edit(cgr("em_21").format(em.sukses, value))
+            return
     elif variable.lower() == "block":
         if gua == True:
             if m.entities:
@@ -200,25 +195,21 @@ async def _(c: user, m):
                         break
                 if emoji_id:
                     udB.set_var(c.me.id, "emo_block", emoji_id)
-                    await jing.edit(
-                        f"{em.sukses} <b>Emoji block diset ke :</b> <emoji id={emoji_id}>{value}</emoji>"
-                    )
-
+                    await jing.edit(cgr("em_22").format(em.sukses, emoji_id, value))
+                    return
         elif gua == False:
             udB.set_var(c.me.id, "emo_block", value)
-            await jing.edit(f"{em.sukses} <b>Emoji block diset ke :</b> {value}")
+            await jing.edit(cgr("em_23").format(em.sukses, value))
+            return
 
     else:
-        await jing.edit(
-            f"{em.gagal} <b>Silakan ketik <code>help {m.command}<code>.</b>"
-        )
+        await jing.edit(cgr("em_5").format(em.gagal, m.command))
+        return
 
 
 @ky.ubot("getemo", sudo=True)
 async def _(c: user, m):
     em = Emojik()
     em.initialize()
-    xx = await m.reply(f"{em.proses} <b>Processing...</b>")
-    await xx.edit(
-        f"{em.sukses}<b>๏ Emoji Yang Digunakan :</b>\n\n Ping : {em.ping}\n Pong : {em.pong}\n Proses : {em.proses}\n Sukses : {em.sukses}\n Gagal : {em.gagal}\n Profil : {em.profil}\n Alive : {em.alive}\n Warning : {em.warn}\n Block : {em.block}"
-    )
+    xx = await m.reply(cgr("proses").format(em.proses))
+    await xx.edit(cgr("em_24").format(em.sukses, em.ping, em.pong, em.proses, em.sukses, em.gagal, em.profil, em.alive, em.warn, em.block))
