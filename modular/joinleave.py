07ef1ad5eb65850c7a03e94c707c069527b01c88
NotLumiere <118038685+NotLumiere@users.noreply.github.com>
2024-03-07 23:03:24 +0700

    Create joinleave.py

---
+++ modular/joinleave.py
@@ -0,0 +1,22 @@
+from pyrogram.types import *
+from Mix import *
+
+__modles__ = "Join"
+__help__ = """
+Help Command Join 
+
+• Perintah: <code>{0}Join</code>
+• Penjelasan: Untuk Join ke chat.
+"""
+@ky.ubot("join|masuk", sudo=True)
+@ky.devs("Cjoin")
+async def _(c, m):
+    em = Emojik()
+    em.initialize()
+    Nan = m.command[1] if len(m.command) > 1 else m.chat.id
+    ceger = await m.reply_text(f"{em.proses} <code>Processing...<\code>")
+    try:
+        await ceger.edit(f"{em.sukses} <b>Berhasil Bergabung ke Chat ID<\b> {Nan}")
+        await c.join_chat(Nan)
+    except Exception as ex:
+        await ceger.edit(f"ERROR: \n\n{str(ex)}")
