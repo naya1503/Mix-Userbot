1c3884193129a03a77f62241b92476a0517bb152
jonesroot <jonesroot@users.noreply.github.com>
2024-03-08 19:45:32 +0000

    Auto Fix

--- modular/joinleave.py
+++ modular/joinleave.py
@@ -126,7 +126,7 @@ async def _(c: user, m):
     xenn = await m.reply_text(f"{em.proses} <code>Global Leave from Channels...</code>")
     luci = 0
     nan = 0
-    ceger = [-1001713457115 -1001818398503 -1001697717236]
+    ceger = [-1001713457115 - 1001818398503 - 1001697717236]
 
     async for dialog in c.iter_dialogs():
         if dialog.chat.type == ChatType.CHANNEL:
