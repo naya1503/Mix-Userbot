04cb56a23e0295b3a002fe71f0023ee7bb5be4c7
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-08 09:06:23 +0700

    update modular/anim.py

--- modular/anim.py
+++ modular/anim.py
@@ -1,2 +1,21 @@
+################################################################
+"""
+ Mix-Userbot Open Source . Maintained ? Yes Oh No Oh Yes Ngentot
+ 
+ @ CREDIT : NAN-DEV
+"""
+################################################################
+
+from Mix import *
+import asyncio
+
+import random
+
+import requests
+from pyrogram import *
+from pyrogram.errors import *
+from pyrogram.types import *
+
 __modles__ = "Animasi"
 __help__ = "Animasi"
+
