b1a59667944e93575589aa7d6a53276c6b8fc708
iamuput <yogaaasp73@gmail.com>
2024-03-09 23:36:52 +0700

    Update toxic.py

--- modular/toxic.py
+++ modular/toxic.py
@@ -8,117 +8,117 @@ __help__ = "Toxic"
 
 @ky.ubot("d", sudo=True)
 async def _(c: user, m):
-    await m.reply("**BACOT NGENTOTTT!!BAPA LU SURUH RIBUT SAMA GUA**")
+    await m.edit("**BACOT NGENTOTTT!!BAPA LU SURUH RIBUT SAMA GUA**")
 
 
 @ky.ubot("e", sudo=True)
 async def _(c: user, m):
-    ea = await m.reply(
+    await m.edit(
         "**GAK USAH SOK KERAS GOBLOK!!KENCING MASIH BERDIRI AJA BELAGU**"
     )
 
 
 @ky.ubot("f", sudo=True)
 async def _(c: user, m):
-    await m.reply("**MUKA LU SEMUA KAYA KONTOL HAHAHAHA!!**")
+    await m.edit("**MUKA LU SEMUA KAYA KONTOL HAHAHAHA!!**")
 
 
 @ky.ubot("i", sudo=True)
 async def _(c: user, m):
-    await m.reply("**KONTOL MASIH BENGKOK AJA BANGGA LU HAHAHAHA!!**")
+    await m.edit("**KONTOL MASIH BENGKOK AJA BANGGA LU HAHAHAHA!!**")
 
 
 @ky.ubot("r", sudo=True)
 async def _(c: user, m):
-    await m.reply("**KONTOL KONTOL APA YANG BESAR?KONTOL LU LAH HAHAHAHA!!**")
+    await m.edit("**KONTOL KONTOL APA YANG BESAR?KONTOL LU LAH HAHAHAHA!!**")
 
 
 @ky.ubot("t", sudo=True)
 async def _(c: user, m):
-    await m.reply("**BABI!!KONTOL!!NGENTOT!!!**")
+    await m.edit("**BABI!!KONTOL!!NGENTOT!!!**")
 
 
 @ky.ubot("u", sudo=True)
 async def _(c: user, m):
-    await m.reply("**BABI LU GOBLOK!!GANTENGAN JUGA GUA HAHAHAHA**")
+    await m.edit("**BABI LU GOBLOK!!GANTENGAN JUGA GUA HAHAHAHA**")
 
 
 @ky.ubot("w", sudo=True)
 async def _(c: user, m):
-    await m.reply("**BABI LU GOBLOK!!CANTIKAN JUGA GUA HAHAHAHA**")
+    await m.edit("**BABI LU GOBLOK!!CANTIKAN JUGA GUA HAHAHAHA**")
 
 
 @ky.ubot("bct", sudo=True)
 async def _(c: user, m):
-    await m.reply("**BACOTAN LU GAK BIKIN GUA TREMOR GOBLOK HAHAHAHA!!**")
+    await m.edit("**BACOTAN LU GAK BIKIN GUA TREMOR GOBLOK HAHAHAHA!!**")
 
 
 @ky.ubot("n", sudo=True)
 async def _(c: user, m):
-    await m.reply("**GIMANA KABAR KAMU SAYANG??APAKAH BAIK?**")
+    await m.edit("**GIMANA KABAR KAMU SAYANG??APAKAH BAIK?**")
 
 
 @ky.ubot("b", sudo=True)
 async def _(c: user, m):
-    await m.reply("**ELEEEHHHH SOK BANGET KEPINTERAN KAMU!!**")
+    await m.edit("**ELEEEHHHH SOK BANGET KEPINTERAN KAMU!!**")
 
 
 @ky.ubot("m", sudo=True)
 async def _(c: user, m):
-    await m.reply("**INI GRUB APA KUBURAN SEPI BANGET ASTAGFIRULLAH!!**")
+    await m.edit("**INI GRUB APA KUBURAN SEPI BANGET ASTAGFIRULLAH!!**")
 
 
 @ky.ubot("c", sudo=True)
 async def _(c: user, m):
-    await m.reply("**KAN UDAH GUA BILANG??MAKANYA JANGAN NGEYEL!!**")
+    await m.edit("**KAN UDAH GUA BILANG??MAKANYA JANGAN NGEYEL!!**")
 
 
 @ky.ubot("x", sudo=True)
 async def _(c: user, m):
-    await m.reply("**GAUSAH SOKAP DEH KAMU!!**")
+    await m.edit("**GAUSAH SOKAP DEH KAMU!!**")
 
 
 @ky.ubot("y", sudo=True)
 async def _(c: user, m):
-    await m.reply("**MACAM BAGUS AE LU BEGITU HMM!!**")
+    await m.edit("**MACAM BAGUS AE LU BEGITU HMM!!**")
 
 
 @ky.ubot("j", sudo=True)
 async def _(c: user, m):
-    await m.reply("**MAAF BUKAN JAGOAN HAHAHAHA!!**")
+    await m.edit("**MAAF BUKAN JAGOAN HAHAHAHA!!**")
 
 
 @ky.ubot("z", sudo=True)
 async def _(c: user, m):
-    await m.reply("**BISMILLAH SLEEP CALL!!😁**")
+    await m.edit("**BISMILLAH SLEEP CALL!!😁**")
 
 
 @ky.ubot("g", sudo=True)
 async def _(c: user, m):
-    await m.reply("**GANTENG LU BEGITU???**")
+    await m.edit("**GANTENG LU BEGITU???**")
 
 
 @ky.ubot("yy", sudo=True)
 async def _(c: user, m):
-    ea = await m.reply(
+    await m.edit(
         "**War War Tai anjing, Ketrigger minta sharelok, Udah di sharelok Ga nyamperin,Keras di sosmed Bhakss...**"
     )
 
 
 @ky.ubot("h", sudo=True)
 async def _(c: user, m):
-    await m.reply("**CANTIK LU BEGITU???**")
+    await m.edit("**CANTIK LU BEGITU???**")
 
 
 @ky.ubot("o", sudo=True)
 async def _(c: user, m):
-    ea = await m.reply(
+    await m.edit(
         "**MENTANG MENTANG PUNYA BOT MAINNYA BOT!!PANTES MUKANYA KAYA BOT😁**"
     )
 
 
 @ky.ubot("war", sudo=True)
 async def _(c: user, m):
-    ea = await m.reply(
+    await m.edit(
         "**NORAK LU KONTOL!! MAKE USERBOT CUMAN BUAT WAR MALAH SOK KERAS**"
     )
