2b09881d8d25a3b84f59a9e56099caade0378eb6
iamuput <iamuput@users.noreply.github.com>
2024-03-09 16:24:42 +0000

    Auto Fix

--- modular/toxic.py
+++ modular/toxic.py
@@ -2,115 +2,123 @@ from pyrogram import *
 
 from Mix import *
 
-
 __modles__ = "Toxic"
 __help__ = "Toxic"
 
 
 @ky.ubot("d", sudo=True)
 async def _(c: user, m):
-    ea = await m.reply("**BACOT NGENTOTTT!!BAPA LU SURUH RIBUT SAMA GUA**")
+    await m.reply("**BACOT NGENTOTTT!!BAPA LU SURUH RIBUT SAMA GUA**")
 
 
 @ky.ubot("e", sudo=True)
 async def _(c: user, m):
-    ea = await m.reply("**GAK USAH SOK KERAS GOBLOK!!KENCING MASIH BERDIRI AJA BELAGU**")
+    ea = await m.reply(
+        "**GAK USAH SOK KERAS GOBLOK!!KENCING MASIH BERDIRI AJA BELAGU**"
+    )
 
 
 @ky.ubot("f", sudo=True)
 async def _(c: user, m):
-    ea = await m.reply("**MUKA LU SEMUA KAYA KONTOL HAHAHAHA!!**")
+    await m.reply("**MUKA LU SEMUA KAYA KONTOL HAHAHAHA!!**")
 
 
 @ky.ubot("i", sudo=True)
 async def _(c: user, m):
-    ea = await m.reply("**KONTOL MASIH BENGKOK AJA BANGGA LU HAHAHAHA!!**")
+    await m.reply("**KONTOL MASIH BENGKOK AJA BANGGA LU HAHAHAHA!!**")
 
 
 @ky.ubot("r", sudo=True)
 async def _(c: user, m):
-    ea = await m.reply("**KONTOL KONTOL APA YANG BESAR?KONTOL LU LAH HAHAHAHA!!**")
+    await m.reply("**KONTOL KONTOL APA YANG BESAR?KONTOL LU LAH HAHAHAHA!!**")
 
 
 @ky.ubot("t", sudo=True)
 async def _(c: user, m):
-    ea = await m.reply("**BABI!!KONTOL!!NGENTOT!!!**")
+    await m.reply("**BABI!!KONTOL!!NGENTOT!!!**")
 
 
 @ky.ubot("u", sudo=True)
 async def _(c: user, m):
-    ea = await m.reply("**BABI LU GOBLOK!!GANTENGAN JUGA GUA HAHAHAHA**")
+    await m.reply("**BABI LU GOBLOK!!GANTENGAN JUGA GUA HAHAHAHA**")
 
 
 @ky.ubot("w", sudo=True)
 async def _(c: user, m):
-    ea = await m.reply("**BABI LU GOBLOK!!CANTIKAN JUGA GUA HAHAHAHA**")
+    await m.reply("**BABI LU GOBLOK!!CANTIKAN JUGA GUA HAHAHAHA**")
 
 
 @ky.ubot("bct", sudo=True)
 async def _(c: user, m):
-    ea = await m.reply("**BACOTAN LU GAK BIKIN GUA TREMOR GOBLOK HAHAHAHA!!**")
+    await m.reply("**BACOTAN LU GAK BIKIN GUA TREMOR GOBLOK HAHAHAHA!!**")
 
 
 @ky.ubot("n", sudo=True)
 async def _(c: user, m):
-    ea = await m.reply("**GIMANA KABAR KAMU SAYANG??APAKAH BAIK?**")
+    await m.reply("**GIMANA KABAR KAMU SAYANG??APAKAH BAIK?**")
 
 
 @ky.ubot("b", sudo=True)
 async def _(c: user, m):
-    ea = await m.reply("**ELEEEHHHH SOK BANGET KEPINTERAN KAMU!!**")
+    await m.reply("**ELEEEHHHH SOK BANGET KEPINTERAN KAMU!!**")
 
 
 @ky.ubot("m", sudo=True)
 async def _(c: user, m):
-    ea = await m.reply("**INI GRUB APA KUBURAN SEPI BANGET ASTAGFIRULLAH!!**")
+    await m.reply("**INI GRUB APA KUBURAN SEPI BANGET ASTAGFIRULLAH!!**")
 
 
 @ky.ubot("c", sudo=True)
 async def _(c: user, m):
-    ea = await m.reply("**KAN UDAH GUA BILANG??MAKANYA JANGAN NGEYEL!!**")
+    await m.reply("**KAN UDAH GUA BILANG??MAKANYA JANGAN NGEYEL!!**")
 
 
 @ky.ubot("x", sudo=True)
 async def _(c: user, m):
-    ea = await m.reply("**GAUSAH SOKAP DEH KAMU!!**")
+    await m.reply("**GAUSAH SOKAP DEH KAMU!!**")
 
 
 @ky.ubot("y", sudo=True)
 async def _(c: user, m):
-    ea = await m.reply("**MACAM BAGUS AE LU BEGITU HMM!!**")
+    await m.reply("**MACAM BAGUS AE LU BEGITU HMM!!**")
 
 
 @ky.ubot("j", sudo=True)
 async def _(c: user, m):
-    ea = await m.reply("**MAAF BUKAN JAGOAN HAHAHAHA!!**")
+    await m.reply("**MAAF BUKAN JAGOAN HAHAHAHA!!**")
 
 
 @ky.ubot("z", sudo=True)
 async def _(c: user, m):
-    ea = await m.reply("**BISMILLAH SLEEP CALL!!😁**")
+    await m.reply("**BISMILLAH SLEEP CALL!!😁**")
 
 
 @ky.ubot("g", sudo=True)
 async def _(c: user, m):
-    ea = await m.reply("**GANTENG LU BEGITU???**")
+    await m.reply("**GANTENG LU BEGITU???**")
 
 
 @ky.ubot("yy", sudo=True)
 async def _(c: user, m):
-    ea = await m.reply("**War War Tai anjing, Ketrigger minta sharelok, Udah di sharelok Ga nyamperin,Keras di sosmed Bhakss...**")
+    ea = await m.reply(
+        "**War War Tai anjing, Ketrigger minta sharelok, Udah di sharelok Ga nyamperin,Keras di sosmed Bhakss...**"
+    )
+
 
 @ky.ubot("h", sudo=True)
 async def _(c: user, m):
-    ea = await m.reply("**CANTIK LU BEGITU???**")
+    await m.reply("**CANTIK LU BEGITU???**")
 
 
 @ky.ubot("o", sudo=True)
 async def _(c: user, m):
-    ea = await m.reply("**MENTANG MENTANG PUNYA BOT MAINNYA BOT!!PANTES MUKANYA KAYA BOT😁**")
+    ea = await m.reply(
+        "**MENTANG MENTANG PUNYA BOT MAINNYA BOT!!PANTES MUKANYA KAYA BOT😁**"
+    )
 
 
 @ky.ubot("war", sudo=True)
 async def _(c: user, m):
-    ea = await m.reply("**NORAK LU KONTOL!! MAKE USERBOT CUMAN BUAT WAR MALAH SOK KERAS**")
+    ea = await m.reply(
+        "**NORAK LU KONTOL!! MAKE USERBOT CUMAN BUAT WAR MALAH SOK KERAS**"
+    )
