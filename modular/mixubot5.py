3cf3c3f7d42cac966694403be1e0cf7f090ebe70
iamuput <yogaaasp73@gmail.com>
2024-03-10 01:09:17 +0700

    Create mixubot5.py

---
+++ modular/mixubot5.py
@@ -0,0 +1,60 @@
+# Part Of Ayiin-Userbot
+
+import asyncio
+
+from pyrogram import *
+
+from Mix import *
+
+__modles__ = "MixUbot4"
+__help__ = "MixUbot4"
+
+
+@ky.ubot("ganteng", sudo=True)
+async def _(c: user, m):
+    uputt = await m.edit("`Lu Mau Tau Sebuah Fakta?`")
+    await asyncio.sleep(0.2)
+    await uputt.edit("`Fakta Yang Belum Terbongkar Selama Ini`")
+    await asyncio.sleep(0.2)
+    await uputt.edit("`**GUA GANTENG FIX NO DEBAT😏**`")
+  
+
+
+@ky.ubot("wibu", sudo=True)
+async def _(c: user, m):
+    uputt = await m.edit("`Kata Emak`")
+    await asyncio.sleep(2)
+    await uputt.edit("`Kalo Ketemu Wibuu`")
+    await asyncio.sleep(2)
+    await uputt.edit("`Harus Lari Sekenceng Mungkin🏃🏻`")
+    await asyncio.sleep(3)
+    await uputt.edit("`ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ🏃🏻`")
+    await uputt.edit("`ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ🏃🏻💨`")
+    await uputt.edit("`ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ🏃🏻💨ㅤ`")
+    await uputt.edit("`ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ🏃🏻💨ㅤㅤ`")
+    await uputt.edit("`ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ🏃🏻💨ㅤㅤㅤ`")
+    await uputt.edit("`ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ🏃🏻💨ㅤㅤㅤㅤ`")
+    await uputt.edit("`ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ🏃🏻💨ㅤㅤㅤㅤㅤ`")
+    await uputt.edit("`ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ🏃🏻💨ㅤㅤㅤㅤㅤㅤ`")
+    await uputt.edit("`ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ🏃🏻💨ㅤㅤㅤㅤㅤㅤㅤ`")
+    await uputt.edit("`ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ🏃🏻💨ㅤㅤㅤㅤㅤㅤㅤㅤ`")
+    await uputt.edit("`ㅤㅤㅤㅤㅤㅤㅤㅤㅤ🏃🏻💨ㅤㅤㅤㅤㅤㅤㅤㅤㅤ`")
+    await uputt.edit("`ㅤㅤㅤㅤㅤㅤㅤㅤ🏃🏻💨ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ`")
+    await uputt.edit("`ㅤㅤㅤㅤㅤㅤㅤ🏃🏻💨ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ`")
+    await uputt.edit("`ㅤㅤㅤㅤㅤㅤ🏃🏻💨ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ`")
+    await uputt.edit("`ㅤㅤㅤㅤㅤ🏃🏻💨ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ`")
+    await uputt.edit("`ㅤㅤㅤㅤ🏃🏻💨ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ`")
+    await uputt.edit("`ㅤㅤㅤ🏃🏻💨ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ`")
+    await uputt.edit("`ㅤㅤ🏃🏻💨ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ`")
+    await uputt.edit("`ㅤ🏃🏻💨ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ`")
+    await uputt.edit("`🏃🏻💨ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ`")
+    await uputt.edit("`🧎🏻‍♂️ huhh... akhirnya bisa lolos dari wibu mematikan`")
+
+# create by ayiin
+
+
+@ky.ubot("ssenggol", sudo=True)
+async def _(c: user, m):
+    uputt = await m.edit("`Bapaknya Udin Di Makan Singkong`")
+    await asyncio.sleep(0.2)
+    await uputt.edit("`Cuma Sendiri ni Senggol Dong`")
