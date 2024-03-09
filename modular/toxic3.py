7a78ce734fe6f4d9c84779d3501b5d08bb52fc8e
iamuput <yogaaasp73@gmail.com>
2024-03-10 00:27:42 +0700

    Create toxic3.py

---
+++ modular/toxic3.py
@@ -0,0 +1,65 @@
+# Part Of Ayiin-Userbot
+
+import asyncio
+
+from pyrogram import *
+
+from Mix import *
+
+__modles__ = "Toxic3"
+__help__ = "Toxic3"
+
+
+@ky.ubot("ceking", sudo=True)
+async def _(c: user, m):
+    sepong = await m.edit("**GIGI KUNING MATA MERAH BADAN KURUS CEKING EMANG PANTES...**")
+    await asyncio.sleep(0.2)
+    await sepong.edit("**DI KENCINGIN JAHANAM**")
+    await asyncio.sleep(0.2)
+    await sepong.edit("**ORANG KAYA LUH ITU...**")
+    await asyncio.sleep(0.2)
+    await sepong.edit("**CUMAN SEMPIT SEMPITIN ISI DUNIA DOANG KONTOL SEMPAK**")
+    await asyncio.sleep(0.2)
+    await sepong.edit("**GUA KASIH TAU NIH YAH USUS LUH TUH UDAH MELINTIR KONTOL**")
+    await asyncio.sleep(0.2)
+    await sepong.edit("**KERONGKONGAN LUH ITU UDAH RUSAK TOLOL...**")
+    await asyncio.sleep(0.2)
+    await sepong.edit("**MASIH AJA MAKSAIN BUAT ADU ROASTING AMA GUA BEGO BANGET SIH LUH...**")
+
+
+@ky.ubot("hina", sudo=True)
+async def _(c: user, m):
+    Kon = await m.edit("**IZIN PANTUN BANG...**")
+    await asyncio.sleep(0.2)
+    await Kon.edit("**KETEMU SI MAMAS DIAJAKIN KE CIBINONG...**")
+    await asyncio.sleep(0.2)
+    await Kon.edit("**PULANG NYE DIANTERIN MAKE KOPAJA...**")
+    await asyncio.sleep(0.2)
+    await Kon.edit("**EH BOCAH AMPAS TITISAN DAJJAL...**")
+    await asyncio.sleep(0.2)
+    await Kon.edit("**MUKA HINA KEK ODONG ODONG**")
+    await asyncio.sleep(0.2)
+    await Kon.edit("**GA USAH SO KERAS DEH LU KALO MENTAL BLOM SEKERAS BAJA...**")
+    await asyncio.sleep(0.2)
+    await Kon.edit("**LUH ITU MANUSIA...**")
+    await asyncio.sleep(0.2)
+    await Kon.edit("**MANUSIA HINA YANG DI CIPTAKAN DENGAN SECARA HINA**")
+    await asyncio.sleep(0.2)
+    await Kon.edit("**MANUSIA HINA YANG DI CIPTAKAN DENGAN SECARA HINA EMANG PANTES UNTUK DI HINA HINA...**")
+
+
+@ky.ubot("ngaca", sudo=True)
+async def _(c: user, m):
+    omek = await m.edit("**IZIN NUMPANG PANTUN BANG...**")
+    await asyncio.sleep(0.2)
+    await omek.edit("**BELI SEPATU KACA KE CHINA...**")
+    await asyncio.sleep(0.2)
+    await omek.edit("**ASEEEKKKK 🤪**")
+    await asyncio.sleep(0.2)
+    await omek.edit("**NGACA DULU BARU NGEHINA KONTOL...**")
+    await asyncio.sleep(0.2)
+    await omek.edit("**UDAH BULUK ITEM PENDEK BERPONI BAJU KEGEDEAN KAYAK JAMET**")
+    await asyncio.sleep(0.2)
+    await omek.edit("**UDAH BULUK ITEM PENDEK BERPONI BAJU KEGEDEAN KAYAK JAMET SOK-SOK AN MAU NGEHINA GUA KONTOL**")
+    await asyncio.sleep(0.2)
+    await omek.edit("**KENA KAN MENTAL LU...**")
