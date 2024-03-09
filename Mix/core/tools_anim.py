166dd27c52f2f57610617b19b3a0119c11a20b03
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-09 17:53:42 +0700

    update Mix/core/tools_anim.py

---
+++ Mix/core/tools_anim.py
@@ -0,0 +1,418 @@
+import asyncio
+
+DEFAULTUSER = "Nay"
+
+NOBLE = [
+    "╲╲╲┏━━┓╭━━━╮╱╱╱\n╲╲╲┗┓┏┛┃╭━╮┃╱╱╱\n╲╲╲╲┃┃┏┫┃╭┻┻┓╱╱\n╱╱╱┏╯╰╯┃╰┫┏━╯╱╱\n╱╱┏┻━┳┳┻━┫┗┓╱╱╱\n╱╱╰━┓┃┃╲┏┫┏┛╲╲╲\n╱╱╱╱┃╰╯╲┃┃┗━╮╲╲\n╱╱╱╱╰━━━╯╰━━┛╲╲",
+    "┏━╮\n┃▔┃▂▂┏━━┓┏━┳━━━┓\n┃▂┣━━┻━╮┃┃▂┃▂┏━╯\n┃▔┃▔╭╮▔┃┃┃▔┃▔┗━┓\n┃▂┃▂╰╯▂┃┗╯▂┃▂▂▂┃\n┃▔┗━━━╮┃▔▔▔┃▔┏━╯\n┃▂▂▂▂▂┣╯▂▂▂┃▂┗━╮\n┗━━━━━┻━━━━┻━━━┛",
+    "┏┓┏━┳━┳━┳━┓\n┃┗┫╋┣┓┃┏┫┻┫\n┗━┻━┛┗━┛┗━┛\n────­­­­­­­­­YOU────",
+    "╦──╔╗─╗╔─╔ ─\n║──║║─║║─╠ ─\n╚═─╚╝─╚╝─╚ ─\n╦─╦─╔╗─╦╦   \n╚╦╝─║║─║║ \n─╩──╚╝─╚╝",
+    "╔══╗....<3 \n╚╗╔╝..('\\../') \n╔╝╚╗..( •.• ) \n╚══╝..(,,)(,,) \n╔╗╔═╦╦╦═╗ ╔╗╔╗ \n║╚╣║║║║╩╣ ║╚╝║ \n╚═╩═╩═╩═╝ ╚══╝",
+    "░I░L░O░V░E░Y░O░U░",
+    "┈┈╭━╱▔▔▔▔╲━╮┈┈┈\n┈┈╰╱╭▅╮╭▅╮╲╯┈┈┈\n╳┈┈▏╰┈▅▅┈╯▕┈┈┈┈\n┈┈┈╲┈╰━━╯┈╱┈┈╳┈\n┈┈┈╱╱▔╲╱▔╲╲┈┈┈┈\n┈╭━╮▔▏┊┊▕▔╭━╮┈╳\n┈┃┊┣▔╲┊┊╱▔┫┊┃┈┈\n┈╰━━━━╲╱━━━━╯┈╳",
+    "╔ღ═╗╔╗\n╚╗╔╝║║ღ═╦╦╦═ღ\n╔╝╚╗ღ╚╣║║║║╠╣\n╚═ღ╝╚═╩═╩ღ╩═╝",
+    "╔══╗ \n╚╗╔╝ \n╔╝(¯'v'¯) \n╚══'.¸./\n╔╗╔═╦╦╦═╗ ╔╗╔╗ \n║╚╣║║║║╩╣ ║╚╝║ \n╚═╩═╩═╩═╝ ╚══╝",
+    "╔╗ \n║║╔═╦═╦═╦═╗ ╔╦╗ \n║╚╣╬╠╗║╔╣╩╣ ║║║ \n╚═╩═╝╚═╝╚═╝ ╚═╝ \n╔═╗ \n║═╬═╦╦╦═╦═╦═╦═╦═╗ \n║╔╣╬║╔╣╩╬╗║╔╣╩╣╔╝ \n╚╝╚═╩╝╚═╝╚═╝╚═╩╝",
+    "╔══╗ \n╚╗╔╝ \n╔╝╚╗ \n╚══╝ \n╔╗ \n║║╔═╦╦╦═╗ \n║╚╣║║║║╚╣ \n╚═╩═╩═╩═╝ \n╔╗╔╗ ♥️ \n║╚╝╠═╦╦╗ \n╚╗╔╣║║║║ \n═╚╝╚═╩═╝",
+    "╔══╗╔╗  ♡ \n╚╗╔╝║║╔═╦╦╦╔╗ \n╔╝╚╗║╚╣║║║║╔╣ \n╚══╝╚═╩═╩═╩═╝\n­­­─────­­­­­­­­­YOU─────",
+    "╭╮╭╮╮╭╮╮╭╮╮╭╮╮ \n┃┃╰╮╯╰╮╯╰╮╯╰╮╯ \n┃┃╭┳━━┳━╮╭━┳━━╮ \n┃┃┃┃╭╮┣╮┃┃╭┫╭╮┃ \n┃╰╯┃╰╯┃┃╰╯┃┃╰┻┻╮ \n╰━━┻━━╯╰━━╯╰━━━╯",
+    "┊┊╭━╮┊┊┊┊┊┊┊┊┊┊┊ \n━━╋━╯┊┊┊┊┊┊┊┊┊┊┊ \n┊┊┃┊╭━┳╮╭┓┊╭╮╭━╮ \n╭━╋━╋━╯┣╯┃┊┃╰╋━╯ \n╰━╯┊╰━━╯┊╰━┛┊╰━━",
+]
+
+R = "❤️"
+W = "🤍"
+
+heart_list = [
+    W * 9,
+    W * 2 + R * 2 + W + R * 2 + W * 2,
+    W + R * 7 + W,
+    W + R * 7 + W,
+    W + R * 7 + W,
+    W * 2 + R * 5 + W * 2,
+    W * 3 + R * 3 + W * 3,
+    W * 4 + R + W * 4,
+    W * 9,
+]
+joined_heart = "\n".join(heart_list)
+heartlet_len = joined_heart.count(R)
+SLEEP = 0.1
+
+
+async def _wrap_edit(message, text: str):
+    """Floodwait-safe utility wrapper for edit"""
+    try:
+        await message.edit(text)
+    except FloodWait as fl:
+        await asyncio.sleep(fl.x)
+
+
+async def phase1(message):
+    """Big scroll"""
+    BIG_SCROLL = "🧡💛💚💙💜🖤🤎"
+    await _wrap_edit(message, joined_heart)
+    for heart in BIG_SCROLL:
+        await _wrap_edit(message, joined_heart.replace(R, heart))
+        await asyncio.sleep(SLEEP)
+
+
+async def phase2(message):
+    """Per-heart randomiser"""
+    ALL = ["❤️"] + list("🧡💛💚💙💜🤎🖤")  # don't include white heart
+
+    format_heart = joined_heart.replace(R, "{}")
+    for _ in range(5):
+        heart = format_heart.format(*random.choices(ALL, k=heartlet_len))
+        await _wrap_edit(message, heart)
+        await asyncio.sleep(SLEEP)
+
+
+async def phase3(message):
+    """Fill up heartlet matrix"""
+    await _wrap_edit(message, joined_heart)
+    await asyncio.sleep(SLEEP * 2)
+    repl = joined_heart
+    for _ in range(joined_heart.count(W)):
+        repl = repl.replace(W, R, 1)
+        await _wrap_edit(message, repl)
+        await asyncio.sleep(SLEEP)
+
+
+async def phase4(message):
+    """Matrix shrinking"""
+    for i in range(7, 0, -1):
+        heart_matrix = "\n".join([R * i] * i)
+        await _wrap_edit(message, heart_matrix)
+        await asyncio.sleep(SLEEP)
+
+class MEMES:
+    REVERSE = (
+        "⠐⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠂\n"
+        "⠄⠄⣰⣾⣿⣿⣿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣆⠄⠄\n"
+        "⠄⠄⣿⣿⣿⡿⠋⠄⡀⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠋⣉⣉⣉⡉⠙⠻⣿⣿⠄⠄\n"
+        "⠄⠄⣿⣿⣿⣇⠔⠈⣿⣿⣿⣿⣿⡿⠛⢉⣤⣶⣾⣿⣿⣿⣿⣿⣿⣦⡀⠹⠄⠄\n"
+        "⠄⠄⣿⣿⠃⠄⢠⣾⣿⣿⣿⠟⢁⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠄⠄\n"
+        "⠄⠄⣿⣿⣿⣿⣿⣿⣿⠟⢁⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠄⠄\n"
+        "⠄⠄⣿⣿⣿⣿⣿⡟⠁⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄\n"
+        "⠄⠄⣿⣿⣿⣿⠋⢠⣾⣿⣿⣿⣿⣿⣿⡿⠿⠿⠿⠿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄\n"
+        "⠄⠄⣿⣿⡿⠁⣰⣿⣿⣿⣿⣿⣿⣿⣿⠗⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿⣿⡟⠄⠄\n"
+        "⠄⠄⣿⡿⠁⣼⣿⣿⣿⣿⣿⣿⡿⠋⠄⠄⠄⣠⣄⢰⣿⣿⣿⣿⣿⣿⣿⠃⠄⠄\n"
+        "⠄⠄⡿⠁⣼⣿⣿⣿⣿⣿⣿⣿⡇⠄⢀⡴⠚⢿⣿⣿⣿⣿⣿⣿⣿⣿⡏⢠⠄⠄\n"
+        "⠄⠄⠃⢰⣿⣿⣿⣿⣿⣿⡿⣿⣿⠴⠋⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⡟⢀⣾⠄⠄\n"
+        "⠄⠄⢀⣿⣿⣿⣿⣿⣿⣿⠃⠈⠁⠄⠄⢀⣴⣿⣿⣿⣿⣿⣿⣿⡟⢀⣾⣿⠄⠄\n"
+        "⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⠄⠄⠄⠄⢶⣿⣿⣿⣿⣿⣿⣿⣿⠏⢀⣾⣿⣿⠄⠄\n"
+        "⠄⠄⣿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⠋⣠⣿⣿⣿⣿⠄⠄\n"
+        "⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢁⣼⣿⣿⣿⣿⣿⠄⠄\n"
+        "⠄⠄⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢁⣴⣿⣿⣿⣿⣿⣿⣿⠄⠄\n"
+        "⠄⠄⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⢁⣴⣿⣿⣿⣿⠗⠄⠄⣿⣿⠄⠄\n"
+        "⠄⠄⣆⠈⠻⢿⣿⣿⣿⣿⣿⣿⠿⠛⣉⣤⣾⣿⣿⣿⣿⣿⣇⠠⠺⣷⣿⣿⠄⠄\n"
+        "⠄⠄⣿⣿⣦⣄⣈⣉⣉⣉⣡⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⠉⠁⣀⣼⣿⣿⣿⠄⠄\n"
+        "⠄⠄⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣾⣿⣿⡿⠟⠄⠄\n"
+        "⠠⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄\n"
+    )
+
+    SLAP_TEMPLATES = [
+        "{hits} {victim} with a {item}.",
+        "{hits} {victim} in the face with a {item}.",
+        "{hits} {victim} around a bit with a {item}.",
+        "{throws} a {item} at {victim}.",
+        "grabs a {item} and {throws} it at {victim}'s face.",
+        "{hits} a {item} at {victim}.",
+        "{throws} a few {item} at {victim}.",
+        "grabs a {item} and {throws} it in {victim}'s face.",
+        "launches a {item} in {victim}'s general direction.",
+        "sits on {victim}'s face while slamming a {item} {where}.",
+        "starts slapping {victim} silly with a {item}.",
+        "pins {victim} down and repeatedly {hits} them with a {item}.",
+        "grabs up a {item} and {hits} {victim} with it.",
+        "starts slapping {victim} silly with a {item}.",
+        "holds {victim} down and repeatedly {hits} them with a {item}.",
+        "prods {victim} with a {item}.",
+        "picks up a {item} and {hits} {victim} with it.",
+        "ties {victim} to a chair and {throws} a {item} at them.",
+        "{hits} {victim} {where} with a {item}.",
+        "ties {victim} to a pole and whips them {where} with a {item}."
+        "gave a friendly push to help {victim} learn to swim in lava.",
+        "sent {victim} to /dev/null.",
+        "sent {victim} down the memory hole.",
+        "beheaded {victim}.",
+        "threw {victim} off a building.",
+        "replaced all of {victim}'s music with Nickelback.",
+        "spammed {victim}'s email.",
+        "made {victim} a knuckle sandwich.",
+        "slapped {victim} with pure nothing.",
+        "hit {victim} with a small, interstellar spaceship.",
+        "quickscoped {victim}.",
+        "put {victim} in check-mate.",
+        "RSA-encrypted {victim} and deleted the private key.",
+        "put {victim} in the friendzone.",
+        "slaps {victim} with a DMCA takedown request!",
+    ]
+
+    ITEMS = [
+        "cast iron skillet",
+        "large trout",
+        "baseball bat",
+        "cricket bat",
+        "wooden cane",
+        "nail",
+        "printer",
+        "shovel",
+        "pair of trousers",
+        "CRT monitor",
+        "diamond sword",
+        "baguette",
+        "physics textbook",
+        "toaster",
+        "portrait of Richard Stallman",
+        "television",
+        "mau5head",
+        "five ton truck",
+        "roll of duct tape",
+        "book",
+        "laptop",
+        "old television",
+        "sack of rocks",
+        "rainbow trout",
+        "cobblestone block",
+        "lava bucket",
+        "rubber chicken",
+        "spiked bat",
+        "gold block",
+        "fire extinguisher",
+        "heavy rock",
+        "chunk of dirt",
+        "beehive",
+        "piece of rotten meat",
+        "bear",
+        "ton of bricks",
+    ]
+
+    THROW = [
+        "throws",
+        "flings",
+        "chucks",
+        "hurls",
+    ]
+
+    HIT = [
+        "hits",
+        "whacks",
+        "slaps",
+        "smacks",
+        "bashes",
+    ]
+
+    WHERE = ["in the chest", "on the head", "on the butt", "on the crotch"]
+
+    REPLACEMENT_MAP = {
+        "a": "ɐ",
+        "b": "q",
+        "c": "ɔ",
+        "d": "p",
+        "e": "ǝ",
+        "f": "ɟ",
+        "g": "ƃ",
+        "h": "ɥ",
+        "i": "ᴉ",
+        "j": "ɾ",
+        "k": "ʞ",
+        "l": "l",
+        "m": "ɯ",
+        "n": "u",
+        "o": "o",
+        "p": "d",
+        "q": "b",
+        "r": "ɹ",
+        "s": "s",
+        "t": "ʇ",
+        "u": "n",
+        "v": "ʌ",
+        "w": "ʍ",
+        "x": "x",
+        "y": "ʎ",
+        "z": "z",
+        "A": "∀",
+        "B": "B",
+        "C": "Ɔ",
+        "D": "D",
+        "E": "Ǝ",
+        "F": "Ⅎ",
+        "G": "פ",
+        "H": "H",
+        "I": "I",
+        "J": "ſ",
+        "K": "K",
+        "L": "˥",
+        "M": "W",
+        "N": "N",
+        "O": "O",
+        "P": "Ԁ",
+        "Q": "Q",
+        "R": "R",
+        "S": "S",
+        "T": "┴",
+        "U": "∩",
+        "V": "Λ",
+        "W": "M",
+        "X": "X",
+        "Y": "⅄",
+        "Z": "Z",
+        "0": "0",
+        "1": "Ɩ",
+        "2": "ᄅ",
+        "3": "Ɛ",
+        "4": "ㄣ",
+        "5": "ϛ",
+        "6": "9",
+        "7": "ㄥ",
+        "8": "8",
+        "9": "6",
+        ",": "'",
+        ".": "˙",
+        "?": "¿",
+        "!": "¡",
+        '"': ",,",
+        "'": ",",
+        "(": ")",
+        ")": "(",
+        "[": "]",
+        "]": "[",
+        "{": "}",
+        "}": "{",
+        "<": ">",
+        ">": "<",
+        "&": "⅋",
+        "_": "‾",
+    }
+
+    SHRUGS = [
+        "┐(´д｀)┌",
+        "┐(´～｀)┌",
+        "┐(´ー｀)┌",
+        "┐(￣ヘ￣)┌",
+        "╮(╯∀╰)╭",
+        "╮(╯_╰)╭",
+        "┐(´д`)┌",
+        "┐(´∀｀)┌",
+        "ʅ(́◡◝)ʃ",
+        "┐(ﾟ～ﾟ)┌",
+        "┐('д')┌",
+        "┐(‘～`;)┌",
+        "ヘ(´－｀;)ヘ",
+        "┐( -“-)┌",
+        "ʅ（´◔౪◔）ʃ",
+        "ヽ(゜～゜o)ノ",
+        "ヽ(~～~ )ノ",
+        "┐(~ー~;)┌",
+        "┐(-。ー;)┌",
+        r"¯\_(ツ)_/¯",
+        r"¯\_(⊙_ʖ⊙)_/¯",
+        r"¯\_༼ ಥ ‿ ಥ ༽_/¯",
+        "乁( ⁰͡  Ĺ̯ ⁰͡ ) ㄏ",
+        r"¯\_༼ •́ ͜ʖ •̀ ༽_/¯",
+        r"¯\_( ͡° ͜ʖ ͡°)_/¯",
+        r"¯\(°_o)/¯",
+        "┐( ∵ )┌",
+        r"¯\_༼ᴼل͜ᴼ༽_/¯",
+        "╮(. ❛ ᴗ ❛.)╭",
+        "乁༼◉‿◉✿༽ㄏ",
+        r"¯\(◉‿◉)/¯",
+        r"¯\_ʘ‿ʘ_/¯",
+        r"¯\_༼ ಥ ‿ ಥ ༽_/¯",
+        "╮(＾▽＾)╭",
+        "乁[ ◕ ᴥ ◕ ]ㄏ",
+        "乁[ᓀ˵▾˵ᓂ]ㄏ",
+        "┐(´(エ)｀)┌",
+        "乁( ⁰͡ Ĺ̯ ⁰͡ ) ㄏ",
+        r"¯\_( ͠° ͟ʖ °͠ )_/¯",
+        "乁( •_• )ㄏ",
+        "乁| ･ 〰 ･ |ㄏ",
+        "┐(‘～;)┌",
+        "┐(￣ヘ￣)┌",
+        "┐(´д)┌",
+        "乁( . ര ʖ̯ ര . )ㄏ",
+        "乁 ˘ o ˘ ㄏ",
+        "乁ʕ •̀ ۝ •́ ʔㄏ",
+        r"¯\_(◕෴◕)_/¯",
+        r"¯\_〳 •̀ o •́ 〵_/¯",
+        "乁║ ˙ 益 ˙ ║ㄏ",
+    ]
+
+    BRAIN = [
+        "YOᑌᖇ ᗷᖇᗩIᑎ ➡️ 🧠\n\n🧠         <(^_^ <)🗑",
+        "YOᑌᖇ ᗷᖇᗩIᑎ ➡️ 🧠\n\n🧠       <(^_^ <)  🗑",
+        "YOᑌᖇ ᗷᖇᗩIᑎ ➡️ 🧠\n\n🧠     <(^_^ <)    🗑",
+        "YOᑌᖇ ᗷᖇᗩIᑎ ➡️ 🧠\n\n🧠   <(^_^ <)      🗑",
+        "YOᑌᖇ ᗷᖇᗩIᑎ ➡️ 🧠\n\n🧠 <(^_^ <)        🗑",
+        "YOᑌᖇ ᗷᖇᗩIᑎ ➡️ 🧠\n\n🧠<(^_^ <)         🗑",
+        "YOᑌᖇ ᗷᖇᗩIᑎ ➡️ 🧠\n\n(> ^_^)>🧠         🗑",
+        "YOᑌᖇ ᗷᖇᗩIᑎ ➡️ 🧠\n\n  (> ^_^)>🧠       🗑",
+        "YOᑌᖇ ᗷᖇᗩIᑎ ➡️ 🧠\n\n    (> ^_^)>🧠     🗑",
+        "YOᑌᖇ ᗷᖇᗩIᑎ ➡️ 🧠\n\n      (> ^_^)>🧠   🗑",
+        "YOᑌᖇ ᗷᖇᗩIᑎ ➡️ 🧠\n\n        (> ^_^)>🧠 🗑",
+        "YOᑌᖇ ᗷᖇᗩIᑎ ➡️ 🧠\n\n          (> ^_^)>🧠🗑",
+        "YOᑌᖇ ᗷᖇᗩIᑎ ➡️ 🧠\n\n           (> ^_^)>🗑",
+        "YOᑌᖇ ᗷᖇᗩIᑎ ➡️ 🧠\n\n           <(^_^ <)🗑",
+    ]
+
+    COCK = (
+        "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠿⢿⣿⣿⠿⠛⠿⣿⣿⣿⣿⣿\n"
+        "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠟⠉⠄⣀⡤⢤⣤⣈⠁⣠⡔⠶⣾⣿⣿⣿\n"
+        "⣿⣿⣿⣿⣿⣿⣿⡿⠛⠋⠁⠄⠄⠄⣼⣿⠁⡀⢹⣿⣷⢹⡇⠄⠎⣿⣿⣿\n"
+        "⣿⣿⣿⠿⠛⠉⠁⠄⠄⠄⠄⠄⠄⠄⠹⣇⣀⣡⣾⣿⡿⠉⠛⠒⠒⠋⠉⢸\n"
+        "⡿⠋⠁⠄⠄⢀⣤⣤⡀⠄⠄⠄⠄⠄⠄⠈⠙⠛⠛⠉⠄⠄⠄⠄⠄⠄⠄⠈\n"
+        "⠄⠄⠄⠄⠄⢹⣧⡈⠿⣷⣄⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⣠⢄⣾\n"
+        "⠄⠄⠄⠄⠄⠈⠻⢿⣶⣌⣙⡛⠛⠿⠶⠶⠶⠶⠶⠖⣒⣒⣚⣋⡩⢱⣾⣿\n"
+        "⠄⠄⠄⠄⠄⠄⠄⠄⠈⠉⠛⠛⠛⠻⠿⠿⠟⠛⠛⠛⠉⢉⣥⣶⣾⣿⣿⣿\n"
+        "⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠒⠶⣿⣿⣿⣿⣿⣿⣿⣿\n"
+        "⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⠻⣿⣿⣿⣿⣿⣿\n"
+        "⣿⡿⠛⠛⠛⢻⣿⠿⠛⠛⠛⢿⣿⣿⡿⠛⠛⠛⢻⡟⠛⣿⡿⠛⣻⣿⣿⣿\n"
+        "⡟⠄⣼⣿⣿⣿⡇⠄⣾⣿⣧⠄⢻⡏⠄⣼⣿⣿⣿⡇⠄⡟⢀⣴⣿⣿⣿⣿\n"
+        "⡇⠄⣿⣿⣿⣿⡄⠄⣿⣿⣿⠄⢸⡇⠄⣿⣿⣿⣿⡇⠄⣀⠈⢻⣿⣿⣿⣿\n"
+        "⣿⣄⠈⠙⠛⢻⣧⡄⠙⠛⠉⣠⣿⣷⣄⠈⠙⠛⢹⡇⠄⣿⣧⠄⠻⣿⣿⣿\n"
+    )
+
+    TABLE_FLIPS = [
+        "(╯°Д°)╯︵/(.□ . \)",
+        "(˚Õ˚)ر ~~~~╚╩╩╝",
+        "(ノಠ益ಠ)ノ彡┻━┻",
+        "(╯°□°)╯︵ ┻━┻",
+        "(┛◉Д◉)┛彡┻━┻",
+        "┻━┻︵ \(°□°)/ ︵ ┻━┻",
+        "(┛ಠ_ಠ)┛彡┻━┻",
+        "(╯°□°)╯︵ ʞooqǝɔɐℲ",
+    ]
+
+    GAMBAR_KONTOL = """
+⣠⡶⠚⠛⠲⢄⡀
+⣼⠁ ⠀⠀⠀ ⠳⢤⣄
+⢿⠀⢧⡀⠀⠀⠀⠀⠀⢈⡇
+⠈⠳⣼⡙⠒⠶⠶⠖⠚⠉⠳⣄
+⠀⠀⠈⣇⠀⠀⠀⠀⠀⠀⠀⠈⠳⣄
+⠀⠀⠀⠘⣆ ⠀⠀⠀⠀ ⠀⠈⠓⢦⣀
+⠀⠀⠀⠀⠈⢳⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠲⢤
+⠀⠀⠀⠀⠀⠀⠙⢦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧
+⠀⠀⠀⠀⠀⠀⠀⡴⠋⠓⠦⣤⡀⠀⠀⠀⠀⠀⠀⠀⠈⣇
+⠀⠀⠀⠀⠀⠀⣸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡄
+⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇
+⠀⠀⠀⠀⠀⠀⢹⡄⠀⠀⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠃
+⠀⠀⠀⠀⠀⠀⠀⠙⢦⣀⣳⡀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠏
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠛⢦⣀⣀⣀⣀⣠⡴⠚⠉
+"""
+
+    GAMBAR_TITIT = """
+😋😋
+😋😋😋
+  😋😋😋
+    😋😋😋
+     😋😋😋
+       😋😋😋
+        😋😋😋
+         😋😋😋
+          😋😋😋
+          😋😋😋
+      😋😋😋😋
+ 😋😋😋😋😋😋
+ 😋😋😋  😋😋😋
+    😋😋       😋😋
+"""
