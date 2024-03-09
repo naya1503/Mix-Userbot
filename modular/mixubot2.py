d2fb1020e563ce5fcce07b37ff7c649f197f2971
iamuput <yogaaasp73@gmail.com>
2024-03-10 00:47:22 +0700

    Create mixubot2.py

---
+++ modular/mixubot2.py
@@ -0,0 +1,98 @@
+# Copyright (C) 2021 Kyy - Userbot
+# Credit by kyy
+# Recode by @AyiinXd
+
+
+import asyncio
+
+from pyrogram import *
+
+from Mix import *
+
+__modles__ = "MixUbot2"
+__help__ = "MixUbot2"
+
+
+@ky.ubot("lipkol", sudo=True)
+async def _(c: user, m):
+    uputt = await m.edit("**Ayaaang** 🥺")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**Kangeeen** 👉👈")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**Pingiinn Slipkool Yaaang** 🥺👉👈")
+
+
+# Create by myself @localheart
+
+
+@ky.ubot("nakal", sudo=True)
+async def _(c: user, m):
+    uputt = await m.edit("**Ayaaang Ih** 🥺")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**Nakal Banget Dah Ayang** 🥺")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**Aku Gak Like Ayang** 😠")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**Pokoknya Aku Gak Like Ih** 😠")
+  
+
+
+@ky.ubot("favboy", sudo=True)
+async def _(c: user, m):
+    uputt = await m.edit("**Duuhh Ada Cowo Ganteng** 👉👈")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**You Are My Favorit Boy** 😍")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**Kamu Harus Jadi Cowo Aku Ya** 😖")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**Pokoknya Harus Jadi Cowo Aku** 👉👈")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**Gak Boleh Ada Yang Lain** 😠")
+
+
+@ky.ubot("favgirl", sudo=True)
+async def _(c: user, m):
+    uputt = await m.edit("**Duuhh Ada Cewe Cantik** 👉👈")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**You Are My Favorit Girl** 😍")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**Kamu Harus Jadi Cewe Aku Ya** 😖")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**Pokoknya Harus Jadi Cewe Aku** 👉👈")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**Gak Boleh Ada Yang Lain** 😠")
+
+
+@ky.ubot("canlay", sudo=True)
+async def _(c: user, m):
+    uputt = await m.edit("**Eh Kamu Cantik-cantik**")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**Kok Alay Banget**")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**Spam Bot Mulu**")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**Baru Bikin Userbot Ya??**")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**Pantes Norak Xixixi**")
+
+
+@ky.ubot("ganlay", sudo=True)
+async def _(c: user, m):
+    uputt = await m.edit("**Eh Kamu Ganteng-ganteng**")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**Kok Alay Banget**")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**Spam Bot Mulu**")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**Baru Bikin Userbot Ya??**")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**Pantes Norak Xixixi**")
+
+
+@ky.ubot("ange", sudo=True)
+async def _(c: user, m):
+    uputt = await m.edit("**Ayanggg 😖**")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**Aku Ange 😫**")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**Ayuukk Picies Yang 🤤**")
