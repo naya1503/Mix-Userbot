c9d4baecacbb34f22fe218d3de304a39acb085a4
iamuput <yogaaasp73@gmail.com>
2024-03-10 01:04:34 +0700

    Create mixubot4.py

---
+++ modular/mixubot4.py
@@ -0,0 +1,125 @@
+# Part Of Ayiin-Userbot
+
+import asyncio
+
+from pyrogram import *
+
+from Mix import *
+
+__modles__ = "MixUbot4"
+__help__ = "MixUbot4"
+
+
+@ky.ubot("ywc", sudo=True)
+async def _(c: user, m):
+    await m.edit("**Oke Sama-sama**")
+
+
+@ky.ubot("jamet", sudo=True)
+async def _(c: user, m):
+    uputt = await m.edit("**WOIII**")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**JAMETTT**")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**CUMA MAU BILANG**")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**GAUSAH SO ASIK**")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**EMANG KENAL?**")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**GAUSAH REPLY**")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**KITA BUKAN KAWAN**")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**GASUKA PC ANJING**")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**BOCAH KAMPUNG**")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**MENTAL TEMPE**")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**LEMBEK NGENTOT🔥**")
+
+
+@ky.ubot("pp", sudo=True)
+async def _(c: user, m):
+    await m.edit("**PASANG PP DULU GOBLOK,BIAR ORANG-ORANG PADA TAU BETAPA HINA NYA MUKA LU 😆**")
+
+
+@ky.ubot("dp", sudo=True)
+async def _(c: user, m):
+    await m.edit("**MUKA LU HINA, GAUSAH SOK KERAS YA ANJENGG!!**")
+
+
+@ky.ubot("so", sudo=True)
+async def _(c: user, m):
+    await m.edit("**GAUSAH SOKAB SAMA GUA GOBLOK, LU BABU GA LEVEL!!**")
+
+
+@ky.ubot("met", sudo=True)
+async def _(c: user, m):
+    await m.edit("**NAMANYA JUGA JAMET CAPER SANA SINI BUAT CARI NAMA**")
+  
+
+@ky.ubot("war", sudo=True)
+async def _(c: user, m):
+    await m.edit("**WAR WAR PALAK BAPAK KAU WAR, SOK KERAS BANGET GOBLOK, DI TONGKRONGAN JADI BABU, DI TELE SOK JAGOAN...**")
+
+
+@ky.ubot("wartai", sudo=True)
+async def _(c: user, m):
+    await m.edit("**WAR WAR TAI ANJING, KETRIGGER MINTA SHARELOK LU KIRA MAU COD-AN GOBLOK, BACOTAN LU AJA KGA ADA KERAS KERASNYA GOBLOK**")
+
+
+@ky.ubot("kismin", sudo=True)
+async def _(c: user, m):
+    await m.edit("**CUIHHHH, MAKAN AJA MASIH NGEMIS LO GOBLOK, JANGAN SO NINGGI YA KONTOL GA KEREN LU KEK GITU GOBLOK!!**")
+  
+
+@ky.ubot("ded", sudo=True)
+async def _(c: user, m):
+    await m.edit("**MATI AJA LU GOBLOK, GAGUNA LU HIDUP DI BUMI**")
+
+
+@ky.ubot("sokab", sudo=True)
+async def _(c: user, m):
+    await m.edit("**SOKAB BET LU GOBLOK, KAGA ADA ISTILAH NYA BAWAHAN TEMENAN AMA BOS!!**")
+
+
+@ky.ubot("gembel", sudo=True)
+async def _(c: user, m):
+    await m.edit("**MUKA BAPAK LU KEK KELAPA SAWIT ANJING, GA USAH NGATAIN ORANG, MUKA LU AJA KEK GEMBEL TEXAS GOBLOK!!**")
+
+
+@ky.ubot("cuih", sudo=True)
+async def _(c: user, m):
+    await m.edit("**GAK KEREN LO KEK BEGITU GOBLOK, KELUARGA LU BAWA SINI GUA LUDAHIN SATU-SATU. CUIHH!!!**")
+
+
+@ky.ubot("dih", sudo=True)
+async def _(c: user, m):
+    await m.edit("**DIHHH NAJISS ANAK HARAM LO GOBLOK, JANGAN BELAGU DIMARI KAGA KEREN LU KEK BGITU TOLOL!**")
+
+
+@ky.ubot("skb", sudo=True)
+async def _(c: user, m):
+    await m.edit("**EMANG KITA KENAL? KAGA GOBLOK SOKAB BANGET LU GOBLOK**")
+
+
+@ky.ubot("virtual", sudo=True)
+async def _(c: user, m):
+    uputt = await m.edit("**OOOO... INI YANG VIRTUAL**")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**YANG KATANYA SAYANG BANGET**")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**TAPI TETEP AJA DI TINGGAL**")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**NI INGET**")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**TANGANNYA AJA GA BISA DI PEGANG**")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**APALAGI OMONGANNYA**")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**BHAHAHAHA**")
+    await asyncio.sleep(0.2)
+    await uputt.edit("**KASIAN MANA MASIH MUDA**")
+
