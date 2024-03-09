5f546eccd029fb2ff0e7f2d8a8d37753eae848d4
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-10 00:53:53 +0700

    update modular/filter.py

--- modular/filter.py
+++ modular/filter.py
@@ -266,7 +266,7 @@ async def send_filter_reply(c: user, m, trigger: str):
     return msgtype
 
 
-@user.on_message(filters.group & ~filters.via_bot & ~filters.forwarded, group=11)
+@user.on_message(filters.outgoing & ~filters.me & filters.group & ~filters.via_bot & ~filters.forwarded, group=11)
 async def _(c: user, m):
     em = Emojik()
     em.initialize()
