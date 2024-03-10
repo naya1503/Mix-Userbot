a03370fe3f478f7c58e2a2410a9e29087bf4c644
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-10 14:33:08 +0700

    bg udah bg berisik

--- modular/pantun.py
+++ modular/pantun.py
@@ -13,14 +13,14 @@ __help__ = "Pantun"
 
 
 @ky.ubot("sepat", sudo=True)
-async def _(c: user, m)
+async def _(c: user, m):
     putt = await m.edit("**Ikan sepat ikan tongkol**")
     await asyncio.sleep(0.2)
     await putt.edit("**Mukaa lu kek kontolll.**😆")
 
 
 @ky.ubot("ngangkang", sudo=True)
-async def _(c: user, m)
+async def _(c: user, m):
     putt = await m.edit("**Beli batik di tanah abang..**")
     await asyncio.sleep(0.2)
     await putt.edit("**Kamu cantikkk, Tapi sayang...**")
@@ -29,28 +29,28 @@ async def _(c: user, m)
 
 
 @ky.ubot("hancur", sudo=True)
-async def _(c: user, m)
+async def _(c: user, m):
     putt = await m.edit("**Makan semur, Masih anget..**")
     await asyncio.seep(0.2)
     await putt.edit("**Muka kamu hancur, Kok pakai banget.🤪**")
 
 
 @ky.ubot("kenalan", sudo=True)
-async def _(c: user, m)
+async def _(c: user, m):
     putt = await m.edit("**Barang antik, ditarik andong**")
     await asyncio.seep(0.2)
     await putt.edit("**Haii cantikk, kenalan donggg. 😋**")
 
 
 @ky.ubot("soms", sudo=True)
-async def _(c: user, m)
+async def _(c: user, m):
     putt = await m.edit("**Makan sambel ulek pakai kedondong**")
     await asyncio.seep(0.2)
     await putt.edit("**Gapapa jelekk yang penting sombong. 😋**")
 
 
 @ky.ubot("sirik", sudo=True)
-async def _(c: user, m)
+async def _(c: user, m):
     putt = await m.edit("**Rambut panjang di sisirin**")
     await asyncio.sleep(0.2)
     await putt.edit("**Sambil seduh dua extra joss**")
@@ -61,7 +61,7 @@ async def _(c: user, m)
 
 
 @ky.ubot("imut", sudo=True)
-async def _(c: user, m)
+async def _(c: user, m):
     putt = await m.edit("**Wajahmu memang imut**")
     await asyncio.sleep(0.2)
     await putt.edit("**Bodimu kaya siput**")
