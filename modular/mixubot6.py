16fc5dbaa015fa04123cb084da20c4c9325137d3
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-10 13:28:14 +0700

    update modular/mixubot6.py

--- modular/mixubot6.py
+++ modular/mixubot6.py
@@ -1,4 +1,12 @@
-# Part Of Ayiin-Userbot
+# Ayiin - Userbot
+# Copyright (C) 2022-2023 @AyiinXd
+#
+# This file is a part of < https://github.com/AyiinXd/Ayiin-Userbot >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/AyiinXd/Ayiin-Userbot/blob/main/LICENSE/>.
+#
+# FROM Ayiin-Userbot <https://github.com/AyiinXd/Ayiin-Userbot>
+# t.me/AyiinXdSupport & t.me/AyiinSupport
 
 import asyncio
 
