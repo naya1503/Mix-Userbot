a44f44125559c94187eb7adb8d7d841b9cf0567b
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-11 03:30:54 +0700

    fixing copy

--- assistant/call_copy.py
+++ assistant/call_copy.py
@@ -15,6 +15,7 @@ from pyrogram.types import *
 from modular.copy_con import *
 
 
+"""
 @ky.callback("copymsg_")
 async def _(c, cq):
     global nyolong_jalan
@@ -33,6 +34,31 @@ async def _(c, cq):
                 m.chat.id, bot.me.username, g.id, reply_to_message_id=msg.id
             )
             await m._c.delete_messages(m.chat.id, COPY_ID[m._c.me.id])
+            await g.delete()
+            nyolong_jalan = False
+    except Exception as e:
+        await cq.edit_message_text(cgr("err_1").format(e))
+"""
+
+
+@ky.callback("copymsg_")
+async def _(c, cq):
+    global nyolong_jalan
+    try:
+        q = int(cq.data.split("_", 1)[1])
+        m = [obj for obj in get_objects() if id(obj) == q][0]
+        await m.c.unblock_user(bot.me.username)
+        await cq.edit_message_text(cgr("proses_1"))
+        copy = await m.c.send_message(bot.me.username, f"/copy {m.text.split()[1]}")
+        msg = m.reply_to_message or m
+        await asyncio.sleep(1.5)
+        await copy.delete()
+        nyolong_jalan = True
+        async for g in m.c.search_messages(bot.me.username, limit=1):
+            await m.c.copy_message(
+                m.chat.id, bot.me.username, g.id, reply_to_message_id=msg.id
+            )
+            await m.c.delete_messages(m.chat.id, COPY_ID[m.c.me.id])
             await g.delete()
             nyolong_jalan = False
     except Exception as e:
