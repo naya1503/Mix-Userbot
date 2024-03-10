d614f4a1ec5fe53a7d9f3de149b71059cb807ded
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-10 13:15:15 +0700

    update modular/filter.py

--- modular/filter.py
+++ modular/filter.py
@@ -244,19 +244,20 @@ async def send_filter_reply(c: user, m, trigger: str):
 async def _(c: user, m):
     em = Emojik()
     em.initialize()
-    text = m.text.lower().strip()
-    if not text:
-        return
     chat_filters = db.get_all_filters(m.chat.id)
     actual_filters = {j for i in chat_filters for j in i.split("|")}
 
-    for word in actual_filters:
-        pattern = r"( |^|[^\w])" + re.escape(word) + r"( |$|[^\w])"
-        if re.search(pattern, text, flags=re.IGNORECASE):
+    for trigger in actual_filters:
+        pattern = r"( |^|[^\w])" + re_escape(trigger) + r"(|$|[^\w])"
+        match = await regex_searcher(pattern, m.text.lower())
+        if match:
             try:
-                await send_filter_reply(c, m, word)
+                msgtype = await send_filter_reply(c, m, trigger)
             except Exception as ef:
-                await m.reply_text(cgr("err").format(em.gagal, ef))
+                await m.reply_text(f"Error: {ef}")
+                LOGGER.error(ef)
+                LOGGER.error(format_exc())
             break
         continue
     return
+
