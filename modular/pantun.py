0ece4f3436fb16b0301ea25efdfd54da610a35ce
iamuput <yogaaasp73@gmail.com>
2024-03-10 14:47:24 +0700

    tjakeppp

--- modular/pantun.py
+++ modular/pantun.py
@@ -14,57 +14,65 @@ __help__ = "Pantun"
 @ky.ubot("sepat", sudo=True)
 async def _(c: user, m):
     putt = await m.edit("**Ikan sepat ikan tongkol**")
-    await asyncio.sleep(1)
+    await asyncio.sleep(2)
+    await putt.edit("**CAKEEEEPPPPP**")
+    await asyncio.sleep(2)
     await putt.edit("**Mukaa lu kek kontolll.**😆")
 
 
 @ky.ubot("ngangkang", sudo=True)
 async def _(c: user, m):
     putt = await m.edit("**Beli batik di tanah abang..**")
-    await asyncio.sleep(1)
+    await asyncio.sleep(2)
     await putt.edit("**Kamu cantikkk, Tapi sayang...**")
-    await asyncio.sleep(1)
+    await asyncio.sleep(2)
     await putt.edit("**Hobby nya ngangkang** 😆🤪")
 
 
 @ky.ubot("hancur", sudo=True)
 async def _(c: user, m):
     putt = await m.edit("**Makan semur, Masih anget..**")
-    await asyncio.sleep(1)
+    await asyncio.sleep(2)
+    await putt.edit("**CAKEEEEPPPPP**")
+    await asyncio.sleep(2)
     await putt.edit("**Muka kamu hancur, Kok pakai banget.🤪**")
 
 
 @ky.ubot("kenalan", sudo=True)
 async def _(c: user, m):
     putt = await m.edit("**Barang antik, ditarik andong**")
-    await asyncio.sleep(1)
+    await asyncio.sleep(2)
+    await putt.edit("**CAKEEEEPPPPP**")
+    await asyncio.sleep(2)
     await putt.edit("**Haii cantikk, kenalan donggg. 😋**")
 
 
 @ky.ubot("soms", sudo=True)
 async def _(c: user, m):
     putt = await m.edit("**Makan sambel ulek pakai kedondong**")
-    await asyncio.sleep(1)
+    await asyncio.sleep(2)
+    await putt.edit("**CAKEEEEPPPPP**")
+    await asyncio.sleep(2)
     await putt.edit("**Gapapa jelekk yang penting sombong. 😋**")
 
 
 @ky.ubot("sirik", sudo=True)
 async def _(c: user, m):
     putt = await m.edit("**Rambut panjang di sisirin**")
-    await asyncio.sleep(1)
+    await asyncio.sleep(2)
     await putt.edit("**Sambil seduh dua extra joss**")
-    await asyncio.sleep(1)
+    await asyncio.sleep(2)
     await putt.edit("**Cerita dikit di nyinyirin**")
-    await asyncio.sleep(1)
+    await asyncio.sleep(2)
     await putt.edit("**Kalo sirikk bilang bosss..**🖕")
 
 
 @ky.ubot("imut", sudo=True)
 async def _(c: user, m):
     putt = await m.edit("**Wajahmu memang imut**")
-    await asyncio.sleep(1)
+    await asyncio.sleep(2)
     await putt.edit("**Bodimu kaya siput**")
-    await asyncio.sleep(1)
+    await asyncio.sleep(2)
     await putt.edit("**Tingkahmu membuatku salutt**")
-    await asyncio.sleep(1)
+    await asyncio.sleep(2)
     await putt.edit("**Tapi sayang hobimu kentutt.** 😭🤌")
