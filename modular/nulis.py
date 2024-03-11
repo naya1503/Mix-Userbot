772b29e29c0aee7101855d7fde6f3e3c66adec8e
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-11 20:34:50 +0700

    update modular/nulis.py

---
+++ modular/nulis.py
@@ -0,0 +1,39 @@
+################################################################
+"""
+ Mix-Userbot Open Source . Maintained ? Yes Oh No Oh Yes Ngentot
+ 
+ @ CREDIT : NAN-DEV || EH KONTOL KALO PUNYA AKAL DIPAKE YA ANJING GAUSAH APUS² CREDIT MODAL NYOPAS LO BAJINGAN!!
+"""
+################################################################
+
+import os
+
+from SafoneAPI import SafoneAPI
+
+from Mix import *
+
+
+__modles__ = "Nulis"
+__help__ = "Nulis"
+
+async def nulis(text, page=None, font=None, color="black"):
+    meki = SafoneAPI()
+    img = await meki.write(text, page, font, color)
+    with open("nulis.png", "wb") as file:
+        file.write(img.getvalue())
+    return "nulis.png"
+
+
+
+@ky.ubot("nulis|write", sudo=True)
+async def _(c, m):
+    em = Emojik()
+    em.initialize()
+    rep = m.reply_to_message
+    if rep:
+        text = rep.text or rep.caption
+    else:
+        text = m.text.split(None, 1)[1]
+    meko = await nulis(text)
+    await m.reply_photo(meko)
+    os.remove(meko)
