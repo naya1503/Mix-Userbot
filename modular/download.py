54810c30460e6cf0dad6a663dcadd38ecb0feffd
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-11 22:05:32 +0700

    add module downloader

---
+++ modular/download.py
@@ -0,0 +1,54 @@
+################################################################
+"""
+ Mix-Userbot Open Source . Maintained ? Yes Oh No Oh Yes Ngentot
+ 
+ @ CREDIT : NAN-DEV
+"""
+################################################################
+
+
+import asyncio
+import aiohttp
+from pyrogram.enums import *
+from pyrogram.errors import *
+from pyrogram.types import *
+
+from Mix import *
+
+
+__modles__ = "Download"
+__help__ = ("Download")
+
+@ky.ubot("sosmed", sudo=True)
+async def _(c, m):
+    if len(m.command) < 2:
+        await m.reply_text("Gunakan perintah `/sosmed [URL_MEDIA]` untuk mengunduh media dari sosial media.")
+        return
+
+    url = m.command[1]
+
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                if resp.status == 200:
+                    filename = url.split("/")[-1]
+                    with open(filename, "wb") as f:
+                        f.write(await resp.read())
+
+                    await c.send_message(
+                        chat_id=message.chat.id,
+                        text="Media berhasil diunduh!",
+                        reply_to_message_id=message.message_id
+                    )
+
+                    await c.send_document(
+                        chat_id=message.chat.id,
+                        document=filename,
+                        caption="Berikut adalah media yang Anda unduh dari sosial media."
+                    )
+
+                else:
+                    await m.reply_text(f"Gagal mengunduh media dengan status {resp.status}")
+
+    except Exception as e:
+        await m.reply_text(f"Terjadi kesalahan: {str(e)}")
