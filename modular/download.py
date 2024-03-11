61ad7f8d743b8adc7525dee6e9598037947b76cd
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-12 00:09:06 +0700

    add some module

--- modular/download.py
+++ modular/download.py
@@ -38,3 +38,23 @@ async def _(self: user, m):
     ulat = await user.resolve_peer(hm)
     await user.invoke(DeleteHistory(peer=ulat, max_id=0, revoke=True))
     return
+
+
+@ky.ubot("dtube", sudo=False)
+async def _(self: user, m):
+    em = Emojik()
+    em.initialize()
+    hm = "luciferbukanrobot_bot"
+    await user.unblock_user(hm)
+    await user.send_message(hm, f"/youtube {m.command[1]}")
+    pros = await m.reply(cgr("proses").format(em.proses))
+    ai = await user.forward_messages(hm, m.chat.id, message_ids=m.id)
+    await user.send_message(hm, "/youtube", reply_to_message_id=ai.id)
+    await asyncio.sleep(5)
+    async for tai in user.search_messages(hm, limit=1):
+        await asyncio.sleep(5)
+        await tai.copy(m.chat.id)
+    await pros.delete()
+    ulat = await user.resolve_peer(hm)
+    await user.invoke(DeleteHistory(peer=ulat, max_id=0, revoke=True))
+    return
