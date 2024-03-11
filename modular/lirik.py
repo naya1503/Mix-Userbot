3f10f17a8d5d48fba717e30f6a5fb08af6d0e9d9
jonesroot <jonesroot@users.noreply.github.com>
2024-03-11 05:59:11 +0000

    Auto Fix

--- modular/lirik.py
+++ modular/lirik.py
@@ -22,27 +22,30 @@ async def _(c, m):
 
     lirik = api.search_song(song_name)
 
-    if lirik is None:return await load.edit(f"{em.gagal} <b>Tidak dapat menemukan lyric untuk : </b> {m.command[1]}")
+    if lirik is None:
+        return await load.edit(
+            f"{em.gagal} <b>Tidak dapat menemukan lyric untuk : </b> {m.command[1]}"
+        )
 
     lyric_title = lirik.title
     lyric_artist = lirik.artist
     lyrics_text = lirik.lyrics
 
     try:
-        await load.edit_text(f"--**{lyric_title}**--\n{lyric_artist}\n\n\n{lyrics_text}\n__Extracted by {bot.me.username}")
+        await load.edit_text(
+            f"--**{lyric_title}**--\n{lyric_artist}\n\n\n{lyrics_text}\n__Extracted by {bot.me.username}"
+        )
 
     except MessageTooLong:
         with open(f"downloads/{lyric_title}.txt", "w") as f:
             f.write(f"{lyric_title}\n{lyric_artist}\n\n\n{lyrics_text}")
 
         await load.edit_text("Lyric too long. Sending as a text file...")
-        await m.reply_chat_action(
-            action="upload_document"
-        )
+        await m.reply_chat_action(action="upload_document")
         await m.reply_document(
             document=f"downloads/{lyric_title}.txt",
             thumb="https://telegra.ph//file/43cec0ae0ded594b55247.jpg",
-            caption=f"\n__--{lyric_title}--\n__{lyric_artist}\n\n__Extracted by {bot.me.username}"
+            caption=f"\n__--{lyric_title}--\n__{lyric_artist}\n\n__Extracted by {bot.me.username}",
         )
 
         await load.delete()
