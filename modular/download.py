75a8fde7a36b42a8bfc413272cf195f74b3127e6
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-11 22:26:14 +0700

    laksdhalskh

--- modular/download.py
+++ modular/download.py
@@ -8,6 +8,7 @@
 
 
 import aiohttp
+import mimetypes
 from pyrogram.enums import *
 from pyrogram.errors import *
 from pyrogram.types import *
@@ -32,21 +33,34 @@ async def _(c, m):
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as resp:
                 if resp.status == 200:
-                    filename = url.split("/")[-1]
+                    content_type = resp.headers.get("content-type", "").split(";")[0]
+
+                    ext = mimetypes.guess_extension(content_type.split("/")[1])
+
+                    filename = f"media{ext}"
                     with open(filename, "wb") as f:
                         f.write(await resp.read())
 
-                    # await c.send_message(
-                    #     chat_id=m.chat.id,
-                    #     text="Media berhasil diunduh!",
-                    #     reply_to_message_id=m.message_id,
-                    # )
-
-                    await c.send_document(
-                        chat_id=m.chat.id,
-                        document=filename,
-                        caption="Berikut adalah media yang Anda unduh dari sosial media.",
-                    )
+                    if content_type.startswith("image"):
+                        await c.send_photo(
+                            chat_id=m.chat.id,
+                            photo=filename,
+                            caption="Berikut adalah foto yang Anda unduh dari sosial media.",
+                        )
+                    elif content_type.startswith("video"):
+                        await c.send_video(
+                            chat_id=m.chat.id,
+                            video=filename,
+                            caption="Berikut adalah video yang Anda unduh dari sosial media.",
+                        )
+                    elif content_type.startswith("audio"):
+                        await c.send_audio(
+                            chat_id=m.chat.id,
+                            audio=filename,
+                            caption="Berikut adalah audio yang Anda unduh dari sosial media.",
+                        )
+                    else:
+                        await m.reply_text("Media yang diunduh tidak didukung.")
 
                 else:
                     await m.reply_text(
