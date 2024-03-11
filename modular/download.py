8a92575439aa155cf4bf74681a40addf489bbc68
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-11 23:58:52 +0700

    fixing download

--- modular/download.py
+++ modular/download.py
@@ -18,27 +18,23 @@ __modles__ = "Download"
 __help__ = "Download"
 
 
-async def kirim_pesan_dan_tunggu(chat_id, pesan):
-    sent_message = await user.send_message(chat_id, pesan)
-    await asyncio.sleep(10)
-    replied_message = await user.get_messages(chat_id, sent_message.message_id)
-    await user.send_message(chat_id, replied_message.text)
-
-
-@ky.ubot("sos", sudo=True)
-async def _(c, m):
-    _, *args = m.text.split()
-
-    if args and args[0].startswith("{") and args[0].endswith("}"):
-        command = args[0].strip("{}")
-
-        if command == "tik":
-            if len(args) >= 2:
-                link_tiktok = args[1]
-                pesan_tiktok = f"/tiktok {link_tiktok}"
-                await kirim_pesan_dan_tunggu(m.chat.id, pesan_tiktok)
-        elif command == "tube":
-            if len(args) >= 2:
-                link_youtube = args[1]
-                pesan_youtube = f"/youtube {link_youtube}"
-                await kirim_pesan_dan_tunggu(m.chat.id, pesan_youtube)
+@ky.ubot("dtik", sudo=False)
+async def _(self: user, m):
+    em = Emojik()
+    em.initialize()
+    hm = "luciferbukanrobot_bot"
+    await user.unblock_user(hm.me.username)
+    await user.send_message(hm.me.username, "/tiktok", {m.command[1]})
+    pros = await m.reply(cgr("proses").format(em.proses))
+    ai = await user.forward_messages(hm.me.username, m.chat.id, message_ids=m.id)
+    await user.send_message(hm.me.username, "/kang", reply_to_message_id=ai.id)
+    await asyncio.sleep(5)
+    async for tai in user.search_messages(
+        hm.me.username, limit=1
+    ):
+        await asyncio.sleep(5)
+        await tai.copy(m.chat.id)
+    await pros.delete()
+    ulat = await user.resolve_peer(hm.me.username)
+    await user.invoke(DeleteHistory(peer=ulat, max_id=0, revoke=True))
+    return
