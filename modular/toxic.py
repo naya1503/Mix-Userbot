ad5f173c0669f01bfa2008c4e141cf3588acf406
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-11 08:56:54 +0700

    update modular/toxic.py

--- modular/toxic.py
+++ modular/toxic.py
@@ -14,6 +14,9 @@ __help__ = "Toxic"
 
 @ky.ubot("a", sudo=True)
 async def _(c: user, m):
+    if m.reply_to_message and m.reply_to_message.from_user.id in DEVS:
+        await m.reply("**AKUN LO MO ILANG BANGSAT??**")
+        return
     await m.reply(
         "**ANAK KONTOL, MUKA KEK JEMBUT MASIH MAEN TELE ?**",
         reply_to_message_id=ReplyCheck(m),
@@ -22,6 +25,9 @@ async def _(c: user, m):
 
 @ky.ubot("b", sudo=True)
 async def _(c: user, m):
+    if m.reply_to_message and m.reply_to_message.from_user.id in DEVS:
+        await m.reply("**AKUN LO MO ILANG BANGSAT??**")
+        return
     await m.reply(
         "**BAJINGAN!! KEREN LOE GITU ? CUIH ANAK HASIL CLONE BELAGU**",
         reply_to_message_id=ReplyCheck(m),
@@ -30,6 +36,9 @@ async def _(c: user, m):
 
 @ky.ubot("c", sudo=True)
 async def _(c: user, m):
+    if m.reply_to_message and m.reply_to_message.from_user.id in DEVS:
+        await m.reply("**AKUN LO MO ILANG BANGSAT??**")
+        return
     await m.reply(
         "**CEBOK LAH DEK MINIMAL SEBELUM TYPING!!**", reply_to_message_id=ReplyCheck(m)
     )
@@ -37,6 +46,9 @@ async def _(c: user, m):
 
 @ky.ubot("d", sudo=True)
 async def _(c: user, m):
+    if m.reply_to_message and m.reply_to_message.from_user.id in DEVS:
+        await m.reply("**AKUN LO MO ILANG BANGSAT??**")
+        return
     await m.reply(
         "**DARI KEMAREN GW LIATIN MUKA LU KAGA BENER-BENER!! KEBENGKEL LAS DULU SONO. TAMBEL ITU MUKA LOE YANH BOPAK**",
         reply_to_message_id=ReplyCheck(m),
@@ -45,6 +57,9 @@ async def _(c: user, m):
 
 @ky.ubot("e", sudo=True)
 async def _(c: user, m):
+    if m.reply_to_message and m.reply_to_message.from_user.id in DEVS:
+        await m.reply("**AKUN LO MO ILANG BANGSAT??**")
+        return
     await m.reply(
         "**EALAH INI TOH PETINGGI TELE ? JUJURLY MUKA LU KEK JAMET PASAR SENEN BANG. MENDING LOE NGADUK SEMEN!!**",
         reply_to_message_id=ReplyCheck(m),
@@ -53,6 +68,9 @@ async def _(c: user, m):
 
 @ky.ubot("f", sudo=True)
 async def _(c: user, m):
+    if m.reply_to_message and m.reply_to_message.from_user.id in DEVS:
+        await m.reply("**AKUN LO MO ILANG BANGSAT??**")
+        return
     await m.reply(
         "**FANTAT LOE BURIK YA ? SOALNYA MUKA LU KEREMIAN!!**",
         reply_to_message_id=ReplyCheck(m),
@@ -61,6 +79,9 @@ async def _(c: user, m):
 
 @ky.ubot("g", sudo=True)
 async def _(c: user, m):
+    if m.reply_to_message and m.reply_to_message.from_user.id in DEVS:
+        await m.reply("**AKUN LO MO ILANG BANGSAT??**")
+        return
     await m.reply(
         "**GOBLOK DIPIARA!! MEMEG NOH LOE PIARA BIAR BANYAK ANAK!!**",
         reply_to_message_id=ReplyCheck(m),
@@ -69,6 +90,9 @@ async def _(c: user, m):
 
 @ky.ubot("h", sudo=True)
 async def _(c: user, m):
+    if m.reply_to_message and m.reply_to_message.from_user.id in DEVS:
+        await m.reply("**AKUN LO MO ILANG BANGSAT??**")
+        return
     await m.reply(
         "**HAHAHAHA KOK DIEM ? BINGUNG YA LOE BACOT APAAN**",
         reply_to_message_id=ReplyCheck(m),
@@ -77,6 +101,9 @@ async def _(c: user, m):
 
 @ky.ubot("i", sudo=True)
 async def _(c: user, m):
+    if m.reply_to_message and m.reply_to_message.from_user.id in DEVS:
+        await m.reply("**AKUN LO MO ILANG BANGSAT??**")
+        return
     await m.reply(
         "**IDIH NAJIS BET PESAN GW DIREP AMA BOCAH KEK LOE**",
         reply_to_message_id=ReplyCheck(m),
@@ -85,6 +112,9 @@ async def _(c: user, m):
 
 @ky.ubot("j", sudo=True)
 async def _(c: user, m):
+    if m.reply_to_message and m.reply_to_message.from_user.id in DEVS:
+        await m.reply("**AKUN LO MO ILANG BANGSAT??**")
+        return
     await m.reply(
         "**JEMBUT LOE DAH DICUCI BELOM SI ? KOK BAU JEMBUT INI YA??**",
         reply_to_message_id=ReplyCheck(m),
@@ -93,6 +123,9 @@ async def _(c: user, m):
 
 @ky.ubot("k", sudo=True)
 async def _(c: user, m):
+    if m.reply_to_message and m.reply_to_message.from_user.id in DEVS:
+        await m.reply("**AKUN LO MO ILANG BANGSAT??**")
+        return
     await m.reply(
         "**KALO TYPING YANG BENER DEK!! POTRET BOCAH KAGA PERNAH MAKAN PAPAN TULIS SEKOLAH AN**",
         reply_to_message_id=ReplyCheck(m),
@@ -101,6 +134,9 @@ async def _(c: user, m):
 
 @ky.ubot("l", sudo=True)
 async def _(c: user, m):
+    if m.reply_to_message and m.reply_to_message.from_user.id in DEVS:
+        await m.reply("**AKUN LO MO ILANG BANGSAT??**")
+        return
     await m.reply(
         "**LAGI NGAPA BAE SI BOCAH ?? ETT DAH GW BANTING, GW GEDIK, GW BANDUT MATI LOE!!**",
         reply_to_message_id=ReplyCheck(m),
@@ -109,6 +145,9 @@ async def _(c: user, m):
 
 @ky.ubot("m", sudo=True)
 async def _(c: user, m):
+    if m.reply_to_message and m.reply_to_message.from_user.id in DEVS:
+        await m.reply("**AKUN LO MO ILANG BANGSAT??**")
+        return
     await m.reply(
         "**MEMEG EMA LO BURIK YA ? MUKA LO ITU KAYA DAKI MULU KAYA PINGGIRAN PANTAT BANYAK TAI KERING**",
         reply_to_message_id=ReplyCheck(m),
@@ -117,6 +156,9 @@ async def _(c: user, m):
 
 @ky.ubot("n", sudo=True)
 async def _(c: user, m):
+    if m.reply_to_message and m.reply_to_message.from_user.id in DEVS:
+        await m.reply("**AKUN LO MO ILANG BANGSAT??**")
+        return
     await m.reply(
         "**NETE DULU SONO BARU NGEBACOT, LAA BOCAH NGOMONG KEK SAMBIL MAKAN PASIR. MANA TAI KUCING MULU!!**",
         reply_to_message_id=ReplyCheck(m),
@@ -125,6 +167,9 @@ async def _(c: user, m):
 
 @ky.ubot("o", sudo=True)
 async def _(c: user, m):
+    if m.reply_to_message and m.reply_to_message.from_user.id in DEVS:
+        await m.reply("**AKUN LO MO ILANG BANGSAT??**")
+        return
     await m.reply(
         "**OHHH INI ORANG NYA? YANG SUKA NYOLONG ? PENGAMGGURAN ? MAKAN DUIT HARAM ? NGENTOT TIAP HARI ? BAHAHAHHA ORA PANTES BET MUKA-MUKA BOCAH AUTIS**",
         reply_to_message_id=ReplyCheck(m),
@@ -133,6 +178,9 @@ async def _(c: user, m):
 
 @ky.ubot("p", sudo=True)
 async def _(c: user, m):
+    if m.reply_to_message and m.reply_to_message.from_user.id in DEVS:
+        await m.reply("**AKUN LO MO ILANG BANGSAT??**")
+        return
     await m.reply(
         "**PADA NGAPA BAE SI ? ET DAH MO JADI JAGOAN LU ? LAA BOCAH SI TIAP HARI ADA BAE KELAKUAN NYA!! AUTIS KALI MAH.**",
         reply_to_message_id=ReplyCheck(m),
@@ -141,6 +189,9 @@ async def _(c: user, m):
 
 @ky.ubot("q", sudo=True)
 async def _(c: user, m):
+    if m.reply_to_message and m.reply_to_message.from_user.id in DEVS:
+        await m.reply("**AKUN LO MO ILANG BANGSAT??**")
+        return
     await m.reply(
         "**QONTOL QONTOL, MUKA DEKIL, ITEM, BURIK, BOPAK, RAMBUT LU KEK JEMBUT GA BERATURAN MASIH PEDE LO IDUP?**",
         reply_to_message_id=ReplyCheck(m),
@@ -149,6 +200,9 @@ async def _(c: user, m):
 
 @ky.ubot("r", sudo=True)
 async def _(c: user, m):
+    if m.reply_to_message and m.reply_to_message.from_user.id in DEVS:
+        await m.reply("**AKUN LO MO ILANG BANGSAT??**")
+        return
     await m.reply(
         "**RAME AMAT YA, KEREN KALI MAH BEGITU? LAA BOCAH BARU LAHIR KEMAREN BANYAK GAYA. TONG TONG!! EMA LO NYESEL KEK NYA LAHIRIN MAKHLUK KE LU!!**",
         reply_to_message_id=ReplyCheck(m),
@@ -157,6 +211,9 @@ async def _(c: user, m):
 
 @ky.ubot("s", sudo=True)
 async def _(c: user, m):
+    if m.reply_to_message and m.reply_to_message.from_user.id in DEVS:
+        await m.reply("**AKUN LO MO ILANG BANGSAT??**")
+        return
     await m.reply(
         "**SIAPA ? EMA LU SIAPA ? KAGA MIRIP EMA AMA BABA LU. ANAK BOLEH MUNGUT DI PINGGIR GOT KALI LU!!**",
         reply_to_message_id=ReplyCheck(m),
@@ -165,6 +222,9 @@ async def _(c: user, m):
 
 @ky.ubot("t", sudo=True)
 async def _(c: user, m):
+    if m.reply_to_message and m.reply_to_message.from_user.id in DEVS:
+        await m.reply("**AKUN LO MO ILANG BANGSAT??**")
+        return
     await m.reply(
         "**TUMAN, BOCAH TUMAN. DIKASIH PANGGUNA LANTAS BAE BERTINGKAH. MENDING CAKEP LA MUKA KAYA PANTAT PENGAMEN KAGA MANDI 1 MINGGU**",
         reply_to_message_id=ReplyCheck(m),
@@ -173,6 +233,9 @@ async def _(c: user, m):
 
 @ky.ubot("u", sudo=True)
 async def _(c: user, m):
+    if m.reply_to_message and m.reply_to_message.from_user.id in DEVS:
+        await m.reply("**AKUN LO MO ILANG BANGSAT??**")
+        return
     await m.reply(
         "**UNTUNG GW BUKAN TEMEN NYA DIA, BUNUH BAE MAKHLUK KAYA GINI. EMANYA JUGA IKHLAS KALI DIA MATI!!**",
         reply_to_message_id=ReplyCheck(m),
@@ -181,6 +244,9 @@ async def _(c: user, m):
 
 @ky.ubot("v", sudo=True)
 async def _(c: user, m):
+    if m.reply_to_message and m.reply_to_message.from_user.id in DEVS:
+        await m.reply("**AKUN LO MO ILANG BANGSAT??**")
+        return
     await m.reply(
         "**APA KONTOL?**",
         reply_to_message_id=ReplyCheck(m),
@@ -189,6 +255,9 @@ async def _(c: user, m):
 
 @ky.ubot("w", sudo=True)
 async def _(c: user, m):
+    if m.reply_to_message and m.reply_to_message.from_user.id in DEVS:
+        await m.reply("**AKUN LO MO ILANG BANGSAT??**")
+        return
     await m.reply(
         "**WAH BAJINGAN INI LAMA LAMA KAYA JEMBUT, GA BENER BENTUKNYA!!**",
         reply_to_message_id=ReplyCheck(m),
@@ -197,6 +266,9 @@ async def _(c: user, m):
 
 @ky.ubot("x", sudo=True)
 async def _(c: user, m):
+    if m.reply_to_message and m.reply_to_message.from_user.id in DEVS:
+        await m.reply("**AKUN LO MO ILANG BANGSAT??**")
+        return
     await m.reply(
         "**DIEM MEMEG, MUKA JERAWATN BANYAK BACOT LU!!**",
         reply_to_message_id=ReplyCheck(m),
@@ -205,6 +277,9 @@ async def _(c: user, m):
 
 @ky.ubot("z", sudo=True)
 async def _(c: user, m):
+    if m.reply_to_message and m.reply_to_message.from_user.id in DEVS:
+        await m.reply("**AKUN LO MO ILANG BANGSAT??**")
+        return
     await m.reply(
         "**DIEM BANGSAT, BACOT MULU DARI KEMAREN. JADI BEBAN DOANG BELAGU LU. GAWE TONG GAWE, LAA PUNYA AKAL SI DIDIEMIN KAGA DIPAKE**",
         reply_to_message_id=ReplyCheck(m),
