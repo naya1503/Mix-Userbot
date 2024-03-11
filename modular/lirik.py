bdfe34f7a22ef20753a978395ae9fc32ef29d824
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-11 17:10:19 +0700

    fixing lirik

--- modular/lirik.py
+++ modular/lirik.py
@@ -1,45 +1,12 @@
+from urllib.request import Request, urlopen
 import json
 import urllib.parse
-from urllib.request import Request, urlopen
-
 from pyrogram import *
 from pyrogram.types import *
-
 from Mix import *
 
 __modles__ = "Lyric"
 __help__ = "Lyrics"
-
-
-@ky.ubot("lirik", sudo=True)
-async def _(c, m):
-    em = Emojik()
-    em.initialize()
-    try:
-        p = m.reply(f"{em.proses} <b>Sedang mencari lirik lagu</>")
-        command = " ".join(m.command[1:])
-        parts = command.split("-")
-        if len(parts) != 2:
-            await m.reply(
-                f"{em.gagal} <b>Format perintah salah. Gunakan format: /lirik nama-penyanyi - judul-lagu</b>"
-            )
-            return
-
-        penyanyi = parts[0].strip()
-        judul = parts[1].strip()
-        lyrics_text = await search_lyrics(penyanyi, judul)
-        if not lyrics_text:
-            lyrics_text = await search_lyrics(judul, penyanyi)
-
-        if lyrics_text:
-            await m.reply_text(f"{em.sukses} <code>{lyrics_text}</code>")
-            await p.delete()
-        else:
-            await m.reply_text(f"{em.gagal} <b>Maaf, lirik lagu tidak ditemukan.</b>")
-            await p.delete()
-    except Exception as e:
-        await m.reply(f"{em.gagal} <b>error :</b> <code>{e}</code>")
-        await p.delete()
 
 
 async def search_lyrics(penyanyi, judul):
@@ -59,3 +26,34 @@ async def search_lyrics(penyanyi, judul):
                 return None
     except Exception as e:
         return None
+
+
+@ky.ubot("lirik", sudo=True)
+async def _(c, m):
+    em = Emojik()
+    em.initialize()
+    try:
+        pft = await m.reply(f"{em.proses} <b>Sedang mencari lirik lagu</b>")
+        command = " ".join(m.command[1:])
+        parts = command.split("-")
+        if len(parts) != 2:
+            await m.reply(
+                f"{em.gagal} <b>Format perintah salah. Gunakan format: /lirik nama-penyanyi - judul-lagu</b>"
+            )
+            return
+
+        penyanyi = parts[0].strip()
+        judul = parts[1].strip()
+        lyrics_text = await search_lyrics(penyanyi, judul)
+        if not lyrics_text:
+            lyrics_text = await search_lyrics(judul, penyanyi)
+
+        if lyrics_text:
+            await m.reply(f"{em.sukses} <code>{lyrics_text}</code>")
+            await pft.delete()
+        else:
+            await m.reply(f"{em.gagal} <b>Maaf, lirik lagu tidak ditemukan.</b>")
+            await pft.delete()
+    except Exception as e:
+        await m.reply(f"{em.gagal} <b>Terjadi kesalahan saat mencari lirik lagu.</b> <code>{e}</code>")
+        await pft.delete()
