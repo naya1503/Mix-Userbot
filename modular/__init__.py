0694ef0db107365ecafb5770e955dc9f563168a1
naya1503 <naya1503@users.noreply.github.com>
2024-03-11 05:52:49 +0000

    Auto Fix

--- modular/__init__.py
+++ modular/__init__.py
@@ -1,12 +1,16 @@
-
 from team.nandev.class_log import LOGGER
+
+
 def import_modular():
-    from os.path import dirname, basename, isfile
     import glob
+    from os.path import basename, dirname, isfile
+
     mod_paths = glob.glob(dirname(__file__) + "/*.py")
-    all_modules = [basename(f)[:-3] for f in mod_paths if isfile(f)
-                   and f.endswith(".py")
-                   and not f.endswith('__init__.py')]
+    all_modules = [
+        basename(f)[:-3]
+        for f in mod_paths
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+    ]
 
     return all_modules
 
