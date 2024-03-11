e63fbaa821c8c1289bf75d441d32214eaa9ddd1e
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-12 01:41:13 +0700

    fixing dtik

--- modular/download.py
+++ modular/download.py
@@ -101,10 +101,11 @@ async def _(self: user, m):
     pros = await m.reply(cgr("proses").format(em.proses))
     ai = await user.forward_messages(hm, m.chat.id, message_ids=m.id)
     await user.send_message(hm, "/tiktok", reply_to_message_id=ai.id)
-    await asyncio.sleep(5)
+    await asyncio.sleep(3)
     async for tai in user.search_messages(hm, limit=1):
-        await asyncio.sleep(5)
-        await tai.copy(m.chat.id)
+        await asyncio.sleep(3)
+        if tai.media:
+            await tai.copy(m.chat.id)
     await pros.delete()
     ulat = await user.resolve_peer(hm)
     await user.invoke(DeleteHistory(peer=ulat, max_id=0, revoke=True))
