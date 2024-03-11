101602c4726f8c576b2a783b81e404af87c50c99
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-11 16:54:01 +0700

    update modular/carbonara.py

--- modular/carbonara.py
+++ modular/carbonara.py
@@ -7,19 +7,19 @@
 ################################################################
 
 import os
-
+import random
 from SafoneAPI import SafoneAPI
-
+from Mix.core.tools_quote import *
 from Mix import *
 
 __modles__ = "Carbon"
 __help__ = get_cgr("help_carbon")
 
 
-async def buat_bon(code, language, theme):
+async def buat_bon(code, bgne, language, theme):
     meki = SafoneAPI()
     bg = {
-        "backgroundColor": "navy",
+        "backgroundColor": bgne,
         "fontFamily": "Roboto",
         "fontSize": "14px",
         "language": language,
@@ -30,32 +30,76 @@ async def buat_bon(code, language, theme):
         file.write(img.getvalue())
     return "carbon.png"
 
-
+@ky.ubot("bglist", sudo=True)
+async def _(c: user, m):
+    em = Emojik()
+    em.initialize()
+    iymek = f"\n• ".join(loanjing)
+    jadi = cgr("qot_1").format(em.proses)
+    if len(iymek) > 4096:
+        with open("bglist.txt", "w") as file:
+            file.write(iymek)
+        await m.reply_document("bglist.txt", caption=cgr("qot_2").format(em.sukses))
+        os.remove("bglist.txt")
+    else:
+        await m.reply(jadi + iymek)
+        
+        
 @ky.ubot("carbon|carbonara", sudo=True)
 async def _(c, m):
     em = Emojik()
     em.initialize()
     text = m.reply_to_message.text or m.reply_to_message.caption
+    acak = None
     if not text:
         return await m.reply(cgr("crbn_1").format(em.gagal))
     ex = await m.reply(cgr("proses").format(em.proses))
     if len(m.command) == 1 and text:
-        meg = await buat_bon(text, "python", "light")
+        acak = random.choice(loanjing)
+        tem = random.choice(loanjing)
+        meg = await buat_bon(text, acak, "python", tem)
         await m.reply_photo(
             meg, caption=cgr("crbn_2").format(em.sukses, user.me.mention)
         )
         os.remove(meg)
     elif len(m.command) == 2:
-        lague = m.text.split(None, 1)[1]
-        meg = await buat_bon(text, lague, "light")
+        warna = m.command[1] if m.command[1] else None
+        if warna:
+            acak = warna
+        else:
+            acak = random.choice(loanjing)
+        tem = random.choice(loanjing)
+        meg = await buat_bon(text, acak, "python", tem)
         await m.reply_photo(
             meg, caption=cgr("crbn_2").format(em.sukses, user.me.mention)
         )
         os.remove(meg)
     elif len(m.command) == 3:
-        lague = m.text.split(None, 2)[1]
-        tem = m.text.split(None, 2)[2]
-        meg = await buat_bon(text, lague, tem)
+        warna = m.command[2] if m.command[2] else None
+        if warna:
+            acak = warna
+        else:
+            acak = random.choice(loanjing)
+        lague = m.command[3] if m.command[3] else None
+        tem = random.choice(loanjing)
+        meg = await buat_bon(text, acak, lague, tem)
+        await m.reply_photo(
+            meg, caption=cgr("crbn_2").format(em.sukses, user.me.mention)
+        )
+        os.remove(meg)
+    elif len(m.command) == 4:
+        warna = m.command[2] if m.command[2] else None
+        if warna:
+            acak = warna
+        else:
+            acak = random.choice(loanjing)
+        lague = m.command[3] if m.command[3] else None
+        tema = m.command[4] if m.command[4] else None
+        if tema:
+            tem = tema
+        else:
+            tem = random.choice(loanjing)
+        meg = await buat_bon(text, acak, lague, tem)
         await m.reply_photo(
             meg, caption=cgr("crbn_2").format(em.sukses, user.me.mention)
         )
