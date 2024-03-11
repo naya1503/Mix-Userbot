a4dcae08f9478f6cf2cc252e807475d80f40a3c9
jonesroot <jonesroot@users.noreply.github.com>
2024-03-11 06:35:48 +0000

    Auto Fix

--- modular/lirik.py
+++ modular/lirik.py
@@ -7,12 +7,12 @@ from pyrogram.types import *
 
 from Mix import *
 
-
 __modles__ = "Lyric"
 __help__ = "Lyrics"
 
 
 api = genius.Genius(genius_api)
+
 
 @ky.ubot("lyric", sudo=True)
 async def _(c, m):
