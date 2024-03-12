7b3c5c9e6bf89394719158c4c6aa69f7069ec75e
naya1503 <naya1503@users.noreply.github.com>
2024-03-12 05:18:08 +0000

    Auto Fix

--- Mix/__main__.py
+++ Mix/__main__.py
@@ -9,20 +9,24 @@ from assistant import BOT_PLUGINS
 from Mix import *
 from modular import USER_MOD
 
+
 async def start_user():
     LOGGER.info(f"Starting Telegram User Client...")
     try:
         await user.start()
         LOGGER.info(f"Importing All Modules...")
         for modul in USER_MOD:
-                imported_module = importlib.import_module(f"modular." + modul)
-                if hasattr(imported_module, "__modles__") and imported_module.__modles__:
-                    imported_module.__modles__ = imported_module.__modles__
-                    if hasattr(imported_module, "__help__") and imported_module.__help__:
-                        CMD_HELP[imported_module.__modles__.replace(" ", "_").lower()] = (imported_module)
+            imported_module = importlib.import_module(f"modular." + modul)
+            if hasattr(imported_module, "__modles__") and imported_module.__modles__:
+                imported_module.__modles__ = imported_module.__modles__
+                if hasattr(imported_module, "__help__") and imported_module.__help__:
+                    CMD_HELP[imported_module.__modles__.replace(" ", "_").lower()] = (
+                        imported_module
+                    )
     except (SessionExpired, ApiIdInvalid, UserDeactivatedBan):
         LOGGER.info("Check your session or api id!!")
         sys.exit(1)
+
 
 async def start_bot():
     LOGGER.info(f"Starting Telegram Bot Client...")
@@ -31,8 +35,8 @@ async def start_bot():
     try:
         await bot.start()
         for plus in BOT_PLUGINS:
-              imported_module = importlib.import_module("assistant." + plus)
-              importlib.reload(imported_module)
+            imported_module = importlib.import_module("assistant." + plus)
+            importlib.reload(imported_module)
     except (AccessTokenExpired, SessionRevoked, AccessTokenInvalid):
         LOGGER.info("Token Expired.")
         sys.exit(1)
@@ -46,8 +50,15 @@ async def starter():
     if ndB.ping():
         LOGGER.info(f"Connected to {ndB.name} Successfully!")
     await asyncio.gather(start_user(), start_bot())
-    await asyncio.gather(refresh_cache(), check_logger(), getFinish(), isFinish(), idle(), aiohttpsession.close())
-    
+    await asyncio.gather(
+        refresh_cache(),
+        check_logger(),
+        getFinish(),
+        isFinish(),
+        idle(),
+        aiohttpsession.close(),
+    )
+
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
