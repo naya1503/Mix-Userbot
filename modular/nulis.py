d44e555aabd49fcff3dd1dbce722efd345aad8c6
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-12 12:44:25 +0700

    update modular/nulis.py

--- modular/nulis.py
+++ modular/nulis.py
@@ -16,9 +16,9 @@ __modles__ = "Nulis"
 __help__ = "Nulis"
 
 
-async def nulis(text, page, font, color="black"):
+async def nulis(text):
     meki = SafoneAPI()
-    imgs = await meki.write(text, page, font, color)
+    imgs = await meki.write(text)
     img_paths = []
     for i, img in enumerate(imgs):
         img_path = f"img{i}.png"
@@ -35,19 +35,8 @@ async def _(c, m):
     rep = m.reply_to_message
     if rep:
         text = rep.text or rep.caption
-        page = m.text.split(None, 3)[1] if len(m.command) > 1 else None
-        font = m.text.split(None, 3)[2] if len(m.command) > 2 else None
-        kolor = m.text.split(None, 3)[3] if len(m.command) > 3 else None
-        meko = await nulis(text, page, font, kolor)
-    elif not rep and len(m.command) > 4:
-        text = m.text.split(None, 4)[1]
-        page = m.text.split(None, 4)[2] if len(m.command) > 2 else None
-
-        font = m.text.split(None, 4)[3] if len(m.command) > 3 else None
-
-        kolor = m.text.split(None, 4)[4] if len(m.command) > 4 else None
-        meko = await nulis(text, page, font, kolor)
     else:
-        meko = await nulis(text)
+        text = m.text.split(None, 1)[1]
+    meko = await nulis(text)
     await m.reply_photo(meko)
     os.remove(meko)
