1d9d785b4cde55014a5be6f03ea21744278c150f
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-12 18:00:49 +0700

    update modular/autoend.py

--- modular/autoend.py
+++ modular/autoend.py
@@ -21,7 +21,6 @@ __help__ = "AutoEndChat"
 async def _(_, m):
     em = Emojik()
     em.initialize()
-    tag = m.command[1].strip()
     puki = await user.extract_user(m)
     rep = m.reply_to_message
     mek = await m.reply(cgr("proses").format(em.proses))
@@ -41,22 +40,24 @@ async def _(_, m):
         except PeerIdInvalid:
             pass
         await m.reply(f"{em.sukses} **Mampus lu jing {org}!! Gw EndChat!!**")
-    if tag.startswith("@") or tag.isnumeric() and not rep:
-        try:
-            info = await user.resolve_peer(who)
-            await user.invoke(DeleteHistory(peer=info, max_id=0, revoke=True))
-        except PeerIdInvalid:
-            pass
-        await m.reply(f"{em.sukses} **Mampus lu jing {who}!! Gw EndChat!!**")
-    elif tag == "all":
-        biji = await refresh_dialog("users")
-        for kelot in biji:
+    elif len(m.command) > 2:
+        tag = m.command[1].strip()
+        if tag.startswith("@") or tag.isnumeric() and not rep:
             try:
-                info = await user.resolve_peer(kelot)
+                info = await user.resolve_peer(who)
                 await user.invoke(DeleteHistory(peer=info, max_id=0, revoke=True))
             except PeerIdInvalid:
-                continue
-        await m.reply(f"{em.sukses} **Mampus {len(biji)} pesan Gw EndChat!!**")
+                pass
+            await m.reply(f"{em.sukses} **Mampus lu jing {who}!! Gw EndChat!!**")
+        elif tag == "all":
+            biji = await refresh_dialog("users")
+            for kelot in biji:
+                try:
+                    info = await user.resolve_peer(kelot)
+                    await user.invoke(DeleteHistory(peer=info, max_id=0, revoke=True))
+                except PeerIdInvalid:
+                    continue
+            await m.reply(f"{em.sukses} **Mampus {len(biji)} pesan Gw EndChat!!**")
     else:
         await m.reply("**Kasih argumen Goblok**")
     await mek.delete()
