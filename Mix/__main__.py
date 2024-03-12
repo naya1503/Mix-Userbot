9cacfac8f59a6bf3ab72f2ebd48cb78138a43065
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-12 12:17:49 +0700

    update Mix/__main__.py

--- Mix/__main__.py
+++ Mix/__main__.py
@@ -5,9 +5,37 @@ import sys
 from pyrogram import *
 from pyrogram.errors import *
 
-from assistant import bot_plugins
+from assistant import BOT_PLUGINS
 from Mix import *
 from modular import USER_MOD
+
+async def start_user():
+    LOGGER.info(f"Starting Telegram User Client...")
+    try:
+        await user.start()
+        LOGGER.info(f"Importing All Modules...")
+        for modul in USER_MOD:
+                imported_module = importlib.import_module(f"modular." + modul)
+                if hasattr(imported_module, "__modles__") and imported_module.__modles__:
+                    imported_module.__modles__ = imported_module.__modles__
+                    if hasattr(imported_module, "__help__") and imported_module.__help__:
+                        CMD_HELP[imported_module.__modles__.replace(" ", "_").lower()] = (imported_module)
+    except (SessionExpired, ApiIdInvalid, UserDeactivatedBan):
+        LOGGER.info("Check your session or api id!!")
+        sys.exit(1)
+
+async def start_bot():
+    LOGGER.info(f"Starting Telegram Bot Client...")
+    if bot_token is None:
+        await autobot()
+    try:
+        await bot.start()
+        for plus in BOT_PLUGINS:
+              imported_module = importlib.import_module("assistant." + plus)
+              importlib.reload(imported_module)
+    except (AccessTokenExpired, SessionRevoked, AccessTokenInvalid):
+        LOGGER.info("Token Expired.")
+        sys.exit(1)
 
 
 async def starter():
@@ -17,38 +45,10 @@ async def starter():
     LOGGER.info(f"Connecting to {ndB.name}...")
     if ndB.ping():
         LOGGER.info(f"Connected to {ndB.name} Successfully!")
-    try:
-        LOGGER.info(f"Starting Telegram Client...")
-        await user.start()
-        LOGGER.info(f"Importing All Modules...")
-        for modul in USER_MOD:
-            imported_module = importlib.import_module(f"modular.{modul}")
-            if hasattr(imported_module, "__modles__") and imported_module.__modles__:
-                imported_module.__modles__ = imported_module.__modles__
-                if hasattr(imported_module, "__help__") and imported_module.__help__:
-                    CMD_HELP[imported_module.__modles__.replace(" ", "_").lower()] = (
-                        imported_module
-                    )
-    except (SessionExpired, ApiIdInvalid, UserDeactivatedBan):
-        LOGGER.info("Check your session or api id!!")
-    if bot_token is None:
-        await autobot()
-        await asyncio.sleep(1)
-    try:
-        await bot.start()
-        await bot_plugins()
-        await asyncio.sleep(1)
-    except (AccessTokenExpired, SessionRevoked, AccessTokenInvalid):
-        LOGGER.info("Token Expired.")
-        sys.exit(1)
-    await refresh_cache()
-    await check_logger()
-    await getFinish()
-    LOGGER.info("Successfully Started Userbot.")
-    await isFinish()
-    await idle()
-    await aiohttpsession.close()
-
+    await asyncio.gather(start_user(), start_bot())
+    await asyncio.gather(refresh_cache(), check_logger(), getFinish(), isFinish(), idle(), aiohttpsession.close())
+    
 
 if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
     loop.run_until_complete(starter())
