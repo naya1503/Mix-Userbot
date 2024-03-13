c6b58d2a5aef666007c6d05b707757c82acd9337
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-14 02:26:20 +0700

    add games module

--- modular/games.py
+++ modular/games.py
@@ -4,11 +4,43 @@ from pyrogram.types import *
 from Mix import *
 
 
+__modles__ = "Games"
+__help__ = get_cgr("help_games")
+
+
+
 @ky.ubot("dice", sudo=True)
 async def _(c, m):
     chat = m.chat.id
     await c.send_dice(chat, "🎲")
     await m.delete()
-    # while message.dice.value != 6:
-    #     await message.delete()
-    #     message = await c.send_dice(chat, "🎲")
+
+
+@ky.ubot("dart", sudo=True)
+async def _(c, m): 
+    await c.send_dice(m.chat.id, "🎯")
+    await m.delete()
+
+
+@ky.ubot("basket", sudo=True)
+async def _(c, m): 
+    await c.send_dice(m.chat.id, "🏀")
+    await m.delete()
+
+
+@ky.ubot("bowling", sudo=True)
+async def _(c, m):
+    await c.send_dice(m.chat.id, "🎳")
+    await m.delete()
+
+
+@ky.ubot("football", sudo=True)
+async def _(c, m): 
+    await c.send_dice(m.chat.id, "⚽")
+    await m.delete()
+
+
+@ky.ubot("slot", sudo=True)
+async def _(c, m): 
+    await c.send_dice(m.chat.id, "🎰")
+    await m.delete()
