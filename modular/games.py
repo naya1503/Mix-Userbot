d347a4f44ac7ad9cd7c47951afb8a5e138359bb9
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-14 01:46:50 +0700

    add games module

---
+++ modular/games.py
@@ -0,0 +1,18 @@
+from pyrogram import *
+from pyrogram.types import *
+
+from Mix import *
+
+@ky.ubot("dice", sudo=True)
+async def _(c, m):
+    six = (m.from_user.id in SUDOERS) if m.from_user else False
+
+    c = m.chat.id
+    if not six:
+        return await c.send_dice(c, "🎲")
+
+    m = await c.send_dice(c, "🎲")
+
+    while m.dice.value != 6:
+        await m.delete()
+        m = await c.send_dice(c, "🎲")
