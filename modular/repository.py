bd03707fa45a26dd223908e7c82b62b4f5bfc933
naya1503 <naya1503@users.noreply.github.com>
2024-03-13 03:08:27 +0000

    Auto Fix

--- modular/repository.py
+++ modular/repository.py
@@ -16,7 +16,9 @@ __help__ = "Repository"
 
 @ky.ubot("repo|repository", sudo=True)
 async def repo(c, m):
-    link = await http.get("https://api.github.com/repos/naya1503/Mix-Userbot/contributors")
+    link = await http.get(
+        "https://api.github.com/repos/naya1503/Mix-Userbot/contributors"
+    )
     orgnya = "".join(
         f"**{count}.** [{org['login']}]({org['html_url']})\n"
         for count, org in enumerate(link, start=1)
