4b291ad386f8489922b79587a07244106c74d478
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-14 02:02:37 +0700

    hm

--- modular/games.py
+++ modular/games.py
@@ -6,14 +6,9 @@ from Mix import *
 
 @ky.ubot("dice", sudo=True)
 async def _(c, m):
-    six = (m.from_user.id in DEVS) if m.from_user else False
-
     chat = m.chat.id
-    if not six:
-        return await c.send_dice(chat, "🎲")
-
-    m = await c.send_dice(chat, "🎲")
-
-    while m.dice.value != 6:
-        await m.delete()
-        m = await c.send_dice(chat, "🎲")
+    message = await c.send_dice(chat, "🎲")
+    m.delete()
+    # while message.dice.value != 6:
+    #     await message.delete()
+    #     message = await c.send_dice(chat, "🎲")
