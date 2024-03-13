bb78ece5e913229544d4eed369950edf191b630c
jonesroot <jonesroot@users.noreply.github.com>
2024-03-13 19:04:36 +0000

    Auto Fix

--- modular/games.py
+++ modular/games.py
@@ -7,7 +7,7 @@ from Mix import *
 @ky.ubot("dice", sudo=True)
 async def _(c, m):
     chat = m.chat.id
-    message = await c.send_dice(chat, "🎲")
+    await c.send_dice(chat, "🎲")
     await m.delete()
     # while message.dice.value != 6:
     #     await message.delete()
