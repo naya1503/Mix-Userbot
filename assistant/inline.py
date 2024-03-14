12cc915e70b036c45aad5e9859df3567c8e289f8
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-14 14:16:27 +0700

    update assistant/inline.py

--- assistant/inline.py
+++ assistant/inline.py
@@ -324,7 +324,7 @@ async def _(c, iq):
                 await m.delete()
         kiki = PM_WARN.format(
             user.me.first_name,
-            pm_text.format(full),
+            pm_text,
             flood2[int(org[1])],
             pm_warns,
         )
