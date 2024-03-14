500f1d5ce17ec285811572d37dcd55228a9bcbe3
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-14 21:24:40 +0700

    update assistant/inline.py

--- assistant/inline.py
+++ assistant/inline.py
@@ -305,14 +305,9 @@ async def _(c, iq):
     for row in button.inline_keyboard:
         for data in row:
             add_keyb = (
-                {data.text: data.url} if data.url else {data.text: data.callback_data}
+                {data.text: data.url} if data.url else {data.text: data.callback_data} {"Setuju": f"pm_okein {int(org[1])}", "Blokir": f"pm_blokbae {int(org[1])}"}
             )
-            add_keyb2 = {
-                "Setuju": f"pm_okein {int(org[1])}",
-                "Blokir": f"pm_blokbae {int(org[1])}",
-            }
             def_keyb.update(add_keyb)
-            def_keyb.update(add_keyb2)
             keyboard = ikb(def_keyb)
     mari = await user.get_users(int(org[1]))
     full = f"[{mari.first_name} {mari.last_name or ''}](tg://user?id={int(org[1])})"
