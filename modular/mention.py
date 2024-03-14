3e1af498761fb290c526c5f3b756327014594593
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-14 17:25:21 +0700

    tagall

--- modular/mention.py
+++ modular/mention.py
@@ -1,5 +1,5 @@
 import asyncio
-
+import re
 from pyrogram import *
 from pyrogram.enums import *
 from pyrogram.errors import *
@@ -12,57 +12,24 @@ __help__ = "Mention"
 
 
 @ky.ubot("tagall|mention", sudo=True)
-async def mention_all(c: Client, m: Message):
+async def _(c, m):
     chat_id = m.chat.id
-    if m.chat.type == ChatType.PRIVATE:
-        return await m.reply(
-            "Perintah ini hanya bisa digunakan untuk Grup atau Channel"
-        )
-
-    is_admin = False
-    try:
-        participant = await c.get_chat_member(chat_id, m.from_user.id)
-    except UserNotParticipant:
-        is_admin = False
+    if not m.from_user.is_admin:
+        await m.reply_text("Anda harus menjadi admin untuk menggunakan perintah ini!")
+        return
+    if m.reply_to_message:
+        target_message = m.reply_to_message
     else:
-        if participant.status in (ChatMemberStatus.ADMINISTRATOR,):
-            is_admin = True
-    if not is_admin:
-        return await m.reply_text(
-            "Hanya admin grup yang bisa menggunakan perintah ini!"
-        )
-
-    usrnum = 0
-    usrtxt = ""
-    if m.text and m.reply_to_message:
-        return await m.reply_text("Berikan saya argumen!")
-    elif m.text:
-        mode = "text_on_cmd"
-        msg = m.text
-    elif m.reply_to_message:
-        mode = "text_on_reply"
-        msg = m.reply_to_message
-        if msg is None:
-            return await m.reply_text(
-                "Saya tidak bisa melakukan tagall dari pesan yang sebelumnya tidak ada"
-            )
+        target_message = m
+    if len(m.command) > 1:
+        text = " ".join(m.command[1:])
     else:
-        return await m.reply_text(
-            "Berikan saya pesan atau balas pesan untuk melakukan tagall"
-        )
-
-    members = await c.get_chat_members(chat_id)
-    for user in members:
-        if user.status == ChatMemberStatus.KICKED:
-            continue
-        usrnum += 1
-        usrtxt += f"[{user.user.first_name}](tg://user?id={user.user.id}) "
-        if usrnum == 5:
-            if mode == "text_on_cmd":
-                txt = f"{usrtxt}\n\n{msg}"
-                await c.send_message(chat_id, txt)
-            elif mode == "text_on_reply":
-                await msg.reply_text(usrtxt)
-            await asyncio.sleep(2)
-            usrnum = 0
-            usrtxt = ""
+        text = None
+    if text is None:
+        await m.reply_text("Harap berikan saya teks atau balas sebuah pesan.")
+        return
+    username_pattern = re.compile(r"@[\w\d_]+")
+    async for member in c.iter_chat_members(chat_id):
+        if not member.user.is_bot:
+            mention_text = re.sub(username_pattern, f"@{member.user.username}", text)
+            await target_message.reply(mention_text)
