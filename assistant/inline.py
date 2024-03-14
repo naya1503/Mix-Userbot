701bc6d013260df4226cb0a2605cc60a48fc7300
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-14 21:10:01 +0700

    update assistant/inline.py

--- assistant/inline.py
+++ assistant/inline.py
@@ -301,15 +301,12 @@ async def _(c, iq):
     getpm_warns = udB.get_var(gw, "PMLIMIT")
     pm_warns = getpm_warns if getpm_warns else LIMIT
     teks, button = text_keyb(ikb, pm_text)
+    def_keyb = {"Setuju": f"pm_okein {int(org[1])}", "Blokir": f"pm_blokbae {int(org[1])}"}
     for row in button.inline_keyboard:
         for data in row:
-            keyboard = ikb(
-                {
-                    "Setuju": f"pm_ okein {int(org[1])}",
-                    "Blokir": f"pm_ blokbae {int(org[1])}",
-                }
-            )
-            keyboard.add({f"{data.text}": f"{data.url}"})
+            add_keyb = {data.text: data.url} if data.url else {data.text: data.callback_data}
+            def_keyb.update(add_keyb)
+            keyboard = ikb(def_keyb)
     mari = await user.get_users(int(org[1]))
     full = f"[{mari.first_name} {mari.last_name or ''}](tg://user?id={int(org[1])})"
     kiki = None
