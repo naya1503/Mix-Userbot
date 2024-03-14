61e1b02bf79616b6981d29ab414f2482e1842d2e
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-14 20:25:46 +0700

    asfsahfl

--- modular/mention.py
+++ modular/mention.py
@@ -57,7 +57,8 @@ async def tag_all_members(c: user, m: Message):
     text = " ".join(m.command[1:])
 
     mention_texts = []
-    async for member in c.iter_chat_members(chat_id):
+    members = c.get_chat_members(chat_id)
+    async for member in members:
         if not member.user.is_bot:
             mention_texts.append(f"{random_emoji()} @{member.user.username}")
             if len(mention_texts) == 4:
