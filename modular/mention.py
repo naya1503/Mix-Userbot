dabca6a3d4969b7081af201458d8e242445acd45
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-16 00:54:24 +0700

    as;ldash;

--- modular/mention.py
+++ modular/mention.py
@@ -56,17 +56,12 @@ async def tag_all_members(c: user, m: Message):
     await progres.delete()
 
     text = " ".join(m.command[1:]) if len(m.command) >= 2 else None
-    rep.text if rep.text else None
     reply_text = rep.text if rep.text else None
-    tegs = await c.get_messages(
-        chat_id=m.chat.id, message_ids=m.reply_to_message.id, replies=0
-    )
-    repli_teks = [tegs]
+
     mention_texts = []
     members = c.get_chat_members(chat_id)
     berenti = True
     count = 0
-    send = c.get_m(m)
     async for member in members:
         if not berenti:
             break
@@ -74,33 +69,32 @@ async def tag_all_members(c: user, m: Message):
             mention_texts.append(f"[{random_emoji()}](tg://user?id={member.user.id})")
             count += 1
             if len(mention_texts) == 4:
-                mention_text = (
-                    f"{repli_teks}\n\n" if reply_text else f"{repli_teks}\n\n"
-                )
+                mention_text = f"{reply_text}\n\n" if reply_text else ""  # Menambahkan teks pesan yang dibalas jika ada
                 mention_text += " ".join(mention_texts)
                 try:
-                    await send.copy(chat_id, mention_text)
+                    await c.send_message(chat_id, mention_text, parse_mode="MarkdownV2")
                 except FloodWait as e:
                     await asyncio.sleep(e.x)
-                    await send.copy(chat_id, mention_text)
+                    await c.send_message(chat_id, mention_text, parse_mode="MarkdownV2")
                 await asyncio.sleep(2)
                 mention_texts = []
 
     if mention_texts:
-        mention_text = f"{repli_teks}\n\n" if reply_text else f"{repli_teks}\n\n"
+        mention_text = f"{reply_text}\n\n" if reply_text else ""  # Menambahkan teks pesan yang dibalas jika ada
         mention_text += "\n".join(mention_texts)
         try:
-            await send.copy(chat_id, mention_text)
+            await c.send_message(chat_id, mention_text, parse_mode="MarkdownV2")
         except FloodWait as e:
             tunggu = asyncio.slee(e.x)
-            await c.send_message(chat_id, f"Silahkan tunggu `{tunggu}` detik")
+            await c.send_message(chat_id, f"Silahkan tunggu `{tunggu}` detik", parse_mode="MarkdownV2")
             await asyncio.sleep(e.x)
-            await send.copy(chat_id, mention_text)
+            await c.send_message(chat_id, mention_text, parse_mode="MarkdownV2")
         await asyncio.sleep(2)
     berenti = False
     await m.reply(
         f"{em.sukses} <b>Berhasil melakukan mention kepada <code>{count}</code> anggota.</b>"
     )
+
 
 
 @ky.ubot("stop", sudo=True)
