6202d477006dd2f029260c0e8ee3ddd9e6421890
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-16 01:16:17 +0700

    salkfhsafkh

--- modular/mention.py
+++ modular/mention.py
@@ -56,7 +56,6 @@ async def tag_all_members(c: user, m: Message):
     await progres.delete()
 
     text = " ".join(m.command[1:]) if len(m.command) >= 2 else None
-    rep.text if rep.text else None
     reply_text = rep.text if rep.text else None
     tegs = await c.get_messages(
         chat_id=m.chat.id, message_ids=m.reply_to_message.id, replies=0
@@ -67,28 +66,26 @@ async def tag_all_members(c: user, m: Message):
     berenti = True
     count = 0
     send = c.get_m(m)
-    ininya = (
-        mention_texts.append(reply_text)
-        if reply_text
-        else mention_texts.append(repli_teks)
-    )
+
+    # Tambahkan pesan mention atau pesan yang dibalas ke dalam list mention_texts
+    mention_texts.append(reply_text) if reply_text else mention_texts.append(repli_teks)
 
     async for member in members:
         if not berenti:
             break
         if not member.user.is_bot:
-            ininya.append(f"[{random_emoji()}](tg://user?id={member.user.id})")
+            mention_texts.append(f"[{random_emoji()}](tg://user?id={member.user.id})")
             count += 1
-            if len(ininya) == 4:
+            if len(mention_texts) == 4:
                 mention_text = (
                     f"{reply_text}\n\n" if reply_text else f"{repli_teks}\n\n"
                 )
-                mention_text += " ".join(ininya)
+                mention_text += " ".join(mention_texts)
                 try:
-                    await send.copy(chat_id, mention_text)
+                    await c.send_message(chat_id, mention_text)
                 except FloodWait as e:
                     await asyncio.sleep(e.x)
-                    await send.copy(chat_id, mention_text)
+                    await c.send_message(chat_id, mention_text)
                 await asyncio.sleep(2)
                 mention_texts = []
 
@@ -101,12 +98,13 @@ async def tag_all_members(c: user, m: Message):
             tunggu = asyncio.slee(e.x)
             await c.send_message(chat_id, f"Silahkan tunggu `{tunggu}` detik")
             await asyncio.sleep(e.x)
-            await send.copy(chat_id, mention_text)
+            await c.send_message(chat_id, mention_text)
         await asyncio.sleep(2)
     berenti = False
     await m.reply(
         f"{em.sukses} <b>Berhasil melakukan mention kepada <code>{count}</code> anggota.</b>"
     )
+
 
 
 @ky.ubot("stop", sudo=True)
