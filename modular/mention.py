84ce847069037ddf754be8a0f15693fd34031f20
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-16 00:50:36 +0700

    ashdlasdhl

--- modular/mention.py
+++ modular/mention.py
@@ -57,6 +57,7 @@ async def tag_all_members(c: user, m: Message):
 
     text = " ".join(m.command[1:]) if len(m.command) >= 2 else None
     rep.text if rep.text else None
+    reply_text = rep.text if rep.text else None
     tegs = await c.get_messages(
         chat_id=m.chat.id, message_ids=m.reply_to_message.id, replies=0
     )
@@ -73,7 +74,7 @@ async def tag_all_members(c: user, m: Message):
             mention_texts.append(f"[{random_emoji()}](tg://user?id={member.user.id})")
             count += 1
             if len(mention_texts) == 4:
-                mention_text = f"{repli_teks}\n\n"  # if reply_text else f"{repli}\n\n"
+                mention_text = f"{repli_teks}\n\n" if reply_text else f"{repli_teks}\n\n"
                 mention_text += " ".join(mention_texts)
                 try:
                     await send.copy(chat_id, mention_text)
@@ -84,7 +85,7 @@ async def tag_all_members(c: user, m: Message):
                 mention_texts = []
 
     if mention_texts:
-        mention_text = f"{repli_teks}\n\n"  # if reply_text else f"{repli}\n\n"
+        mention_text = f"{repli_teks}\n\n" if reply_text else f"{repli_teks}\n\n"
         mention_text += "\n".join(mention_texts)
         try:
             await send.copy(chat_id, mention_text)
