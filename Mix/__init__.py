c2dd38e608213ec908c1b7375f67c993b49dbefc
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-15 12:06:49 +0700

    update Mix/__init__.py

--- Mix/__init__.py
+++ Mix/__init__.py
@@ -4,6 +4,14 @@ from team.nandev.class_ubot import Bot, Userbot
 
 from config import *
 from Mix.core import *
+import pytgcalls
+
+# part of https://github.com/thehamkercat/Telegram_VC_Bot
+
+CLIENT_TYPE = pytgcalls.GroupCallFactory.MTPROTO_CLIENT_TYPE.PYROGRAM
+PLAYOUT_FILE = "input.raw"
+PLAY_LOCK = asyncio.Lock()
+OUTGOING_AUDIO_BITRATE_KBIT = 128
 
 git()
 heroku()
