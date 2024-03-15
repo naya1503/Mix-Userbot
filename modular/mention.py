b2f4b8a12e126d84b66d60bc4bdc6acd33549342
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-16 00:39:34 +0700

    sakdasdn

--- modular/mention.py
+++ modular/mention.py
@@ -57,9 +57,7 @@ async def tag_all_members(c: user, m: Message):
 
     text = " ".join(m.command[1:]) if len(m.command) >= 2 else None
     reply_text = rep.text if rep.text else None
-    repli_teks = await c.get_m(
-        chat_id=m.chat.id, message_ids=m.reply_to_message.id, replies=0
-    )
+    repli_teks = await c.get_m(m)
     repli = [repli_teks]
     mention_texts = []
     members = c.get_chat_members(chat_id)
