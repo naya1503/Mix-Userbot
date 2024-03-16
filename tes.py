554661380243052c48723d9680ee1e0c6f291407
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-16 18:54:42 +0700

    update tes.py

--- tes.py
+++ tes.py
@@ -15,4 +15,5 @@ class Greeting(Resource):
 
 
 api.add_resource(Greeting, "/")
-app.run(host="0.0.0.0", port=os.environ.get("PORT", 8080))
+app.run(host="0.0.0.0", port=os.environ.get("PORT", 8080), debug=False, use_reloader=False)
+
