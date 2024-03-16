9a0933188b90916a0e64fe6436ff5c957644a594
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-16 12:58:35 +0700

    update Mix/core/waktu.py

--- Mix/core/waktu.py
+++ Mix/core/waktu.py
@@ -83,8 +83,8 @@ async def put_cleanmode(chat_id, message_id):
 async def auto_clean():
     while not await asyncio.sleep(5):
         try:
-            for chat_id in cleanmode:
-                if not udB.is_cleanmode_on(chat_id):
+            for org in cleanmode:
+                if not udB.is_cleanmode_on(user.me.id):
                     continue
                 for x in cleanmode[chat_id]:
                     if datetime.now() <= x["timer_after"]:
