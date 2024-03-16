ec00d300f393a0d3db3a45bdce8b06149299cc9b
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-17 01:33:46 +0700

    fixing

--- modular/ask.py
+++ modular/ask.py
@@ -1,7 +1,7 @@
 import random
-
 import requests
 from gpytranslate import Translator
+import asyncio
 
 from Mix import *
 
@@ -18,13 +18,14 @@ async def get_truth(category="classic|kids|party|hot|mixed"):
         response = requests.get(
             f"https://api.safone.dev/truth?category={random_category}"
         )
+        response.raise_for_status()  # Raise an error for non-200 status codes
         data = response.json()
-        if response.status_code == 200 and "truth" in data:
+        if "truth" in data:
             truth = await translator.translate(data["truth"], "en", "id")
             return truth
         else:
             return None
-    except Exception as e:
+    except requests.exceptions.RequestException as e:
         print("Failed to fetch Truth:", e)
         return None
 
@@ -36,13 +37,14 @@ async def get_dare(category="classic|kids|party|hot|mixed"):
         response = requests.get(
             f"https://api.safone.dev/dare?category={random_category}"
         )
+        response.raise_for_status()  # Raise an error for non-200 status codes
         data = response.json()
-        if response.status_code == 200 and "dare" in data:
+        if "dare" in data:
             dare = await translator.translate(data["dare"], "en", "id")
             return dare
         else:
             return None
-    except Exception as e:
+    except requests.exceptions.RequestException as e:
         print("Failed to fetch Dare:", e)
         return None
 
@@ -51,21 +53,27 @@ async def get_dare(category="classic|kids|party|hot|mixed"):
 async def dare_command(client, message):
     proses = await message.reply(f"`Tunggu ...`")
     dare = await get_dare()
-    if dare:
-        response = f"**Dare:** `{dare}`"
+    response_text = dare.get('text', dare.get('text_raw'))
+    if response_text:
+        response = f"**Dare:** `{response_text}`"
     else:
         response = "**Gagal mengambil Dare. Silakan coba lagi nanti.**"
-    await message.reply_text(response)
-    await proses.delete()
+    await asyncio.gather(
+        message.reply_text(response),
+        proses.delete()
+    )
 
 
 @ky.ubot("truth", sudo=True)
 async def truth_command(client, message):
     proses = await message.reply(f"`Tunggu ...`")
     truth = await get_truth()
-    if truth:
-        response = f"**Truth :** `{truth}`"
+    response_text = truth.get('text', truth.get('text_raw'))
+    if response_text:
+        response = f"**Truth :** `{response_text}`"
     else:
         response = "**Gagal mengambil Truth. Silakan coba lagi nanti.**"
-    await message.reply_text(response)
-    await proses.delete()
+    await asyncio.gather(
+        message.reply_text(response),
+        proses.delete()
+    )
