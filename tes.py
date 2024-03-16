53c95cef85ab08c0f830d0bde3fec880a991cd9f
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-16 18:58:32 +0700

    update tes.py

--- tes.py
+++ tes.py
@@ -1,20 +1,15 @@
 # part of Zaid-Userbot
 
 import os
-
 from flask import Flask
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
-
 class Greeting(Resource):
     def get(self):
         return "Running..."
 
-
 api.add_resource(Greeting, "/")
-app.run(
-    host="0.0.0.0", port=os.environ.get("PORT", 5000), debug=False, use_reloader=False
-)
+app.run(host="0.0.0.0", port=os.environ.get("PORT", 5000), debug=False, use_reloader=False)
