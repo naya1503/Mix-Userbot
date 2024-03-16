76a6eed9deb57499a358b3168ac838d82173c383
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-17 02:06:39 +0700

    update modular/auutored.py

--- modular/auutored.py
+++ modular/auutored.py
@@ -9,13 +9,32 @@
 
 
 from hydrogram.errors import *
-
 from Mix import *
-
-from .gcast import refresh_dialog
+from hydrogram.enums import ChatType
 
 __modles__ = "AutoRead"
 __help__ = get_cgr("help_autoread")
+
+async def diread_dong(q):
+    chats = []
+    chat_types = {
+        "group": [ChatType.GROUP, ChatType.SUPERGROUP],
+        "users": [ChatType.PRIVATE],
+        "bot": [ChatType.BOT],
+        "ch": [ChatType.CHANNEL],
+        "allread": [
+            ChatType.GROUP,
+            ChatType.SUPERGROUP,
+            ChatType.CHANNEL,
+            ChatType.PRIVATE,
+            ChatType.BOT,
+        ],
+    }
+    async for dialog in nlx.get_dialogs():
+        if dialog.chat.type in chat_types[q]
+            chats.append(dialog.chat.id)
+
+    return chats
 
 
 @ky.ubot("autoread", sudo=True)
@@ -29,7 +48,7 @@ async def _(_, m):
         return
     biji, peler = m.command[:2]
     if peler.lower() == "gc":
-        bcgc = await refresh_dialog("group")
+        bcgc = await diread_dong("group")
         for gc in bcgc:
             try:
                 await nlx.read_chat_history(gc, max_id=0)
@@ -43,7 +62,7 @@ async def _(_, m):
         await mek.edit(cgr("autoread_2").format(em.sukses, len(bcgc)))
         return
     elif peler.lower() == "us":
-        bcus = await refresh_dialog("users")
+        bcus = await diread_dong("users")
         for us in bcus:
             try:
                 await nlx.read_chat_history(us, max_id=0)
@@ -57,7 +76,7 @@ async def _(_, m):
         await mek.edit(cgr("autoread_3").format(em.sukses, len(bcus)))
         return
     elif peler.lower() == "bot":
-        bcbot = await refresh_dialog("bot")
+        bcbot = await diread_dong("bot")
         for bt in bcbot:
             try:
                 await nlx.read_chat_history(bt, max_id=0)
@@ -71,7 +90,7 @@ async def _(_, m):
         await mek.edit(cgr("autoread_7").format(em.sukses, len(bcbot)))
         return
     elif peler.lower() == "ch":
-        bcch = await refresh_dialog("ch")
+        bcch = await diread_dong("ch")
         for ch in bcch:
             try:
                 await nlx.read_chat_history(ch, max_id=0)
@@ -85,7 +104,7 @@ async def _(_, m):
         await mek.edit(cgr("autoread_4").format(em.sukses, len(bcch)))
         return
     elif peler.lower() == "all":
-        bcall = await refresh_dialog("allread")
+        bcall = await diread_dong("allread")
         for aih in bcall:
             try:
                 await nlx.read_chat_history(aih, max_id=0)
