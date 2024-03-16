5450c6cb2291435864892e9623dff758310ffe94
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-17 00:27:11 +0700

    fixing

--- modular/ask.py
+++ modular/ask.py
@@ -1,35 +1,36 @@
-import requests
+import aiohttp
+import asyncio
 from bs4 import BeautifulSoup
 from gpytranslate import Translator
-
+from pyrogram import filters
+from pyrogram.types import Message
 from Mix import *
 
 __modles__ = "ask"
 __help__ = "ask"
-
 
 async def get_duckduckgo_answer(query):
     url = f"https://duckduckgo.com/html/?q={'+'.join(query.split())}"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
     }
-    response = await requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.text, "html.parser")
-    answer = soup.find("a", class_="result__snippet")
-    if answer:
-        return answer.text
-    else:
-        return "Maaf, tidak dapat menemukan jawaban untuk pertanyaan tersebut."
-
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers=headers) as response:
+            if response.status == 200:
+                html_content = await response.text()
+                soup = BeautifulSoup(html_content, "html.parser")
+                answer = soup.find("a", class_="result__snippet")
+                if answer:
+                    return answer.text
+    return "Maaf, tidak dapat menemukan jawaban untuk pertanyaan tersebut."
 
 async def translate_text(text, target_language="id"):
     translator = Translator()
     translated_text = await translator.translate(text, target_lang=target_language)
     return translated_text
 
-
 @ky.ubot("ask", sudo=True)
-async def ask_command(_, message):
+async def ask_command(_, message: Message):
     command_args = message.text.split(maxsplit=1)
     if len(command_args) == 2:
         query = command_args[1]
