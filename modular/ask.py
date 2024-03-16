02ec5fd26701480a39792f5824bf1a8704fed88d
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-17 01:23:36 +0700

    fixing TOD

--- modular/ask.py
+++ modular/ask.py
@@ -1,12 +1,10 @@
 import random
-
 import requests
-
+from gpytranslate import translate
 from Mix import *
 
 __modles__ = "TOD"
 __help__ = "TOD"
-
 
 async def get_truth(category="classic|kids|party|hot|mixed"):
     try:
@@ -17,13 +15,13 @@ async def get_truth(category="classic|kids|party|hot|mixed"):
         )
         data = response.json()
         if response.status_code == 200 and "truth" in data:
-            return data["truth"]
+            truth = await translate(data["truth"], "en", "id")
+            return truth
         else:
             return None
     except Exception as e:
         print("Failed to fetch Truth:", e)
         return None
-
 
 async def get_dare(category="classic|kids|party|hot|mixed"):
     try:
@@ -34,29 +32,32 @@ async def get_dare(category="classic|kids|party|hot|mixed"):
         )
         data = response.json()
         if response.status_code == 200 and "dare" in data:
-            return data["dare"]
+            dare = await translate(data["dare"], "en", "id")
+            return dare
         else:
             return None
     except Exception as e:
         print("Failed to fetch Dare:", e)
         return None
 
-
 @ky.ubot("dare", sudo=True)
 async def dare_command(client, message):
+    proses = await message.reply(f"`Tunggu ...`")
     dare = await get_dare()
     if dare:
-        response = f"Dare: {dare}"
+        response = f"**Dare:** `{dare}`"
     else:
-        response = "Failed to fetch Dare. Please try again later."
+        response = "**Gagal mengambil Dare. Silakan coba lagi nanti.**"
     await message.reply_text(response)
-
+    await proses.delete()
 
 @ky.ubot("truth", sudo=True)
 async def truth_command(client, message):
+    proses = await message.reply(f"`Tunggu ...`")
     truth = await get_truth()
     if truth:
-        response = f"Truth: {truth}"
+        response = f"**Truth :** `{truth}`"
     else:
-        response = "Failed to fetch Truth. Please try again later."
+        response = "**Gagal mengambil Truth. Silakan coba lagi nanti.**"
     await message.reply_text(response)
+    await proses.delete()
