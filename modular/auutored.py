5ae4f2c32950d60307da18c82321ae85b9279503
naya1503 <naya1503@users.noreply.github.com>
2024-03-16 15:22:33 +0000

    Auto Fix

--- modular/auutored.py
+++ modular/auutored.py
@@ -31,7 +31,7 @@ async def _(_, m):
     if peler.lower() == "gc":
         bcgc = await refresh_dialog("group")
         for gc in bcgc:
-              await user.read_chat_history(gc, max_id=0)
+            await user.read_chat_history(gc, max_id=0)
         await mek.edit(cgr("autoread_2").format(em.sukses, len(bcgc)))
         return
     elif peler.lower() == "us":
