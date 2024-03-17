96457146cde46ffe57fbd746d885ecfdc748c970
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-17 19:37:41 +0700

    update beban.py

--- beban.py
+++ beban.py
@@ -9,38 +9,122 @@
 
 from pyrogram.enums import ChatType
 from team.nandev.class_log import LOGGER
-
-
-async def akunbebanamat(c):
-    chats = []
-    async for bb in c.get_dialogs(limit=500):
-        if bb.chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]:
-            chats.append(bb.chat.id)
-    return chats
+from Mix import nlx
+from team.nandev.database import udB
 
 
 async def dasar_laknat(c):
     LOGGER.info("Check whether this account is a burden or not...")
-    jml = await akunbebanamat(c)
-    for jamet in jml:
-        try:
-            await c.read_chat_history(jamet, max_id=0)
-        except (ChannelPrivate, PeerIdInvalid, UserBannedInChannel):
-            continue
-        except FloodWait as e:
-            await asyncio.slee(e.value)
+    async for bb in c.get_dialogs(limit=500):
+        if bb.chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]:
             try:
-                await c.read_chat_history(jamet, max_id=0)
-            except:
+                await c.read_chat_history(bb.chat.id, max_id=0)
+            except (ChannelPrivate, PeerIdInvalid, UserBannedInChannel):
                 continue
+            except FloodWait as e:
+                await asyncio.sleep(e.value)
+                try:
+                    await c.read_chat_history(bb.chat.id, max_id=0)
+                except:
+                    continue
     LOGGER.info("Finished Read Message...")
     
-    
-"""
-async def dasar_laknat(c):
-    async for bc in c.get_dialogs(limit=500):
-        if bc.chat.type == ChatType.SUPERGROUP:
-            LOGGER.info(f"Reading messages {bc.chat.title}")
-            # lastm = (await c.get_history(bc.chat.id, limit=1))[0].message_id
-            await c.read_chat_history(bc.chat.id, max_id=0)
-"""
+
+async def autor_gc():
+    if udB.get_var(nlx.me.id, "read_gc") is None:
+        return
+    while not await asyncio.sleep(300):
+        LOGGER.info("Running Autoread For Group...")
+        async for bb in nlx.get_dialogs(limit=500):
+            if bb.chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]:
+                try:
+                    await nlx.read_chat_history(bb.chat.id, max_id=0)
+                except (ChannelPrivate, PeerIdInvalid, UserBannedInChannel):
+                    continue
+                except FloodWait as e:
+                    await asyncio.sleep(e.value)
+                    try:
+                        await nlx.read_chat_history(bb.chat.id, max_id=0)
+                    except:
+                        continue
+        LOGGER.info("Finished Read Message...")
+        
+
+async def autor_ch():
+    if udB.get_var(nlx.me.id, "read_ch") is None:
+        return
+    while not await asyncio.sleep(300):
+        LOGGER.info("Running Autoread For Channel...")
+        async for bb in nlx.get_dialogs(limit=500):
+            if bb.chat.type == ChatType.CHANNEL:
+                try:
+                    await nlx.read_chat_history(bb.chat.id, max_id=0)
+                except (ChannelPrivate, PeerIdInvalid, UserBannedInChannel):
+                    continue
+                except FloodWait as e:
+                    await asyncio.sleep(e.value)
+                    try:
+                        await nlx.read_chat_history(bb.chat.id, max_id=0)
+                    except:
+                        continue
+        LOGGER.info("Finished Read Message...")
+        
+
+async def autor_us():
+    if udB.get_var(nlx.me.id, "read_us") is None:
+        return
+    while not await asyncio.sleep(300):
+        LOGGER.info("Running Autoread For Users...")
+        async for bb in nlx.get_dialogs(limit=500):
+            if bb.chat.type == ChatType.PRIVATE:
+                try:
+                    await nlx.read_chat_history(bb.chat.id, max_id=0)
+                except (ChannelPrivate, PeerIdInvalid, UserBannedInChannel):
+                    continue
+                except FloodWait as e:
+                    await asyncio.sleep(e.value)
+                    try:
+                        await nlx.read_chat_history(bb.chat.id, max_id=0)
+                    except:
+                        continue
+        LOGGER.info("Finished Read Message...")
+        
+        
+async def autor_bot():
+    if udB.get_var(nlx.me.id, "read_bot") is None:
+        return
+    while not await asyncio.sleep(300):
+        LOGGER.info("Running Autoread For Users...")
+        async for bb in nlx.get_dialogs(limit=500):
+            if bb.chat.type == ChatType.BOT:
+                try:
+                    await nlx.read_chat_history(bb.chat.id, max_id=0)
+                except (ChannelPrivate, PeerIdInvalid, UserBannedInChannel):
+                    continue
+                except FloodWait as e:
+                    await asyncio.sleep(e.value)
+                    try:
+                        await nlx.read_chat_history(bb.chat.id, max_id=0)
+                    except:
+                        continue
+        LOGGER.info("Finished Read Message...")
+        
+        
+async def autor_all():
+    if udB.get_var(nlx.me.id, "read_all") is None:
+        return
+    while not await asyncio.sleep(300):
+        LOGGER.info("Running Autoread For All...")
+        async for bb in nlx.get_dialogs(limit=500):
+            if bb.chat.type in [ChatType.GROUP, ChatType.SUPERGROUP, ChatType.CHANNEL, ChatType.PRIVATE, ChatType.BOT]:
+                try:
+                    await nlx.read_chat_history(bb.chat.id, max_id=0)
+                except (ChannelPrivate, PeerIdInvalid, UserBannedInChannel):
+                    continue
+                except FloodWait as e:
+                    await asyncio.sleep(e.value)
+                    try:
+                        await nlx.read_chat_history(bb.chat.id, max_id=0)
+                    except:
+                        continue
+        LOGGER.info("Finished Read Message...")
