c119edda5bf55e628a5b78f041c08b325e402f3b
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-17 14:16:23 +0700

    update Mix/mix_client.py

--- Mix/mix_client.py
+++ Mix/mix_client.py
@@ -19,7 +19,7 @@ from pyrogram.handlers import *
 from pyrogram.types import *
 from team.nandev.class_log import LOGGER
 from team.nandev.database import ndB, udB
-
+from team.nandev.class_modules import CMD_HELP
 from assistant import BOT_PLUGINS
 from config import *
 from modular import USER_MOD
