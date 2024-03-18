6bd67be03daed8fad6360e9b7c66174280a16358
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-18 09:55:43 +0700

    update assistant/inline.py

--- assistant/inline.py
+++ assistant/inline.py
@@ -22,7 +22,7 @@ from telegraph import upload_file
 
 from Mix import *
 from Mix.core.waktu import get_time, start_time
-# from Mix.core.sender_tools import escape_tag
+from Mix.core.sender_tools import escape_tag
 from modular.copy_con import *
 from modular.pmpermit import *
 
@@ -346,16 +346,14 @@ async def _(c, iq):
         "Setuju": f"pm_ okein {int(org[1])}",
         "Blokir": f"pm_ blokbae {int(org[1])}",
     }
-    for row in button.inline_keyboard:
-        for data in row:
-            add_keyb = (
-                {data.text: data.url} if data.url else {data.text: data.callback_data}
-            )
-            def_keyb.update(add_keyb)
-            keyboard = ikb(def_keyb)
+    if button:
+        for row in button.inline_keyboard:
+            for data in row:
+                add_keyb = ({data.text: data.url} if data.url else {data.text: data.callback_data})
+                def_keyb.update(add_keyb)
+    keyboard = ikb(def_keyb)
     mari = await nlx.get_users(int(org[1]))
-    # tekss = await escape_tag(mari, teks, parse_words)
-    tekss = teks
+    tekss = await escape_tag(mari, teks, parse_words)
     full = f"[{mari.first_name} {mari.last_name or ''}](tg://user?id={int(org[1])})"
     kiki = None
     if nlx.me.id == gw:
