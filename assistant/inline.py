f71271c9f4462f0d4b8009ba6b5f0b5a9fe19fb2
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-18 10:20:11 +0700

    update assistant/inline.py

--- assistant/inline.py
+++ assistant/inline.py
@@ -347,7 +347,7 @@ async def _(c, iq):
                 def_keyb.update(add_keyb)
     keyboard = ikb(def_keyb)
     mari = await nlx.get_users(int(org[1]))
-    tekss = await escape_tag(int(org[1]), teks, parse_words)
+    tekss = await escape_tag(int(org[1]), pm_text, parse_words)
     full = f"[{mari.first_name} {mari.last_name or ''}](tg://user?id={int(org[1])})"
     kiki = None
     if nlx.me.id == gw:
