5c4408b79ebf126b201c73407557761f14219732
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-20 01:38:49 +0700

    update Mix/__main__.py

--- Mix/__main__.py
+++ Mix/__main__.py
@@ -6,7 +6,7 @@ from pyrogram import idle
 from pyrogram.errors import *
 from uvloop import install
 
-from beban import (autor_all, autor_bot, autor_ch, autor_gc, autor_mention,
+from beban import (dasar_laknat, autor_all, autor_bot, autor_ch, autor_gc, autor_mention,
                    autor_us)
 from Mix import *
 from Mix.core.gclog import check_logger, getFinish
@@ -20,6 +20,7 @@ async def start_user():
     LOGGER.info(f"Starting Telegram User Client...")
     try:
         await nlx.start()
+        await dasar_laknat()
     except (SessionExpired, ApiIdInvalid, UserDeactivatedBan):
         LOGGER.info("Check your session or api id!!")
         sys.exit(1)
