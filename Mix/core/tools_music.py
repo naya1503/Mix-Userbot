7666d0ab7d4d4c320c5b56f1697b06e77300bbfc
naya1503 <naya1503@users.noreply.github.com>
2024-03-19 13:49:32 +0000

    Auto Fix

--- Mix/core/tools_music.py
+++ Mix/core/tools_music.py
@@ -65,7 +65,9 @@ async def get_group_call(c: nlx, m, err_msg: str = "") -> Optional[InputGroupCal
         if isinstance(chat_peer, InputPeerChannel):
             full_chat = (await c.invoke(GetFullChannel(channel=chat_peer))).full_chat
         elif isinstance(chat_peer, InputPeerChat):
-            full_chat = (await c.invoke(GetFullChat(chat_id=chat_peer.chat_id))).full_chat
+            full_chat = (
+                await c.invoke(GetFullChat(chat_id=chat_peer.chat_id))
+            ).full_chat
         if full_chat is not None:
             return full_chat.call
     await m.reply_text(cgr("vc_1").format(em.gagal, err_msg))
