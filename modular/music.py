745d202a4b989b81a0174783d7c975a27fcfefea
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-20 01:19:02 +0700

    update modular/music.py

--- modular/music.py
+++ modular/music.py
@@ -10,7 +10,7 @@ from Mix.core.tools_music import *
 @ky.ubot("play", sudo=True)
 @init_client
 async def start_playout(_, message):
-    if not group_call:
+    if not vc:
         await message.reply(
             f"<b>You are not joined [type <code>{message.command}join</code>]</b>"
         )
@@ -27,7 +27,7 @@ async def start_playout(_, message):
     ).overwrite_output().run()
     os.remove(audio_original)
     await message.edit(f"<b>Playing {message.reply_to_message.audio.title}</b>...")
-    group_call.input_filename = input_filename
+    vc.input_filename = input_filename
 
 
 @ky.ubot("volume", sudo=True)
@@ -35,7 +35,7 @@ async def start_playout(_, message):
 async def volume(_, message):
     if len(message.command) < 2:
         await message.edit("<b>You forgot to pass volume [1-200]</b>")
-    await group_call.set_my_volume(message.command[1])
+    await vc.set_my_volume(message.command[1])
     await message.edit(
         f"<b>Your volume is set to</b><code> {message.command[1]}</code>"
     )
@@ -44,33 +44,33 @@ async def volume(_, message):
 @ky.ubot("stop", sudo=True)
 @init_client
 async def stop_playout(_, message):
-    group_call.stop_playout()
+    vc.stop_playout()
     await message.edit("<b>Stoping successfully!</b>")
 
 
 @ky.ubot("vmute", sudo=True)
 @init_client
 async def mute(_, message):
-    group_call.set_is_mute(True)
+    vc.set_is_mute(True)
     await message.edit("<b>Sound off!</b>")
 
 
 @ky.ubot("vunmute", sudo=True)
 @init_client
 async def unmute(_, message):
-    group_call.set_is_mute(False)
+    vc.set_is_mute(False)
     await message.edit("<b>Sound on!</b>")
 
 
 @ky.ubot("pause", sudo=True)
 @init_client
 async def pause(_, message):
-    group_call.pause_playout()
+    vc.pause_playout()
     await message.edit("<b>Paused!</b>")
 
 
 @ky.ubot("resume", sudo=True)
 @init_client
 async def resume(_, message):
-    group_call.resume_playout()
+    vc.resume_playout()
     await message.edit("<b>Resumed!</b>")
