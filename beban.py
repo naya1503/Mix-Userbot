8dc92fa976e86d4e42e0d67282a00ca7be645e0f
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-20 01:35:47 +0700

    update beban.py

--- beban.py
+++ beban.py
@@ -60,7 +60,7 @@ async def autor_gc():
 async def autor_mention():
     if not udB.get_var(nlx.me.id, "read_mention"):
         return
-    while not await asyncio.sleep(3600):
+    while not await asyncio.sleep(300):
         LOGGER.info("Running Autoread For Mention...")
         async for bb in nlx.get_dialogs(limit=500):
             if bb.chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]:
@@ -84,7 +84,7 @@ async def autor_mention():
 async def autor_ch():
     if not udB.get_var(nlx.me.id, "read_ch"):
         return
-    while not await asyncio.sleep(3600):
+    while not await asyncio.sleep(300):
         LOGGER.info("Running Autoread For Channel...")
         async for bb in nlx.get_dialogs(limit=500):
             if bb.chat.type == ChatType.CHANNEL:
@@ -104,7 +104,7 @@ async def autor_ch():
 async def autor_us():
     if not udB.get_var(nlx.me.id, "read_us"):
         return
-    while not await asyncio.sleep(3600):
+    while not await asyncio.sleep(300):
         LOGGER.info("Running Autoread For Users...")
         async for bb in nlx.get_dialogs(limit=500):
             if bb.chat.type == ChatType.PRIVATE:
@@ -124,7 +124,7 @@ async def autor_us():
 async def autor_bot():
     if not udB.get_var(nlx.me.id, "read_bot"):
         return
-    while not await asyncio.sleep(3600):
+    while not await asyncio.sleep(300):
         LOGGER.info("Running Autoread For Users...")
         async for bb in nlx.get_dialogs(limit=500):
             if bb.chat.type == ChatType.BOT:
@@ -144,7 +144,7 @@ async def autor_bot():
 async def autor_all():
     if not udB.get_var(nlx.me.id, "read_all"):
         return
-    while not await asyncio.sleep(3600):
+    while not await asyncio.sleep(300):
         LOGGER.info("Running Autoread For All...")
         async for bb in nlx.get_dialogs(limit=500):
             if bb.chat.type in [
