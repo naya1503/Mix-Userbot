39756c1db4a406afb03d16fd451ab13a6b733c3d
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-19 20:45:49 +0700

    update Mix/core/tools_music.py

--- Mix/core/tools_music.py
+++ Mix/core/tools_music.py
@@ -58,17 +58,15 @@ ydl = YoutubeDL(ydl_opts)
 
 
 async def get_group_call(c: nlx, m, err_msg: str = "") -> Optional[InputGroupCall]:
-
     em = Emojik()
-
     em.initialize()
     chat_peer = await c.resolve_peer(m.chat.id)
     if isinstance(chat_peer, (InputPeerChannel, InputPeerChat)):
         if isinstance(chat_peer, InputPeerChannel):
-            full_chat = (await c.invoke(GetFullChannel(channel=chat_peer))).full_chat
+            full_chat = (await c.send(GetFullChannel(channel=chat_peer))).full_chat
         elif isinstance(chat_peer, InputPeerChat):
             full_chat = (
-                await c.invoke(GetFullChat(chat_id=chat_peer.chat_id))
+                await c.send(GetFullChat(chat_id=chat_peer.chat_id))
             ).full_chat
         if full_chat is not None:
             return full_chat.call
