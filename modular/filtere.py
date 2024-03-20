654e99cd5e54e54ac40170cea10de03803fb949c
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-20 23:09:49 +0700

    update modular/filtere.py

--- modular/filtere.py
+++ modular/filtere.py
@@ -14,7 +14,7 @@ from pyrogram.errors import *
 from pyrogram.types import *
 
 from Mix import *
-from Mix.core.sender_tools import parse_words, send_cmd, split_quotes
+from Mix.core.sender_tools import parse_words, send_cmd, split_quotes, escape_tag
 
 __modles__ = "Filter"
 __help__ = get_cgr("help_filr")
