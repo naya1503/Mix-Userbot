2063e849cbf40fd7d33252ac841957d1de7e2c25
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-20 10:03:19 +0700

    update Mix/core/tools_music.py

--- Mix/core/tools_music.py
+++ Mix/core/tools_music.py
@@ -14,21 +14,7 @@ import ffmpeg
 from pyrogram.errors import FloodWait, MessageNotModified
 from youtube_dl import YoutubeDL
 
-from Mix import *
-
-
-async def eor(message, text):
-    sudo_id = udB.get_list_from_var(nlx.me.id, "SUDO_USER", "ID_NYA")
-    """Edit Message If Its From Self, Else Reply To Message, (Only Works For Sudo's)"""
-    if not message:
-        return await message.edit(text)
-    if not message.from_user:
-        return await message.edit(text)
-    if message.from_user.id in sudo_id:
-        if message.reply_to_message:
-            return await message.reply_to_message.reply_text(text)
-        return await message.reply_text(text)
-    return await message.edit(text)
+from .colong import *
 
 
 stream_vc = {}
