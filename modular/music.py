7ffee448e3d33d0202eaceeb29b0319205313cca
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-20 16:11:01 +0700

    update modular/music.py

--- modular/music.py
+++ modular/music.py
@@ -13,7 +13,7 @@ import string
 
 from pytgcalls import GroupCallFactory, GroupCallFileAction
 
-from Mix import ReplyCheck, nlx
+from Mix import ReplyCheck, nlx, ky
 from Mix.core.tools_music import *
 
 gbr = "https://telegra.ph//file/b2a9611753657547acf15.jpg"
