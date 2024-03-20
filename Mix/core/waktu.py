61e2695420a938588095a07e28c39a3defa03503
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-20 09:39:53 +0700

    update Mix/core/waktu.py

--- Mix/core/waktu.py
+++ Mix/core/waktu.py
@@ -60,26 +60,6 @@ async def get_time(seconds):
     return up_time
 
 
-def time_formatter(milliseconds):
-    minutes, seconds = divmod(int(milliseconds / 1000), 60)
-    hours, minutes = divmod(minutes, 60)
-    days, hours = divmod(hours, 24)
-    weeks, days = divmod(days, 7)
-    tmp = (
-        ((str(weeks) + "w:") if weeks else "")
-        + ((str(days) + "d:") if days else "")
-        + ((str(hours) + "h:") if hours else "")
-        + ((str(minutes) + "m:") if minutes else "")
-        + ((str(seconds) + "s") if seconds else "")
-    )
-    if not tmp:
-        return "0s"
-
-    if tmp.endswith(":"):
-        return tmp[:-1]
-    return tmp
-
-
 # Part Of MissKaty
 async def put_cleanmode(chat_id, message_id):
     if chat_id not in cleanmode:
