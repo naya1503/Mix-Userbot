5fb1eb68dbc5cadd1ca8984ac40b92af2a0a5309
naya1503 <naya1503@users.noreply.github.com>
2024-03-20 17:33:16 +0000

    Auto Fix

--- Mix/mix_client.py
+++ Mix/mix_client.py
@@ -27,7 +27,6 @@ from modular import USER_MOD
 TOKEN_BOT = ndB.get_key("BOT_TOKEN") or bot_token
 
 
-    
 class Userbot(Client):
     _prefix = {}
     _translate = {}
@@ -40,7 +39,13 @@ class Userbot(Client):
             session_string=session,
             device_model="Mix-Userbot",
             ipv6=True,
-            proxy={"scheme": "socks5", "hostname": proxy_host, "port": 1080, "username": proxy_username, "password": proxy_password},
+            proxy={
+                "scheme": "socks5",
+                "hostname": proxy_host,
+                "port": 1080,
+                "username": proxy_username,
+                "password": proxy_password,
+            },
             **kwargs,
         )
 
