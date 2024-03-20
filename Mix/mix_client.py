81ef89123565ba99987256563c1771972aa96d2e
naya1503 <naya1503@users.noreply.github.com>
2024-03-20 09:57:39 +0000

    Auto Fix

--- Mix/mix_client.py
+++ Mix/mix_client.py
@@ -19,7 +19,6 @@ from pyrogram.types import *
 from team.nandev.class_log import LOGGER
 from team.nandev.class_modules import CMD_HELP
 from team.nandev.database import ndB, udB
-from thegokil import DEVS
 
 from assistant import BOT_PLUGINS
 from config import *
