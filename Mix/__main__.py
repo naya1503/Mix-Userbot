f0c245b0029599cd8d0147f748253ae8da94de61
naya1503 <naya1503@users.noreply.github.com>
2024-03-20 18:35:13 +0000

    Auto Fix

--- Mix/__main__.py
+++ Mix/__main__.py
@@ -13,7 +13,7 @@ from Mix.core.waktu import auto_clean
 
 loop = asyncio.get_event_loop_policy()
 event_loop = loop.get_event_loop()
-#loop = asyncio.get_event_loop()
+# loop = asyncio.get_event_loop()
 
 
 async def start_user():
@@ -78,7 +78,7 @@ async def main():
 
 if __name__ == "__main__":
     nlx.run()
-    #install()
-    #asyncio.set_event_loop(event_loop)
-    #event_loop.run_until_complete(main())
-    #loop.run_until_complete(main())
+    # install()
+    # asyncio.set_event_loop(event_loop)
+    # event_loop.run_until_complete(main())
+    # loop.run_until_complete(main())
