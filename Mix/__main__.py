c7f413a9b695fc3fe1a126dfd99525fdae999ce3
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-21 01:36:35 +0700

    update Mix/__main__.py

--- Mix/__main__.py
+++ Mix/__main__.py
@@ -4,6 +4,7 @@ from sys import executable
 
 from pyrogram import idle
 from pyrogram.errors import *
+import uvloop
 
 from beban import (autor_all, autor_bot, autor_ch, autor_gc, autor_mention,
                    autor_us, dasar_laknat)
@@ -13,7 +14,6 @@ from Mix.core.waktu import auto_clean
 
 loop = asyncio.get_event_loop_policy()
 event_loop = loop.get_event_loop()
-# loop = asyncio.get_event_loop()
 
 
 async def start_user():
@@ -77,8 +77,6 @@ async def main():
 
 
 if __name__ == "__main__":
-    nlx.run()
-    # install()
-    # asyncio.set_event_loop(event_loop)
-    # event_loop.run_until_complete(main())
-    # loop.run_until_complete(main())
+    uvloop.install()
+    asyncio.set_event_loop(event_loop)
+    event_loop.run_until_complete(main())
