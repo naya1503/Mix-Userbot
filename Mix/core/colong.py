aee83f130d9c1ea9a874c6c9b16d138418b09461
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-20 16:46:33 +0700

    update Mix/core/colong.py

--- Mix/core/colong.py
+++ Mix/core/colong.py
@@ -6,6 +6,13 @@ import time
 from asyncio import get_event_loop
 from concurrent.futures import ThreadPoolExecutor
 from functools import partial
+from asyncio import get_event_loop
+from functools import partial
+from json import dumps, loads
+from urllib.parse import quote_plus
+
+from requests import get
+from yt_dlp import YoutubeDL
 
 from yt_dlp import YoutubeDL
 
@@ -93,15 +100,6 @@ def run_in_exc(f):
     return wrapper
 
 
-from asyncio import get_event_loop
-from functools import partial
-from json import dumps, loads
-from urllib.parse import quote_plus
-
-from requests import get
-from yt_dlp import YoutubeDL
-
-
 def run_sync(func, *args, **kwargs):
     return get_event_loop().run_in_executor(None, partial(func, *args, **kwargs))
 
@@ -137,83 +135,3 @@ async def YoutubeDownload(url, as_video=False):
     views = f"{ytdl_data['view_count']:,}".replace(",", ".")
     thumb = f"https://img.youtube.com/vi/{videoid}/hqdefault.jpg"
     return file_name, title, url, duration, views, channel, thumb, data_ytp
-
-
-class YouTubeSearch:
-    def __init__(self, search_terms: str, max_results=None):
-        self.search_terms = search_terms
-        self.max_results = max_results
-        self.videos = self._search()
-
-    def _search(self):
-        encoded_search = quote_plus(self.search_terms)
-        url = f"https://www.youtube.com/results?search_query={encoded_search}"
-        response = get(url).text
-        while "ytInitialData" not in response:
-            response = get(url).text
-        results = self._parse_html(response)
-        if self.max_results is not None and len(results) > self.max_results:
-            return results[: self.max_results]
-        return results
-
-    @staticmethod
-    def _parse_html(response):
-        results = []
-        start = response.index("ytInitialData") + len("ytInitialData") + 3
-        end = response.index("};", start) + 1
-        json_str = response[start:end]
-        data = loads(json_str)
-
-        for contents in data["contents"]["twoColumnSearchResultsRenderer"][
-            "primaryContents"
-        ]["sectionListRenderer"]["contents"]:
-            for video in contents["itemSectionRenderer"]["contents"]:
-                res = {}
-                if "videoRenderer" in video.keys():
-                    video_data = video.get("videoRenderer", {})
-                    res["id"] = video_data.get("videoId", None)
-                    res["title"] = (
-                        video_data.get("title", {})
-                        .get("runs", [[{}]])[0]
-                        .get("text", None)
-                    )
-                    res["duration"] = video_data.get("lengthText", {}).get(
-                        "simpleText", 0
-                    )
-                    res["views"] = video_data.get("viewCountText", {}).get(
-                        "simpleText", 0
-                    )
-                    res["link"] = "https://www.youtube.com" + (
-                        video_data.get("navigationEndpoint", {})
-                        .get("commandMetadata", {})
-                        .get("webCommandMetadata", {})
-                        .get("url", None)
-                    )
-                    if (
-                        res["duration"] != 0
-                        and res["views"] != 0
-                        and sum(
-                            int(x) * 60**i
-                            for i, x in enumerate(
-                                reversed(str(res["duration"]).split(":"))
-                            )
-                        )
-                        < 7200
-                    ):
-                        results.append(res)
-
-            if results:
-                return results
-        return results
-
-    def to_dict(self, clear_cache=True):
-        result = self.videos
-        if clear_cache:
-            self.videos = ""
-        return result
-
-    def to_json(self, clear_cache=True):
-        result = dumps({"videos": self.videos})
-        if clear_cache:
-            self.videos = ""
-        return result
