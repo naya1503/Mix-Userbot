53a35ecebd6038f4f3cbaff587c394e9d429126d
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-20 22:40:10 +0700

    update modular/pinge.py

--- modular/pinge.py
+++ modular/pinge.py
@@ -30,4 +30,4 @@ async def _(c: nlx, m):
     _ping = cgr("ping_1").format(
         em.ping, str(delta_ping).replace(".", ","), em.pong, upnya
     )
-    await c.eor(m, _ping)
+    await m.reply(_ping)
