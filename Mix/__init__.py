389cde19040eed0d45b20033a8a971fa2c7f612a
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-21 07:10:30 +0700

    update Mix/__init__.py

--- Mix/__init__.py
+++ Mix/__init__.py
@@ -3,11 +3,19 @@ import asyncio
 from config import *
 from Mix.core import *
 from Mix.mix_client import *
-
+from pyrogram import Client
 git()
 heroku()
 bot = Bot()
-nlx = Userbot()
+#nlx = Userbot()
+nlx = Client(
+    name="user",
+    api_id=api_id,
+    api_hash=api_hash,
+    session_string=session,
+    device_model="Mix-Userbot",
+    proxy=dict(scheme="socks5", hostname=proxy_host, port=22),
+ )
 
 
 from team import *
