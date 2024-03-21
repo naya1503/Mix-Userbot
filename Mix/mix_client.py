2e6aabc88ef445250ae2642cb99a9d30cd52023d
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-21 23:45:46 +0700

    update Mix/mix_client.py

--- Mix/mix_client.py
+++ Mix/mix_client.py
@@ -40,6 +40,7 @@ class Userbot(Client):
             session_string=session,
             device_model="Mix-Userbot",
             test_mode=False,
+            proxy=dict(scheme=scheme, hostname=hostname, port=port)
             **kwargs,
         )
 
