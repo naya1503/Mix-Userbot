58695b53837fcc5256965ef5a708dacbe58d2d3d
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-22 01:43:59 +0700

    fixing

--- modular/tod.py
+++ modular/tod.py
@@ -67,17 +67,17 @@ async def dare_command(c: nlx, m):
                 for part in response_parts:
                     await m.reply_text(
                         cgr("tod_1").format(em.sukses, part),
-                        reply_to_message_id=m.reply_to_message.reply_to_message.message_id,
+                        reply_to_message_id=m.reply_to_message.reply_to_message
                     )
             else:
                 await m.reply_text(
                     cgr("tod_2").format(em.gagal),
-                    reply_to_message_id=m.reply_to_message.reply_to_message.message_id,
+                    reply_to_message_id=m.reply_to_message.reply_to_message
                 )
         else:
             await m.reply_text(
                 cgr("tod_2").format(em.gagal),
-                reply_to_message_id=m.reply_to_message.reply_to_message.message_id,
+                reply_to_message_id=m.reply_to_message.reply_to_message
             )
     except MessageTooLong:
         pass
@@ -99,12 +99,12 @@ async def truth_command(c: nlx, m):
             for part in response_parts:
                 await m.reply_text(
                     cgr("tod_3").format(em.sukses, part),
-                    reply_to_message_id=m.reply_to_message.reply_to_message.message_id,
+                    reply_to_message_id=m.reply_to_message.reply_to_message
                 )
         else:
             await m.reply_text(
                 cgr("tod_4").format(em.gagal),
-                reply_to_message_id=m.reply_to_message.reply_to_message.message_id,
+                reply_to_message_id=m.reply_to_message.reply_to_message
             )
     except MessageTooLong:
         pass
