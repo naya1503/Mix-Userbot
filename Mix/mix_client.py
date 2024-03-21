9fa997d009cb8865a47c23dd6052ce6b0f165ca4
naya1503 <naya1503@users.noreply.github.com>
2024-03-21 16:46:17 +0000

    Auto Fix

--- Mix/mix_client.py
+++ Mix/mix_client.py
@@ -40,8 +40,7 @@ class Userbot(Client):
             session_string=session,
             device_model="Mix-Userbot",
             test_mode=False,
-            proxy=dict(scheme=scheme, hostname=hostname, port=int(port))
-            **kwargs,
+            proxy=dict(scheme=scheme, hostname=hostname, port=int(port)) ** kwargs,
         )
 
     def on_message(self, filters=None, group=-1):
