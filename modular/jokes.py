5a0d2f575b617f0ed8dc307c41ee3116001ab4b2
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-23 21:47:35 +0700

    add module joke

---
+++ modular/jokes.py
@@ -0,0 +1,24 @@
+import requests
+
+from Mix import *
+
+
+__modles__ = "Joke"
+__help__ = "Joke"
+
+
+def get_joke():
+    url = "https://api.safon.dev/joke"
+    response = requests.get(url)
+    data = response.json()
+    joke = data.get("joke")
+    return joke
+
+
+@ky.ubot("joke", sudo=True)
+async def _(c :nlx, m):
+    em = Emojik()
+    em.initialize()
+    pros = await m.reply(cgr("proses").format(em.proses))
+    joke = get_joke()
+    await pros.edit(joke)
