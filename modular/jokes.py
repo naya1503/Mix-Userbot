3edefe125a006c072e79c05416de89b9765781f4
jonesroot <jonesroot@users.noreply.github.com>
2024-03-23 14:57:12 +0000

    Auto Fix

--- modular/jokes.py
+++ modular/jokes.py
@@ -1,16 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
+
 from Mix import *
 
 __modles__ = "Joke"
 __help__ = "Joke"
 
+
 def get_joke():
     url = "https://api.safon.dev/joke"
     response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    joke = soup.find('p').text.strip()
+    soup = BeautifulSoup(response.text, "html.parser")
+    joke = soup.find("p").text.strip()
     return joke
+
 
 @ky.ubot("joke", sudo=True)
 async def _(c: nlx, m):
