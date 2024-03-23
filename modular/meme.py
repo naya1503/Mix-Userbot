b2b8a595a75e6db7d9c6ea2319c4e9ad994f22fa
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-24 01:50:44 +0700

    fixing meme

--- modular/meme.py
+++ modular/meme.py
@@ -1,5 +1,4 @@
-import json
-import subprocess
+import requests
 
 from Mix import *
 
@@ -7,29 +6,19 @@ __modles__ = "Meme"
 __help__ = "Meme"
 
 
-async def scrape_memes(count_page=1):
+async def scrape_memes():
     memes = []
     try:
-        command = f'curl -X GET "https://api.safone.dev/meme?page={count_page}" -H "accept: application/json"'
-        process = subprocess.Popen(
-            command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        )
-        output, error = process.communicate()
-        if process.returncode == 0:
-            try:
-                data = json.loads(output)
-                results = data.get("results", [])
-                for meme_data in results:
-                    if (
-                        "image/jpeg" in meme_data.get("type", "").lower()
-                        or "image/jpg" in meme_data.get("type", "").lower()
-                    ):
-                        image_url = meme_data.get("image")
-                        memes.append(image_url)
-            except json.JSONDecodeError as e:
-                print(f"Failed to decode JSON: {e}")
+        url = "https://memeapi.dev/api/v1/memes"
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            meme_data = data.get("data")
+            if meme_data and "image" in meme_data:
+                image_url = meme_data["image"]
+                memes.append(image_url)
         else:
-            print(f"Failed to scrape memes: {error.decode()}")
+            print(f"Failed to scrape memes: {response.text}")
     except Exception as e:
         print(f"Failed to scrape memes: {e}")
     return memes
