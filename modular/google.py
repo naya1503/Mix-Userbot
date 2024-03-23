f158d4efb7f47448f2d80bc9934955f97968ff24
jonesroot <jonesroot@users.noreply.github.com>
2024-03-23 16:02:31 +0000

    Auto Fix

--- modular/google.py
+++ modular/google.py
@@ -45,7 +45,10 @@ async def google_command(c: nlx, m):
 
     if "answer" in data:
         results = data["answer"]
-        await c.send_message(chat_id=m.chat.id, text=f"{em.sukses} **Pertanyaan :** `{query}`\n\n{em.profil} **Hasil :** `{results}`")
+        await c.send_message(
+            chat_id=m.chat.id,
+            text=f"{em.sukses} **Pertanyaan :** `{query}`\n\n{em.profil} **Hasil :** `{results}`",
+        )
         await pros.delete()
     else:
         await c.send_message(
