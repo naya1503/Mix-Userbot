554e4999ebd3133a933c68a4c8b4cc3a24088349
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-24 01:21:32 +0700

    fixing

--- modular/proxy.py
+++ modular/proxy.py
@@ -8,7 +8,7 @@ __modles__ = "Proxy"
 __help__ = get_cgr("help_prox")
 
 
-def fetch_proxies(proxy_type):
+async def fetch_proxies(proxy_type):
     url = f"https://www.proxy-list.download/api/v1/get?type={proxy_type}"
     response = requests.get(url)
     if response.status_code == 200:
@@ -18,19 +18,19 @@ def fetch_proxies(proxy_type):
         return None
 
 
-async def get_best_proxy(proxy_type):
-    proxies = fetch_proxies(proxy_type)
-    if proxies:
-        return random.choice(proxies)
-    else:
-        return None
+# async def get_best_proxy(proxy_type):
+#     proxies = fetch_proxies(proxy_type)
+#     if proxies:
+#         return random.choice(proxies)
+#     else:
+#         return None
 
 
 async def send_proxy(c: nlx, chat_id, proxy):
     if proxy:
         await c.send_message(chat_id, proxy)
     else:
-        await c.send_message(chat_id, "Tidak dapat menemukan proxy yang valid.")
+        await c.send_message(chat_id, f"{em.gagal} Tidak dapat menemukan proxy yang valid.")
 
 
 @ky.ubot("getproxy", sudo=True)
@@ -38,13 +38,16 @@ async def get_proxy_command(c: nlx, m):
     em = Emojik()
     em.initialize()
     try:
+        pros = await m.reply(cgr("proses").format(em.proses))
         command = m.text.split()[1].lower()
         if command not in ["http", "socks4", "socks5"]:
-            await c.send_message(m.chat.id, "Perintah tidak valid.")
+            await c.send_message(m.chat.id, f"{em.gagal} Perintah tidak valid.")
             return
 
         proxy_type = command
-        best_proxy = await get_best_proxy(proxy_type)
+        best_proxy = await fetch_proxies(proxy_type)
         await send_proxy(c, m.chat.id, best_proxy)
+        await pros.delete()
     except IndexError:
-        await c.send_message(m.chat.id, "Perintah tidak valid.")
+        await c.send_message(m.chat.id, f"{em.gagal} Perintah tidak valid.")
+        await pros.delete()
