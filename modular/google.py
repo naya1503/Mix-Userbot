3f89fc65e64653f41a1829efff60f45c8cdcfdc4
jonesroot <jonesroot@users.noreply.github.com>
2024-03-23 15:24:28 +0000

    Auto Fix

--- modular/google.py
+++ modular/google.py
@@ -1,11 +1,10 @@
-from pyrogram import *
 import requests
+from pyrogram import *
 
 from Mix import *
 
 __modles__ = "Google"
 __help__ = "Google"
-
 
 
 async def google_search(query, limit=3):
@@ -18,31 +17,31 @@ async def google_search(query, limit=3):
             {
                 "description": result["description"],
                 "link": result["link"],
-                "title": result["title"]
+                "title": result["title"],
             }
             for result in data["results"][:limit]
-        ]
+        ],
     }
+
 
 @ky.ubot("google", sudo=True)
 async def google_command(c: nlx, m):
     query = " ".join(m.command[1:])
-    
+
     if query:
         results = await google_search(query)
-        if results['results']:
-            for result in results['results']:
+        if results["results"]:
+            for result in results["results"]:
                 await c.send_message(
                     chat_id=m.chat.id,
-                    text=f"{result['title']}\n{result['link']}\n{result['description']}\n"
+                    text=f"{result['title']}\n{result['link']}\n{result['description']}\n",
                 )
         else:
             await c.send_message(
                 chat_id=m.chat.id,
-                text="Maaf, tidak dapat menemukan hasil untuk pencarian ini."
+                text="Maaf, tidak dapat menemukan hasil untuk pencarian ini.",
             )
     else:
         await c.send_message(
-            chat_id=m.chat.id,
-            text="Silakan berikan query untuk pencarian Google."
+            chat_id=m.chat.id, text="Silakan berikan query untuk pencarian Google."
         )
