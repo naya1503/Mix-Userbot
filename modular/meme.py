8539168d94613dc644288c9f161379c338228578
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-23 23:53:30 +0700

    fixing meme

--- modular/meme.py
+++ modular/meme.py
@@ -10,7 +10,7 @@ __help__ = "Meme"
 async def scrape_memes(count_page=1):
     memes = []
     try:
-        command = f'curl -X GET "https://api.safone.dev/meme?page=1" -H "accept: application/json"'
+        command = f'curl -X GET "https://api.safone.dev/meme?page={count_page}" -H "accept: application/json"'
         process = subprocess.Popen(
             command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
         )
@@ -20,7 +20,7 @@ async def scrape_memes(count_page=1):
                 data = json.loads(output)
                 results = data.get("results", [])
                 for meme_data in results:
-                    if "image/jpeg" in meme_data.get("type", "").lower():
+                    if "image/jpeg" in meme_data.get("type", "").lower() or "image/jpg" in meme_data.get("type", "").lower():
                         image_url = meme_data.get("image")
                         memes.append(image_url)
             except json.JSONDecodeError as e:
@@ -34,20 +34,7 @@ async def scrape_memes(count_page=1):
 
 @ky.ubot("meme", sudo=True)
 async def _(c: nlx, m):
-    try:
-        command_parts = m.text.split(" ")
-        if len(command_parts) == 1:
-            count_page = 1
-        elif len(command_parts) == 2:
-            count_page = int(command_parts[1])
-        else:
-            await m.reply("Format perintah salah. Gunakan: meme [count_page]")
-            return
-    except ValueError:
-        await m.reply("Halaman harus berupa bilangan bulat.")
-        return
-
-    memes = await scrape_memes(count_page)
+    memes = await scrape_memes()
     if memes:
         for image_url in memes:
             await m.reply_photo(photo=image_url)
