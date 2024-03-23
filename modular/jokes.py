9a35525c8aa9ade9b6a8a53a84a1a8520b996ed3
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-23 21:56:52 +0700

    fixing

--- modular/jokes.py
+++ modular/jokes.py
@@ -1,23 +1,21 @@
 import requests
-
+from bs4 import BeautifulSoup
 from Mix import *
 
 __modles__ = "Joke"
 __help__ = "Joke"
 
-
 def get_joke():
     url = "https://api.safon.dev/joke"
     response = requests.get(url)
-    data = response.json()
-    joke = data.get("joke")
+    soup = BeautifulSoup(response.text, 'html.parser')
+    joke = soup.find('p').text.strip()
     return joke
-
 
 @ky.ubot("joke", sudo=True)
 async def _(c: nlx, m):
     em = Emojik()
     em.initialize()
     pros = await m.reply(cgr("proses").format(em.proses))
-    joke = await c.get_joke(joke)
+    joke = get_joke()
     await pros.edit(f"{joke}")
