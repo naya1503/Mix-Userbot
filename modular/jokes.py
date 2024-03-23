5add133f6847e1b7fc1ee1c56aeb940d247ff0ef
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-23 21:59:51 +0700

    fixing joke

--- modular/jokes.py
+++ modular/jokes.py
@@ -1,24 +1,23 @@
-import requests
+import aiohttp
 from bs4 import BeautifulSoup
-
 from Mix import *
 
 __modles__ = "Joke"
 __help__ = "Joke"
 
-
-def get_joke():
-    url = "https://api.safon.dev/joke"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    joke = soup.find("p").text.strip()
-    return joke
-
+async def get_joke():
+    async with aiohttp.ClientSession() as session:
+        url = "https://api.safon.dev/joke"
+        async with session.get(url) as response:
+            html = await response.text()
+            soup = BeautifulSoup(html, 'html.parser')
+            joke = soup.find('p').text.strip()
+            return joke
 
 @ky.ubot("joke", sudo=True)
 async def _(c: nlx, m):
     em = Emojik()
     em.initialize()
     pros = await m.reply(cgr("proses").format(em.proses))
-    joke = get_joke()
+    joke = await get_joke()
     await pros.edit(f"{joke}")
