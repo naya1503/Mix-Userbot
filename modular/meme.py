c4009a119ac5359fa8075ba198590b4dfe9fa359
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-24 01:56:59 +0700

    fixing meme

--- modular/meme.py
+++ modular/meme.py
@@ -1,31 +1,63 @@
 import requests
+import os
 
 from Mix import *
+
 
 __modles__ = "Meme"
 __help__ = "Meme"
 
 
+TEMP_DIR = "temp"
+
+
+async def download_image(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            if not os.path.exists(TEMP_DIR):
+                os.makedirs(TEMP_DIR)
+            file_path = os.path.join(TEMP_DIR, "meme.jpg")
+            with open(file_path, "wb") as file:
+                file.write(response.content)
+            return file_path
+        else:
+            print(f"Failed to download image: {response.text}")
+            return None
+    except Exception as e:
+        print(f"Failed to download image: {e}")
+        return None
+
+
 async def scrape_memes():
-    memes = []
     try:
         url = "https://memeapi.dev/meme"
         response = requests.get(url)
         if response.status_code == 200:
-            meme_url = response.text
-            memes.append(meme_url)
+            data = response.json()
+            meme_url = data.get("data")
+            return meme_url
         else:
             print(f"Failed to scrape memes: {response.text}")
+            return None
     except Exception as e:
         print(f"Failed to scrape memes: {e}")
-    return memes
+        return None
 
 
 @ky.ubot("meme", sudo=True)
 async def _(c: nlx, m):
-    memes = await scrape_memes()
-    if memes:
-        for meme_url in memes:
-            await m.reply_photo(photo=meme_url)
-    else:
-        await m.reply("Gagal mendapatkan meme. Silakan coba lagi nanti.")
+    try:
+        meme_url = await scrape_memes()
+        if meme_url:
+            image_path = await download_image(meme_url)
+            if image_path:
+                await m.reply_photo(photo=image_path)
+                os.remove(image_path)
+            else:
+                await m.reply("Gagal mendownload gambar. Silakan coba lagi nanti.")
+        else:
+            await m.reply("Gagal mendapatkan URL gambar. Silakan coba lagi nanti.")
+    except Exception as e:
+        print(f"Failed to process meme: {e}")
+        await m.reply("Terjadi kesalahan dalam pemrosesan meme.")
