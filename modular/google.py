50f8d3f16dbfd4d0793b9d2d900b12859b4474e4
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-23 22:43:27 +0700

    fixing google

--- modular/google.py
+++ modular/google.py
@@ -38,7 +38,7 @@ async def google_command(c: nlx, m):
     em = Emojik()
     em.initialize()
     await m.reply(cgr("proses").format(em.proses))
-    query = m.command[1]
+    query = m.text.split(maxsplit=1)[1]
 
     if query:
         results = await google_search(query)
