4e07a27a6f9b82c270a5f790e6615de515bf0d12
naya1503 <naya1503@users.noreply.github.com>
2024-03-24 13:23:21 +0000

    Auto Fix

--- modular/convert.py
+++ modular/convert.py
@@ -272,7 +272,7 @@ async def _(c: nlx, message):
         if args in list_efek:
             indir = await c.download_media(reply)
             cemew = f"ffmpeg -i '{indir}' {get_efek[args]} audio.mp3"
-            mek = await c.bash(cemew)
+            await c.bash(cemew)
             await pros.delete()
             await message.reply_voice(
                 open("audio.mp3", "rb"), caption=f"{em.sukses} Efek {args}"
