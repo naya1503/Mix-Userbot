6ceb6f7916363ce4e541ffb32dc9e0bc510b14df
naya1503 <naya1503@users.noreply.github.com>
2024-03-24 13:35:08 +0000

    Auto Fix

--- modular/convert.py
+++ modular/convert.py
@@ -275,8 +275,7 @@ async def _(c: nlx, message):
             cemew = f"ffmpeg -i '{indir}' {get_efek[args]} {out_file}"
             await c.run_cmd(cemew)
             await pros.delete()
-            await message.reply_voice(out_file, caption=f"{em.sukses} Efek {args}"
-            )
+            await message.reply_voice(out_file, caption=f"{em.sukses} Efek {args}")
             for files in (out_file, indir):
                 if files and os.path.exists(files):
                     os.remove(files)
