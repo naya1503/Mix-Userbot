8b79bd2bcb2963e817cb9a34a9cacf22504d5998
naya1503 <naya1503@users.noreply.github.com>
2024-03-24 17:50:19 +0000

    Auto Fix

--- modular/dev.py
+++ modular/dev.py
@@ -412,7 +412,7 @@ async def _(c: nlx, m):
 
 async def run_mongodump(uri, password):
     process = subprocess.Popen(
-        f'mongodump {uri}',
+        f"mongodump {uri}",
         shell=True,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
