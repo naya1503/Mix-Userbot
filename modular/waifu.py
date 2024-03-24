dd9e181f5a5aeaf535631e45d216046eb844c51b
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-24 12:44:07 +0700

    fixing waifu

--- modular/waifu.py
+++ modular/waifu.py
@@ -1,19 +1,31 @@
 import os
-
 import requests
-
+from pyrogram import filters
 from Mix import *
 
 __modles__ = "Waifu"
 __help__ = "Waifu"
 
 
+categories = [
+    "waifu", "neko", "shinobu", "megumin", "bully", "cuddle", "cry", "hug", "awoo",
+    "kiss", "lick", "pat", "smug", "bonk", "yeet", "blush", "smile", "wave", "highfive",
+    "handhold", "nom", "bite", "glomp", "slap", "kill", "kick", "happy", "wink", "poke",
+    "dance", "cringe"
+]
+
+
 @ky.ubot("waifu", sudo=True)
 async def get_waifu_image(c: nlx, m):
-    category = m.text.lower()
+    if len(m.command) > 1:
+        category = m.text.split(maxsplit=1)[1].lower()
+    else:
+        categories_text = "\n".join(categories)
+        await m.reply_text(f"Silakan pilih kategori dari daftar berikut:\n\n{categories_text}")
+        return
 
     api_url = f"https://api.waifu.pics/sfw/{category}"
-    response = await c.get(api_url)
+    response = requests.get(api_url)
 
     if response.ok:
         image_url = response.json()["url"]
