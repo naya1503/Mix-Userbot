47af6d86f293dbbba98671cf864180130c595507
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-24 22:36:59 +0700

    update Mix/__main__.py

--- Mix/__main__.py
+++ Mix/__main__.py
@@ -9,7 +9,8 @@ from Mix import *
 from Mix.core.gclog import check_logger, getFinish
 from Mix.core.waktu import auto_clean
 
-loop = asyncio.get_event_loop()
+loop = asyncio.get_event_loop_policy()
+event_loop = loop.get_event_loop()
 
 
 async def start_user():
@@ -72,4 +73,5 @@ async def main():
 
 
 if __name__ == "__main__":
-    loop.run_until_complete(main())
+    asyncio.set_event_loop(event_loop)
+    event_loop.run_until_complete(main())
