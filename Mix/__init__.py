70719335344fe0ee340645f2a3d69d52403e4ddf
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-24 22:34:43 +0700

    update Mix/__init__.py

--- Mix/__init__.py
+++ Mix/__init__.py
@@ -2,7 +2,7 @@ import asyncio
 
 from pyrogram import Client
 from pyromod import listen
-
+import uvloop
 from config import *
 from Mix.core import *
 from Mix.mix_client import *
@@ -11,7 +11,7 @@ git()
 heroku()
 bot = Bot()
 nlx = Userbot()
-
+uvloop.install()
 
 from team import *
 from thegokil import DEVS, NO_GCAST
