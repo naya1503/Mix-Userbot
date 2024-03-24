7bc863e6b8c0ffd990dbcbbf2b19f99dccd54a35
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-24 17:11:19 +0700

    update modular/quran.py

--- modular/quran.py
+++ modular/quran.py
@@ -1,14 +1,12 @@
 import requests
-import wget
 from pyrogram import *
-
+import wget
 from Mix import *
 
 processed_surah_numbers = set()
 
 __modules__ = "Quran"
 __help__ = "Quran"
-
 
 def download_audio(url, file_name):
     try:
@@ -17,7 +15,6 @@ def download_audio(url, file_name):
     except Exception as e:
         print(f"Failed to download audio: {e}")
         return False
-
 
 def ambil_nama_surah(surah_name):
     response = requests.get("https://equran.id/api/v2/surat")
@@ -28,6 +25,28 @@ def ambil_nama_surah(surah_name):
                 return surah_info
     return None
 
+
+def ambil_daftar_surah():
+    response = requests.get("https://equran.id/api/v2/surat")
+    surah_list = []
+    if response.status_code == 200:
+        data = response.json()["data"]
+        for surah_info in data:
+            surah_list.append((surah_info["namaLatin"], surah_info["jumlahAyat"], surah_info["tempatTurun"]))
+    return surah_list
+
+
+@ky.ubot("list-surah|daftar-surah", sudo=True)
+async def _(c: nlx, m):
+    em = Emojik()
+    em.initialize()
+    pros = await m.reply(cgr("proses").format(em.proses))
+    msg = ""
+    for count, nama, jumlah, turun in ambil_daftar_surah():
+        msg += "**• {}. {} `{}` {}**\n".format(count, nama, jumlah, turun)
+    await m.reply(msg, reply_to_message_id=ReplyCheck(m))
+    await pros.delete()
+    return
 
 @ky.ubot("surat|surah|qs", sudo=True)
 async def _(c: nlx, m):
@@ -73,18 +92,13 @@ async def _(c: nlx, m):
                     file_name = f"{surah_name.capitalize()}.txt"
                     with open(file_name, "w", encoding="utf-8") as file:
                         file.write(response_text)
-                    aud = await m.reply_audio(
-                        audio_file_name, reply_to_message_id=ReplyCheck(m)
-                    )
+                    aud = await m.reply_audio(audio_file_name, reply_to_message_id=ReplyCheck(m))
                     await m.reply_document(file_name, reply_to_message_id=aud.id)
                 else:
-                    await m.reply_audio(
-                        audio_file_name,
-                        caption=response_text,
-                        reply_to_message_id=ReplyCheck(m),
+                    await m.reply_audio(audio_file_name, caption=response_text, reply_to_message_id=ReplyCheck(m)
                     )
             else:
-                await m.reply(f"{em.gagal} Gagal mengunduh audio.")
+                 await m.reply(f"{em.gagal} Gagal mengunduh audio.")
         else:
             await m.reply(response_text, reply_to_message_id=ReplyCheck(m))
 
