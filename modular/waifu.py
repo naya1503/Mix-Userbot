842412410dd9ab069b25a9598de112a9a96801e7
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-24 12:37:22 +0700

    add module waifu

---
+++ modular/waifu.py
@@ -0,0 +1,37 @@
+import os
+import requests
+from pyrogram import filters
+from Mix import *
+
+
+__modles__ = "Waifu"
+__help__ = "Waifu"
+
+
+
+@ky.ubot("waifu", sudo=True)
+async def get_waifu_image(c: nlx, m):
+    category = m.text.lower()
+    
+    api_url = f"https://api.waifu.pics/sfw/{category}"
+    response = await c.get(api_url)
+
+    if response.ok:
+        image_url = response.json()["url"]
+        image_response = requests.get(image_url)
+
+        if image_response.status_code == 200:
+            await download_and_send_image(c, m, image_url, image_response.content)
+        else:
+            await m.reply_text("Gagal mengunduh gambar.")
+    else:
+        await m.reply_text("Gagal mengambil gambar.")
+
+
+async def download_and_send_image(client, message, image_url, image_content):
+    await client.send_photo(message.chat.id, image_content)
+    folder_path = 'waifu_images'
+    os.makedirs(folder_path, exist_ok=True)
+    filename = image_url.split('/')[-1]
+    filepath = os.path.join(folder_path, filename)
+    os.remove(filepath)
